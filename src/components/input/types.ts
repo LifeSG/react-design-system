4767e3dd6ac80df31ea65a80694c4e55c112dcd6
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-07-01 17:56:00 +0800

    [MISC][KT] Add ImageSelector and Input components

--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -1,34 +1,34 @@
 import React from "react";
 
 export type InputType =
-	| "button"
-	| "checkbox"
-	| "color"
-	| "date"
-	| "datetime-local"
-	| "email"
-	| "file"
-	| "hidden"
-	| "image"
-	| "month"
-	| "number"
-	| "password"
-	| "radio"
-	| "range"
-	| "reset"
-	| "search"
-	| "submit"
-	| "tel"
-	| "text"
-	| "time"
-	| "url"
-	| "week";
+    | "button"
+    | "checkbox"
+    | "color"
+    | "date"
+    | "datetime-local"
+    | "email"
+    | "file"
+    | "hidden"
+    | "image"
+    | "month"
+    | "number"
+    | "password"
+    | "radio"
+    | "range"
+    | "reset"
+    | "search"
+    | "submit"
+    | "tel"
+    | "text"
+    | "time"
+    | "url"
+    | "week";
 
 export interface InputProps
-	extends React.InputHTMLAttributes<HTMLInputElement> {
-	type?: InputType;
-	spacing?: number;
-	error?: boolean;
+    extends React.InputHTMLAttributes<HTMLInputElement> {
+    type?: InputType;
+    spacing?: number;
+    error?: boolean;
 }
 
 /** To be exposed for Form component inheritance */
