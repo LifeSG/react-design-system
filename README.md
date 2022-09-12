e22c74fe805d1a8e61fd42020d5de1d8cec04d79
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-09-13 00:02:55 +0800

    [MISC][KT] Update various settings

---
+++ README.md
@@ -0,0 +1,3 @@
+# React LifeSG Design System
+
+This library contains the web ui components that is shared for the usage in LifeSG web pages.
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    roots: ["./src"],
     moduleFileExtensions: ["tsx", "ts", "js"],
-    testMatch: ["<rootDir>/src/**/__tests__/**/*.(js|ts)?(x)"],
+    testMatch: [
+        "<rootDir>/src/**/__tests__/**/*.(js|ts)?(x)",
+        "<rootDir>/tests/**/*.(ts)?(x)",
+    ],
     moduleNameMapper: {
         "react-player": "<rootDir>/src/__mocks__/fileMock.js",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
