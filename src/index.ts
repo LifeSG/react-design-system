f1392c42e5fe794ef7d03a97917e4a8b88f15e01
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-09-20 00:51:50 +0800

    [PORTOVER][KT] Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ export * from "./button";
 export * from "./checkbox";
 export * from "./color";
 export * from "./icon";
+export * from "./icon-button";
+export * from "./layout";
 export * from "./media";
 export * from "./overlay";
 export * from "./text";
