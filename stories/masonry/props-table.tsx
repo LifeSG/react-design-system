4bb504783068180a8a8b74e93fb070c22defdaa7
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-09-21 01:30:27 +0800

    [PORTOVER][KT] Add Masonry

--- stories/masonry/props-table.tsx
+++ stories/masonry/props-table.tsx
@@ -3,6 +3,7 @@ import {
     DefaultCol,
     DescriptionCol,
     NameCol,
+    Section,
     Table,
 } from "../storybook-common/api-table";
 import { TabAttribute, Tabs } from "../storybook-common/tabs";
@@ -11,10 +12,32 @@ export const MasonryGridTable = () => (
     <Table>
         <tr>
             <NameCol mandatory>numOfCols</NameCol>
-            <DescriptionCol
-                propTypes={["{ lg: number, md: number, sm: number }"]}
-            >
+            <DescriptionCol propTypes={["ColumnCountAttribute"]}>
                 The number of columns for the different layouts
+            </DescriptionCol>
+            <DefaultCol />
+        </tr>
+
+        <Section>ColumnCountAttribute</Section>
+
+        <tr>
+            <NameCol>lg</NameCol>
+            <DescriptionCol propTypes={["number"]}>
+                The number of columns for the large layouts
+            </DescriptionCol>
+            <DefaultCol />
+        </tr>
+        <tr>
+            <NameCol>md</NameCol>
+            <DescriptionCol propTypes={["number"]}>
+                The number of columns for the medium layouts
+            </DescriptionCol>
+            <DefaultCol />
+        </tr>
+        <tr>
+            <NameCol>sm</NameCol>
+            <DescriptionCol propTypes={["number"]}>
+                The number of columns for the small layouts
             </DescriptionCol>
             <DefaultCol />
         </tr>
