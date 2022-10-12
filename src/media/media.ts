eedbad1fbe9164b8f6bf0cc03617cb0154c93909
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-10-12 17:03:30 +0800

    [PORTOVER][KT] Add export for media widths

--- src/media/media.ts
+++ src/media/media.ts
@@ -1,12 +1,12 @@
-import { MediaWidths } from "../spec/media-spec";
+import { MediaWidths as MediaWidthsSpec } from "../spec/media-spec";
 import { MediaType, MediaWidth } from "./types";
 
 // =============================================================================
 // HELPER FUNCTION
 // =============================================================================
 const getMediaQuerySpec = (type: "max-width" | "min-width") => {
-    return Object.keys(MediaWidths).reduce((accumulator, key) => {
-        const mediaWidth = MediaWidths[key as keyof MediaWidth];
+    return Object.keys(MediaWidthsSpec).reduce((accumulator, key) => {
+        const mediaWidth = MediaWidthsSpec[key as keyof MediaWidth];
         accumulator[
             key as keyof MediaType
         ] = `@media screen and (${type}: ${mediaWidth}px)`;
@@ -22,3 +22,5 @@ export const MediaQuery = {
     MaxWidth: getMediaQuerySpec("max-width"),
     MinWidth: getMediaQuerySpec("min-width"),
 };
+
+export const MediaWidths = MediaWidthsSpec;
