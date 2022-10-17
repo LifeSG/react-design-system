8050930baa88eae2f3958bffd72578ee2736f8d9
zhixian.teo <zhixian.teo@ufinity.com>
2022-10-17 17:04:39 +0800

    [MOL-10990][ZX|KT] correction

--- stories/form/form-textarea/doc-elements.tsx
+++ stories/form/form-textarea/doc-elements.tsx
@@ -2,15 +2,16 @@ import { Text } from "../../../src/text/text";
 import styled from "styled-components";
 import React from "react";
 
+const CustomCounterLabel = styled(Text.H6)`
+    text-align: right;
+    color: #a4a4a4 !important;
+    font-size: 14px;
+`;
+
 export const renderCustomCounter = (
     maxLength: number,
     currentValueLength: number
 ): JSX.Element => {
-    const CustomCounterLabel = styled(Text.H6)`
-        text-align: right;
-        color: #a4a4a4 !important;
-        font-size: 14px;
-    `;
     return (
         <CustomCounterLabel data-testid={"counter-label"} weight="semibold">
             {currentValueLength}/{maxLength}
