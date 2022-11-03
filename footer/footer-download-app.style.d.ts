daeb1884ff0859e2f6279525110ed35da5e1ffed
Docker Alpha <bamboo@ip-172-16-237-161.ap-southeast-1.compute.internal>
2022-11-03 09:39:44 +0800

    Built package | Version: v1.0.0-alpha.8 | Bamboo: react-design-system master #49

---
+++ footer/footer-download-app.style.d.ts
@@ -0,0 +1,6 @@
+export declare const Wrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const Label: import("styled-components").StyledComponent<"h5", any, import("../text").TextProps & {
+    as: string;
+}, "as">;
+export declare const Items: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const AppStoreLink: import("styled-components").StyledComponent<"a", any, {}, never>;
---
+++ popover/popover.styles.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+export declare type OffsetPosition = "top-center" | "top-left" | "top-right" | "left" | "right" | "none";
+interface PopoverStyleProps {
+    $visible?: boolean;
+    $offset: OffsetPosition;
+}
+export declare const Bubble: import("styled-components").StyledComponent<"div", any, PopoverStyleProps, never>;
+export declare const MobileModalBox: import("styled-components").StyledComponent<({ id, children, onClose, showCloseButton, ...otherProps }: import("..").ModalBoxProps) => JSX.Element, any, {}, never>;
+export declare const HOCWrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const HOCTrigger: import("styled-components").StyledComponent<"button", any, {}, never>;
+export {};
