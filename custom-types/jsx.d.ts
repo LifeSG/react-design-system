0d40b7bc26960ffa1f8f8e47ebe404519e53ea3b
Keith Tan <keith_xw_tan@tech.gov.sg>
2022-11-10 14:50:49 +0800

    [MOL-11639][KT] Switch out to use the sgds masthead web components instead

---
+++ custom-types/jsx.d.ts
@@ -0,0 +1,8 @@
+declare module JSX {
+    interface IntrinsicElements {
+        "sgds-masthead": React.DetailedHTMLProps<
+            React.HTMLAttributes<HTMLElement>,
+            HTMLElement
+        >;
+    }
+}
--- src/masthead/masthead.tsx
+++ src/masthead/masthead.tsx
@@ -1,14 +1,48 @@
-import "@govtechsg/sgds-masthead/dist/sgds-masthead/sgds-masthead.css";
-import { SgdsMasthead } from "@govtechsg/sgds-masthead-react";
-import styled from "styled-components";
+import { useEffect } from "react";
+import { Wrapper } from "./masthead.style";
 
-export const Masthead = (): JSX.Element => (
-    <Wrapper>
-        <SgdsMasthead />
-    </Wrapper>
-);
+export const Masthead = (): JSX.Element => {
+    // =============================================================================
+    // EFFECTS
+    // =============================================================================
+    useEffect(() => {
+        if (!doAssetsExist()) {
+            addAssets();
+        }
+    }, []);
 
-const Wrapper = styled.div`
-    background: hsl(0, 0%, 94%);
-    padding: 0 1rem;
-`;
+    // =============================================================================
+    // HELPER FUNCTIONS
+    // =============================================================================
+    const doAssetsExist = () => {
+        return document.getElementById(SCRIPT_ID);
+    };
+
+    const addAssets = () => {
+        if (!document.getElementById(SCRIPT_ID)) {
+            const script = document.createElement("script");
+            script.id = SCRIPT_ID;
+            script.type = "module";
+            script.src = SCRIPT_SRC;
+
+            document.head.appendChild(script);
+        }
+    };
+
+    // =============================================================================
+    // RENDER FUNCTIONS
+    // =============================================================================
+
+    return (
+        <Wrapper>
+            <sgds-masthead></sgds-masthead>
+        </Wrapper>
+    );
+};
+
+// =============================================================================
+// CONSTANTS
+// =============================================================================
+const SCRIPT_ID = "lifesg-ds-masthead-script";
+const SCRIPT_SRC =
+    "https://cdn.jsdelivr.net/npm/@govtechsg/sgds-web-component/Masthead/index.js";
