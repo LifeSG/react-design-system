03051c5a20e7aadbc20e232ca655ae3cb05f2b33
gnzhen <jasmine.choy@ufinity.com>
2022-11-25 17:29:03 +0800

    [MOL-11779][JC|KM] simplify transform css

--- src/smart-app-banner/smart-app-banner.styles.ts
+++ src/smart-app-banner/smart-app-banner.styles.ts
@@ -9,11 +9,11 @@ import { Text, TextStyleHelper } from "../text";
 const slideDown = keyframes`
 	from {
 		opacity: 0;
-		transform: translate3d(0, -20%, 0);
+		transform: translateY(-20%);
 	}
 	to {
 		opacity: 1;
-		transform: translate3d(0, 0, 0);
+		transform: translateY(0);
 	}
 `;
 
