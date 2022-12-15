a8ae2aff3141e1297c9632848ac104d0071189b1
Boon Xian <boonxian.tan@ufinity.com>
2022-12-15 17:31:19 +0800

    [MOL-9398][BX] added the example for custom render item

---
+++ stories/form/form-select/doc-element.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ImageWrapper = styled.div`
+    display: flex;
+    border-radius: 4px;
+    border: 1px solid;
+    overflow: hidden;
+    align-items: center;
+`;
+
+export const Image = styled.img`
+    height: 40px;
+    width: 184px;
+`;
