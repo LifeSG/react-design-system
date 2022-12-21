bac84860a7adc33bfa3cc180526b88dd4c6c5a45
GinWee <low_gin_wee@tech.gov.sg>
2022-12-21 20:57:40 +0800

    [MOL-11876][GW] Fix feedback rating stories

--- stories/feedback-rating/doc-elements.tsx
+++ stories/feedback-rating/doc-elements.tsx
@@ -6,14 +6,3 @@ import styled from "styled-components";
 export const ModalBoxDiv = styled.div`
     padding: 2rem;
 `;
-
-export const Div = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-`;
