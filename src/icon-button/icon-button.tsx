262eb55faba084f95c691ad1a986206b74bc0381
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-01-17 08:50:13 +0800

    [MISC][RL] Migrate IconButton

--- src/icon-button/icon-button.tsx
+++ src/icon-button/icon-button.tsx
@@ -1,30 +1,15 @@
 import React from "react";
 import { ButtonRef } from "../button/types";
-import { Icon } from "../icon/icon";
 import { Main } from "./icon-button.style";
 import { IconButtonProps } from "./types";
 
 const Component = (
-    { children, iconType, ...otherProps }: IconButtonProps,
+    { children, ...otherProps }: IconButtonProps,
     ref: ButtonRef
 ) => {
-    let componentToRender;
-
-    if (children) {
-        // Custom component passed in
-        componentToRender = children;
-    } else if (iconType) {
-        componentToRender = <Icon type={iconType} />;
-    } else {
-        console.error(
-            "IconButton - Did not specify a valid children or iconType prop"
-        );
-        return null;
-    }
-
     return (
         <Main ref={ref} {...otherProps}>
-            {componentToRender}
+            {children}
         </Main>
     );
 };
--- src/icon-button/types.ts
+++ src/icon-button/types.ts
@@ -1,7 +1,2 @@
-import { IconType } from "../icon/types";
-
 export interface IconButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    /** Setting this will override the children prop */
-    iconType?: IconType | undefined;
-}
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
