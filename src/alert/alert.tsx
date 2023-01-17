7e8864c8a2ba277eafd05d935ec7f0c1e8a46996
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-01-17 08:59:22 +0800

    [MISC][RL] Migrate Alert

--- src/alert/alert.tsx
+++ src/alert/alert.tsx
@@ -3,33 +3,33 @@ import { AlertProps } from "./types";
 import { ActionLinkText, ArrowIcon, Wrapper } from "./alert.style";
 
 export const Alert = ({
-	type,
-	className,
-	children,
-	actionLink,
-	...otherProps
+    type,
+    className,
+    children,
+    actionLink,
+    ...otherProps
 }: AlertProps): JSX.Element => {
-	const renderLink = () => {
-		return (
-			<ActionLinkText
-				weight="semibold"
-				{...actionLink}
-				data-testid="action-link"
-			>
-				{actionLink.children}
-				<ArrowIcon type="arrow-right" />
-			</ActionLinkText>
-		);
-	};
+    const renderLink = () => {
+        return (
+            <ActionLinkText
+                weight="semibold"
+                {...actionLink}
+                data-testid="action-link"
+            >
+                {actionLink.children}
+                <ArrowIcon />
+            </ActionLinkText>
+        );
+    };
 
-	return (
-		<Wrapper
-			className={className}
-			$type={type}
-			data-testid={otherProps["data-testid"]}
-		>
-			{children}
-			{actionLink && renderLink()}
-		</Wrapper>
-	);
+    return (
+        <Wrapper
+            className={className}
+            $type={type}
+            data-testid={otherProps["data-testid"]}
+        >
+            {children}
+            {actionLink && renderLink()}
+        </Wrapper>
+    );
 };
