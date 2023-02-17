4002d584f3d100e5d1488d2ce638b86367216243
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-02-17 17:11:03 +0800

    [TESTS][JL] Update jest config to ignore common files

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,7 @@ module.exports = {
     moduleFileExtensions: ["tsx", "ts", "js"],
     coverageDirectory: "<rootDir>/tests/coverage",
     testEnvironment: "jsdom",
-    testMatch: [
-        "<rootDir>/src/**/__tests__/**/*.(js|ts)?(x)",
-        "<rootDir>/tests/**/*.(ts)?(x)",
-    ],
+    testMatch: ["<rootDir>/tests/**/*.spec.[jt]s?(x)"],
     moduleNameMapper: {
         "react-player": "<rootDir>/src/__mocks__/fileMock.js",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
