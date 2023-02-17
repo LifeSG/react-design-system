3291b793c3fca954d6a99a758d6d74b69832c98b
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-02-17 15:16:16 +0800

    [TESTS][JL] Create common data file for unit tests

---
+++ tests/components/common/data.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_TEXT = "default text";
+export const DEFAULT_TITLE = "default title";
+
+export const DEFAULT_THEME = {
+    colorScheme: "base",
+    textStyleScheme: "base",
+    designTokenScheme: "base",
+};
