edf2ff7ddce669b04b7a70e29f749d8d851c9b84
Wilker <wilker.soh@cognizant.com>
2023-02-17 09:55:09 +0800

    [MOL-3868][WK] Edit jsdocs comment in types

--- src/calendar/types.ts
+++ src/calendar/types.ts
@@ -1,9 +1,9 @@
 export interface CalendarProps {
-    /** disabledDates: ["2023-04-30"] */
+    /** ["2023-04-30"] */
     disabledDates?: string[] | undefined;
-    /** onChange("2023-03-30") */
+    /** 2023-03-30 */
     onChange?: ((value: string) => void) | undefined;
-    /** value: 2023-03-30 */
+    /** 2023-03-30 */
     value?: string | undefined;
     type?: CalendarType | undefined;
 }
