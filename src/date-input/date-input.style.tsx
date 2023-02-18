097eb8d36a4cf4f7754cf54a9c4cc6c79b9504e5
Wilker <wilker.soh@cognizant.com>
2023-02-18 15:08:45 +0800

    [MISC][WK] Remove unused style

--- src/date-input/date-input.style.tsx
+++ src/date-input/date-input.style.tsx
@@ -1,7 +1,5 @@
 import styled, { css } from "styled-components";
 import { Color } from "../color";
-import { TextStyleHelper } from "../text/helper";
-import { Text } from "../text/text";
 
 // =============================================================================
 // STYLE INTERFACE, transient props are denoted with $
@@ -11,11 +9,6 @@ interface ContainerStyleProps {
     disabled?: boolean;
     $error?: boolean;
     $readOnly?: boolean;
-}
-interface LabelStyleProps {
-    $hide?: boolean;
-    $compress?: boolean;
-    $addGap?: boolean;
 }
 
 // =============================================================================
@@ -68,80 +61,6 @@ export const Container = styled.div<ContainerStyleProps>`
                 :focus-within {
                     border: 1px solid ${Color.Validation.Red.Border(props)};
                 }
-            `;
-        }
-    }}
-`;
-
-export const InputContainer = styled.div<ContainerStyleProps>`
-    position: absolute;
-    top: 0;
-    left: ${(props) => (props.$readOnly ? "-0.5rem" : "1rem")};
-    height: 100%;
-    display: flex;
-    align-items: center;
-`;
-
-export const BaseInput = styled.input`
-    ${TextStyleHelper.getTextStyle("Body", "regular")}
-    background: transparent;
-    height: 100%;
-    width: 2.5rem;
-    border: none;
-    text-align: center;
-    padding: 0;
-
-    // Chrome, Safari, Edge, Opera
-    ::-webkit-outer-spin-button,
-    ::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
-    // Safari (remove top shadow)
-    --webkit-appearance: none;
-
-    // Firefox
-    --moz-appearance: textfield;
-
-    :focus,
-    :active {
-        outline: none;
-    }
-
-    ${(props) => {
-        if (props.disabled) {
-            return css`
-                cursor: not-allowed;
-            `;
-        }
-    }}
-`;
-
-export const MonthInput = styled(BaseInput)`
-    width: 2.6rem;
-`;
-
-export const YearInput = styled(BaseInput)`
-    width: 3.5rem;
-`;
-
-export const Divider = styled(Text.Body)<LabelStyleProps>`
-    margin: 0.1rem 0.2rem 0 0.2rem;
-    ${(props) => {
-        if (props.$hide) {
-            return css`
-                color: ${Color.Neutral[3]};
-            `;
-        }
-    }}
-`;
-
-export const MonthDivider = styled(Divider)`
-    ${(props) => {
-        if (props.$compress) {
-            return css`
-                margin: 0.1rem 0.1rem 0 0.1rem;
             `;
         }
     }}
