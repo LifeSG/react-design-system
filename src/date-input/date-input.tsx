6c9604ab10f07b7ab9a0c2966182482b771e11c3
Wilker <wilker.soh@cognizant.com>
2023-02-20 14:30:04 +0800

    [MOL-3757][WK] Add performOnBlur and performOnChange handler

--- src/date-input/date-input.tsx
+++ src/date-input/date-input.tsx
@@ -1,6 +1,10 @@
+import dayjs from "dayjs";
+import { useState } from "react";
 import { Container } from "./date-input.style";
 import { StandAloneInput } from "./stand-alone-input";
-import { DateInputProps } from "./types";
+import { ChangeValueTypes, DateInputProps } from "./types";
+
+export type InputType = "start" | "end";
 
 export const DateInput = ({
     disabled,
@@ -17,6 +21,7 @@ export const DateInput = ({
     // =============================================================================
     // CONST, STATE, REF
     // =============================================================================
+    const [inputValues, setInputValues] = useState<ChangeValueTypes>({});
 
     // =============================================================================
     // REF FUNCTIONS
@@ -29,10 +34,75 @@ export const DateInput = ({
     // =============================================================================
     // EVENT HANDLERS
     // =============================================================================
+    const handleChange = (value: ChangeValueTypes) => {
+        const updatedValue = { ...inputValues, ...value };
+
+        setInputValues(updatedValue);
+        performOnChangeHandler(updatedValue);
+    };
+
+    const handleBlur = () => {
+        performOnBlurHandler();
+    };
 
     // =============================================================================
     // HELPER FUNCTIONS
     // =============================================================================
+    const performOnChangeHandler = (values: ChangeValueTypes) => {
+        if (onChange) {
+            const returnValue = getFormattedValue(values);
+
+            onChange(returnValue);
+        }
+
+        if (onChangeRaw) {
+            onChangeRaw(values);
+        }
+    };
+
+    const performOnBlurHandler = () => {
+        if (onBlur) {
+            const returnValue = getFormattedValue(inputValues);
+
+            onBlur(returnValue);
+        }
+
+        if (onBlurRaw) {
+            onBlurRaw(inputValues);
+        }
+    };
+
+    const getFormattedValue = (values: ChangeValueTypes) => {
+        const result = Object.keys(values).reduce((_, cur) => {
+            const valueArr = Object.values(values[cur]) as any;
+
+            if (
+                valueArr[0].length === 4 &&
+                valueArr[1].length >= 1 &&
+                valueArr[2].length >= 1
+            ) {
+                const formatted = dayjs(valueArr.join("-")).format(
+                    "DD-MM-YYYY"
+                );
+
+                /**
+                    Buggy (do it once calendar rebase)
+                    user type 10 / 20 / 2023 ->
+                    dayjs transform to 10 / 08 / 2024
+                    issue the StandAlone handle itself for this
+                    so the return value to user and on view are different
+                 */
+
+                return { [cur]: formatted };
+            } else if (valueArr.every((value) => value === "")) {
+                return { [cur]: "" };
+            } else {
+                return { [cur]: INVALID_VALUE };
+            }
+        }, {});
+
+        return result;
+    };
 
     // =============================================================================
     // RENDER FUNCTION
@@ -44,13 +114,11 @@ export const DateInput = ({
             id={id}
             data-testid={otherProps["data-testid"]}
             $readOnly={readOnly}
+            onBlur={handleBlur}
         >
             <StandAloneInput
                 disabled={disabled}
-                onBlur={onBlur}
-                onBlurRaw={onBlurRaw}
-                onChange={onChange}
-                onChangeRaw={onChangeRaw}
+                onChange={handleChange}
                 readOnly={readOnly}
                 names={["start-day", "start-month", "start-year"]}
                 value={value}
@@ -58,3 +126,8 @@ export const DateInput = ({
         </Container>
     );
 };
+
+// =============================================================================
+// CONSTANTS
+// =============================================================================
+const INVALID_VALUE = "Invalid Date";
--- src/date-input/types.ts
+++ src/date-input/types.ts
@@ -12,16 +12,32 @@ export interface DateInputProps extends React.AriaAttributes {
     value?: string | undefined;
     error?: boolean | undefined;
     disabled?: boolean | undefined;
-    onChange?: ((value: string) => void) | undefined;
+    onChange?: ((value: ChangeValueTypes) => void) | undefined;
     /**
      * Function that returns the raw values in the DateInput on change in an array format
      * as such [day, month, year]
      */
-    onChangeRaw?: ((value: string[]) => void) | undefined;
-    onBlur?: ((value: string) => void) | undefined;
+    onChangeRaw?: ((value: ChangeValueTypes) => void) | undefined;
+    onBlur?: ((value: ChangeValueTypes) => void) | undefined;
     /**
      * Function that returns the raw values in the DateInput on blur in an array format
      * as such [day, month, year]
      */
-    onBlurRaw?: ((value: string[]) => void) | undefined;
+    onBlurRaw?: ((value: ChangeValueTypes) => void) | undefined;
+}
+
+export interface ChangeValueTypes {
+    start?: StartChangeValue;
+    end?: EndChangeValue;
+}
+
+interface StartChangeValue {
+    "start-day": string;
+    "start-month": string;
+    "start-year": string;
+}
+interface EndChangeValue {
+    "end-day": string;
+    "end-month": string;
+    "end-year": string;
 }
