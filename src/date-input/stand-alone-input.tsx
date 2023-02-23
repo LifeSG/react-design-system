0629fd5fed0892f93776a72d5a67c6d587ff75e7
Wilker <wilker.soh@cognizant.com>
2023-02-23 13:46:11 +0800

    [MOL-3934][WK] Add overlay placeholder

--- src/date-input/stand-alone-input.tsx
+++ src/date-input/stand-alone-input.tsx
@@ -5,7 +5,9 @@ import {
     DayInput,
     Divider,
     InputContainer,
+    InputSection,
     MonthInput,
+    Placeholder,
     VariantStyleProps,
     YearInput,
 } from "./stand-alone-input.style";
@@ -44,6 +46,8 @@ export const StandAloneInput = ({
     const [monthValue, _setMonthValue] = useState<string>("");
     const [yearValue, _setYearValue] = useState<string>("");
     const [currentFocus, _setCurrentFocus] = useState<FieldType>("none");
+
+    const [isDirty, setIsDirty] = useState<boolean>(false);
 
     const nodeRef = useRef<HTMLDivElement>(null);
     const dayInputRef = useRef<HTMLInputElement>(null);
@@ -136,6 +140,7 @@ export const StandAloneInput = ({
     // =============================================================================
     // EVENT HANDLERS
     // =============================================================================
+
     const handleMouseDown = (event: MouseEvent) => {
         if (disabled || readOnly) {
             return;
@@ -149,6 +154,13 @@ export const StandAloneInput = ({
         // outside click
         if (currentFocusStateRef.current !== "none") {
             setCurrentFocus("none");
+        }
+    };
+
+    const handleNodeClick = () => {
+        console.log(currentFocus);
+        if (currentFocus === "none" && dayInputRef.current) {
+            dayInputRef.current.focus();
         }
     };
 
@@ -239,6 +251,10 @@ export const StandAloneInput = ({
         }
     };
 
+    const handleClickPlaceholder = () => {
+        setIsDirty(true);
+    };
+
     // =============================================================================
     // HELPER FUNCTIONS
     // =============================================================================
@@ -286,73 +302,102 @@ export const StandAloneInput = ({
         onChange(returnValue);
     };
 
+    // =============================================================================
+    // RENDER FUNCTIONS
+    // =============================================================================
+    const RenderPlaceholder = () => {
+        if (value) return;
+
+        switch (variant) {
+            case "start":
+            case "range":
+                return (
+                    <Placeholder
+                        $isDirty={isDirty}
+                        onClick={handleClickPlaceholder}
+                    >
+                        {variant === "start" ? "From" : "To"}
+                    </Placeholder>
+                );
+            default:
+                return;
+        }
+    };
+
     return (
-        <InputContainer
-            ref={nodeRef}
+        <InputSection
             $readOnly={readOnly}
             $variant={variant}
+            onClick={handleNodeClick}
             {...otherProps}
         >
-            <DayInput
-                name={names[0]}
-                maxLength={2}
-                value={dayValue}
-                ref={dayInputRef}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                disabled={disabled}
-                type="text"
-                inputMode="numeric"
-                pattern="[0-9]{2}"
-                data-testid={`${names[0]}-input`}
-                aria-label={`${names[0]}-input`}
-                readOnly={readOnly}
-                autoComplete={"off"}
-                placeholder={currentFocus === names[0] && !readOnly ? "" : "DD"}
-            />
-            <Divider $hide={dayValue.length === 0}>/</Divider>
-            <MonthInput
-                name={names[1]}
-                maxLength={2}
-                value={monthValue}
-                ref={monthInputRef}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                disabled={disabled}
-                type="text"
-                inputMode="numeric"
-                pattern="[0-9]{2}"
-                data-testid={`${names[1]}-input`}
-                aria-label={`${names[1]}-input`}
-                readOnly={readOnly}
-                autoComplete={"off"}
-                placeholder={currentFocus === names[1] && !readOnly ? "" : "MM"}
-            />
-            <Divider $hide={monthValue.length === 0}>/</Divider>
-            <YearInput
-                name={names[2]}
-                maxLength={4}
-                value={yearValue}
-                ref={yearInputRef}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                disabled={disabled}
-                type="text"
-                inputMode="numeric"
-                pattern="[0-9]{4}"
-                data-testid={`${names[2]}-input`}
-                aria-label={`${names[2]}-input`}
-                readOnly={readOnly}
-                autoComplete={"off"}
-                placeholder={
-                    currentFocus === names[2] && !readOnly ? "" : "YYYY"
-                }
-            />
-        </InputContainer>
+            <InputContainer ref={nodeRef}>
+                <DayInput
+                    name={names[0]}
+                    maxLength={2}
+                    value={dayValue}
+                    ref={dayInputRef}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    disabled={disabled}
+                    type="text"
+                    inputMode="numeric"
+                    pattern="[0-9]{2}"
+                    data-testid={`${names[0]}-input`}
+                    aria-label={`${names[0]}-input`}
+                    readOnly={readOnly}
+                    autoComplete={"off"}
+                    placeholder={
+                        currentFocus === names[0] && !readOnly ? "" : "DD"
+                    }
+                />
+                <Divider $hide={dayValue.length === 0}>/</Divider>
+                <MonthInput
+                    name={names[1]}
+                    maxLength={2}
+                    value={monthValue}
+                    ref={monthInputRef}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    disabled={disabled}
+                    type="text"
+                    inputMode="numeric"
+                    pattern="[0-9]{2}"
+                    data-testid={`${names[1]}-input`}
+                    aria-label={`${names[1]}-input`}
+                    readOnly={readOnly}
+                    autoComplete={"off"}
+                    placeholder={
+                        currentFocus === names[1] && !readOnly ? "" : "MM"
+                    }
+                />
+                <Divider $hide={monthValue.length === 0}>/</Divider>
+                <YearInput
+                    name={names[2]}
+                    maxLength={4}
+                    value={yearValue}
+                    ref={yearInputRef}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    disabled={disabled}
+                    type="text"
+                    inputMode="numeric"
+                    pattern="[0-9]{4}"
+                    data-testid={`${names[2]}-input`}
+                    aria-label={`${names[2]}-input`}
+                    readOnly={readOnly}
+                    autoComplete={"off"}
+                    placeholder={
+                        currentFocus === names[2] && !readOnly ? "" : "YYYY"
+                    }
+                />
+            </InputContainer>
+            {RenderPlaceholder()}
+        </InputSection>
     );
 };
