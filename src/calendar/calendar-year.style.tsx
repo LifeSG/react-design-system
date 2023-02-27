4b076233da61ed3eb0a15e9a2b786c4c462ebb42
Wilker <wilker.soh@cognizant.com>
2023-02-27 12:04:39 +0800

    [MOL-3943][WK] Style layout of input calendar

--- src/calendar/calendar-year.style.tsx
+++ src/calendar/calendar-year.style.tsx
@@ -19,18 +19,19 @@ interface WrapperStyleProps {
 export const Wrapper = styled.div<WrapperStyleProps>`
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(4, 4rem);
     align-content: center;
+    grid-template-columns: repeat(3, 1fr);
 
     ${(props) => {
         switch (props.$type) {
             case "standalone":
                 return css`
+                    grid-template-rows: repeat(4, 4rem);
                     gap: 0.5rem 2rem;
                 `;
             case "input":
                 return css`
+                    grid-template-rows: repeat(4, 4.375rem);
                     gap: 0.5rem 1rem;
                 `;
         }
