fadd98012fccce56f957501702bf24e990162415
Mohammad Afiq Bin Shahidan <mohammadafiq.binshahidan@cognizant.com>
2023-03-01 14:08:01 +0800

    [BOOKINGSG-3980][AS] BookingSG new loading animations

---
+++ src/animations/loading-dots-spinner/loading-dots-spinner.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import { LottieLoadingDotsSpinner } from "./lottie-animation";
+
+export const LoadingDotsSpinner = () => {
+    return (
+        <Container>
+            <LottieLoadingDotsSpinner />
+        </Container>
+    );
+};
+
+// =============================================================================
+// STYLING
+// =============================================================================
+const Container = styled.div`
+    margin: 0 auto;
+`;
