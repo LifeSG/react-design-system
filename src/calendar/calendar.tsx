b1b3877ecc50f82eba8a86898edd814e5bb760be
Wilker <wilker.soh@cognizant.com>
2023-03-01 13:24:05 +0800

    [MOL-3934][WK] Feature hover value for date input

--- src/calendar/calendar.tsx
+++ src/calendar/calendar.tsx
@@ -1,5 +1,5 @@
 import dayjs, { Dayjs } from "dayjs";
-import { useEffect, useState } from "react";
+import React, { useEffect, useImperativeHandle, useRef, useState } from "react";
 import { CalendarHelper } from "../util/calendar-helper";
 import { CalendarDay } from "./calendar-day";
 import { CalendarMonth } from "./calendar-month";
@@ -24,35 +24,110 @@ import {
     SideArrowButton,
     ToggleZone,
 } from "./calendar.style";
-import { CalendarProps } from "./types";
+import { CalendarAction, CalendarProps, CalendarRef, View } from "./types";
 
-export type View = "default" | "month-options" | "year-options";
-
-export const Calendar = ({
-    disabledDates,
-    onSelect,
-    isOpen,
-    value,
-    withButton,
-    type = "standalone",
-    ...otherProps
-}: CalendarProps) => {
+export const Component = (
+    {
+        disabledDates,
+        onCalendarView,
+        onSelect,
+        onHover,
+        onWithButton,
+        actionComponent,
+        isOpen,
+        value,
+        endValue,
+        currentFocus,
+        currentType,
+        withButton,
+        variant,
+        between,
+        type = "standalone",
+        ...otherProps
+    }: CalendarProps,
+    ref: React.ForwardedRef<CalendarRef>
+) => {
     // =============================================================================
     // CONST, STATE, REF
     // =============================================================================
     const [calendarDate, setCalendarDate] = useState<Dayjs>(dayjs());
     const [currentView, setCurrentView] = useState<View>("default");
-    const [selectedStartDate, setSelectedStartDate] = useState<string>(""); // YYYY-MM-DD
+    const [selectedStartDate, setSelectedStartDate] = useState<string>(); // YYYY-MM-DD
+    const [selectedEndDate, setSelectedEndDate] = useState<string>(); // YYYY-MM-DD
+    const [isNewSelection, setIsNewSelection] = useState<boolean>(true);
+
+    const doneButtonRef = useRef<HTMLButtonElement>(null);
+    const cancelButtonRef = useRef<HTMLButtonElement>(null);
+
+    // =============================================================================
+    // HOOKS
+    // =============================================================================
+    useImperativeHandle(
+        ref,
+        (): any => {
+            return {
+                defaultView() {
+                    setCurrentView("default");
+                    onCalendarView("default");
+                },
+            };
+        },
+        []
+    );
 
     // =============================================================================
     // EFFECTS
     // =============================================================================
     useEffect(() => {
-        if (!value) return;
+        // open with 'confirmed' value for day calendar in first mounted
+        if (!isOpen) return;
+
+        const initCalendar = currentFocus === "end" ? endValue : value;
+
+        setCalendarDate(dayjs(initCalendar));
+        setCurrentView("default");
+
+        if (variant === "range" && value?.length && endValue?.length) {
+            setIsNewSelection(false);
+        }
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (!currentType) return;
+
+        // go back selected day calendar
+        handleSelectedDayView();
+    }, [currentType]);
+
+    useEffect(() => {
+        setIsNewSelection(true);
+
+        if (!value) {
+            setSelectedStartDate(undefined);
+            return;
+        }
+
+        if (actionComponent === "input") {
+            setCalendarDate(dayjs(value));
+        }
 
         setSelectedStartDate(value);
-        setCalendarDate(dayjs(value));
     }, [value]);
+
+    useEffect(() => {
+        setIsNewSelection(true);
+
+        if (!endValue) {
+            setSelectedEndDate(undefined);
+            return;
+        }
+
+        if (actionComponent === "input") {
+            setCalendarDate(dayjs(endValue));
+        }
+
+        setSelectedEndDate(endValue);
+    }, [endValue]);
 
     // =============================================================================
     // EVENT HANDLERS
@@ -85,22 +160,64 @@ export const Calendar = ({
         }
     };
 
+    function handleKeyDown(
+        event: React.KeyboardEvent<HTMLButtonElement>,
+        isDoneDisabled: boolean
+    ) {
+        const target = event.target as HTMLButtonElement;
+
+        if (
+            cancelButtonRef.current.contains(target) &&
+            event.code === "Tab" &&
+            isDoneDisabled
+        ) {
+            // 'Tab' away from cancel button if done button is disabled
+            performOnWithButtonHandler("reset");
+        } else if (
+            doneButtonRef.current.contains(target) &&
+            event.code === "Tab" &&
+            !isDoneDisabled
+        ) {
+            // 'Tab' away from done button if done button is active
+            performOnWithButtonHandler("reset");
+        }
+    }
+
     const handleDateSelect = (value: Dayjs) => {
         const stringValue = value.format("YYYY-MM-DD");
 
         setCalendarDate(value);
-
-        setSelectedStartDate(stringValue);
+        handleSelectedType(stringValue);
 
         performOnSelectHandler(stringValue);
     };
 
     const handleCancelButton = () => {
-        // close calendar and use confirm value if exist
+        // close calendar and use 'confirmed' value if exist
+        performOnWithButtonHandler("reset");
     };
 
-    const handleDoneButton = () => {
-        // close calendar and confirm the value
+    const handleDoneButton = (isDisabled: boolean) => {
+        if (isDisabled) return;
+        // close calendar and 'confirmed' the value
+        performOnWithButtonHandler("confirmed");
+    };
+
+    const handleHover = (value: string) => {
+        performOnHoverHandler(value);
+    };
+
+    const handleSelectedDayView = () => {
+        if (currentType !== "restore") return;
+
+        switch (currentFocus) {
+            case "start":
+                setCalendarDate(dayjs(value));
+                break;
+            case "end":
+                setCalendarDate(dayjs(endValue));
+                break;
+        }
     };
 
     // =============================================================================
@@ -112,11 +229,54 @@ export const Calendar = ({
         }
     };
 
+    const performOnHoverHandler = (value: string) => {
+        if (onHover) {
+            onHover(value);
+        }
+    };
+
+    const performOnWithButtonHandler = (action: CalendarAction) => {
+        if (onWithButton) {
+            onWithButton(action);
+        }
+    };
+
+    const handleSelectedType = (value: string) => {
+        if (!currentFocus) {
+            // standalone calendar
+            setSelectedStartDate(value);
+
+            return;
+        }
+
+        switch (currentFocus) {
+            case "start":
+                setSelectedStartDate(value);
+                break;
+            case "end":
+                setSelectedEndDate(value);
+                break;
+            default:
+                setSelectedStartDate(value);
+        }
+    };
+
     const handleMonthDropdownClick = () => {
         if (currentView !== "month-options") {
+            const targetValue =
+                currentFocus === "start" || !currentFocus
+                    ? selectedStartDate
+                    : selectedEndDate;
+
+            /**
+             * Buggy here if swap to next the value didn't render to the focus type
+             */
+            setCalendarDate(dayjs(targetValue));
             setCurrentView("month-options");
+            performOnCalendarView("month-options");
         } else {
             setCurrentView("default");
+            performOnCalendarView("default");
         }
     };
 
@@ -128,8 +288,16 @@ export const Calendar = ({
          */
         if (currentView !== "default") {
             setCurrentView("default");
+            performOnCalendarView("default");
         } else {
+            const targetValue =
+                currentFocus === "start" || !currentFocus
+                    ? selectedStartDate
+                    : selectedEndDate;
+
+            setCalendarDate(dayjs(targetValue));
             setCurrentView("year-options");
+            performOnCalendarView("year-options");
         }
     };
 
@@ -144,14 +312,21 @@ export const Calendar = ({
         }
     };
 
+    const performOnCalendarView = (view: View) => {
+        if (onCalendarView) {
+            onCalendarView(view);
+        }
+    };
+
     // =============================================================================
     // RENDER FUNCTIONS
     // =============================================================================
-    const _renderDropdownButtons = () => {
+    const renderDropdownButtons = () => {
         return (
             <>
                 <DropdownButton
                     type="button"
+                    tabIndex={-1}
                     $type={type}
                     $expandedDisplay={currentView === "month-options"}
                     $visible={currentView === "default"}
@@ -165,6 +340,7 @@ export const Calendar = ({
                 </DropdownButton>
                 <DropdownButton
                     type="button"
+                    tabIndex={-1}
                     $type={type}
                     $expandedDisplay={currentView !== "default"}
                     id="year-dropdown"
@@ -186,7 +362,11 @@ export const Calendar = ({
                     <CalendarMonth
                         type={type}
                         calendarDate={calendarDate}
+                        currentFocus={currentFocus}
                         selectedStartDate={selectedStartDate}
+                        selectedEndDate={selectedEndDate}
+                        between={between}
+                        isNewSelection={isNewSelection}
                         onSelect={handleDateSelect}
                     />
                 );
@@ -195,7 +375,11 @@ export const Calendar = ({
                     <CalendarYear
                         type={type}
                         calendarDate={calendarDate}
+                        currentFocus={currentFocus}
                         selectedStartDate={selectedStartDate}
+                        selectedEndDate={selectedEndDate}
+                        between={between}
+                        isNewSelection={isNewSelection}
                         onSelect={handleDateSelect}
                     />
                 );
@@ -208,19 +392,25 @@ export const Calendar = ({
         switch (type) {
             case "standalone":
                 return (
-                    <HeaderDropdown>{_renderDropdownButtons()}</HeaderDropdown>
+                    <HeaderDropdown>{renderDropdownButtons()}</HeaderDropdown>
                 );
             case "input":
                 return (
                     <Header>
                         <HeaderInputDropdown>
-                            {_renderDropdownButtons()}
+                            {renderDropdownButtons()}
                         </HeaderInputDropdown>
                         <HeaderArrows>
-                            <HeaderArrowButton onClick={handleLeftArrowClick}>
+                            <HeaderArrowButton
+                                tabIndex={-1}
+                                onClick={handleLeftArrowClick}
+                            >
                                 <ArrowLeft />
                             </HeaderArrowButton>
-                            <HeaderArrowButton onClick={handleRightArrowClick}>
+                            <HeaderArrowButton
+                                tabIndex={-1}
+                                onClick={handleRightArrowClick}
+                            >
                                 <ArrowRight />
                             </HeaderArrowButton>
                         </HeaderArrows>
@@ -228,7 +418,7 @@ export const Calendar = ({
                 );
             default:
                 return (
-                    <HeaderDropdown>{_renderDropdownButtons()}</HeaderDropdown>
+                    <HeaderDropdown>{renderDropdownButtons()}</HeaderDropdown>
                 );
         }
     };
@@ -236,16 +426,45 @@ export const Calendar = ({
     const renderCancelDoneButton = () => {
         if (type === "standalone" || !withButton) return;
 
+        let isDisabled = true;
+        const isDayView = ["default"].includes(currentView);
+
+        switch (variant) {
+            case "single":
+                isDisabled = selectedStartDate ? false : true;
+                break;
+            case "range":
+                isDisabled = !selectedStartDate || !selectedEndDate;
+                break;
+        }
+
+        const disabled = !isDayView ? false : isDisabled;
+
         return (
             <ActionButtonSection>
-                <CancelButton onClick={handleCancelButton}>Cancel</CancelButton>
-                <DoneButton onClick={handleDoneButton}>Done</DoneButton>
+                <CancelButton
+                    styleType="light"
+                    ref={cancelButtonRef}
+                    onClick={handleCancelButton}
+                    onKeyDown={(event) => handleKeyDown(event, disabled)}
+                >
+                    Cancel
+                </CancelButton>
+                <DoneButton
+                    ref={doneButtonRef}
+                    onClick={() => handleDoneButton(disabled)}
+                    onKeyDown={(event) => handleKeyDown(event, disabled)}
+                    disabled={disabled}
+                >
+                    Done
+                </DoneButton>
             </ActionButtonSection>
         );
     };
 
-    // handle the transition
-    if (type === "input" && !isOpen) return <Container $type={type} />;
+    // handle the animation transition
+    if (type === "input" && !isOpen)
+        return <Container $type={type} tabIndex={-1} />;
 
     return (
         <Container $type={type} $isOpen={isOpen} {...otherProps}>
@@ -262,9 +481,15 @@ export const Calendar = ({
                     <CalendarDay
                         type={type}
                         calendarDate={calendarDate}
+                        currentFocus={currentFocus}
                         disabledDates={disabledDates}
                         selectedStartDate={selectedStartDate}
+                        selectedEndDate={selectedEndDate}
+                        variant={variant}
+                        between={between}
+                        isNewSelection={isNewSelection}
                         onSelect={handleDateSelect}
+                        onHover={handleHover}
                     />
                     <OptionsOverlay $visible={currentView !== "default"}>
                         {renderOptionsOverlay()}
@@ -282,3 +507,5 @@ export const Calendar = ({
         </Container>
     );
 };
+
+export const Calendar = React.forwardRef(Component);
--- src/calendar/types.ts
+++ src/calendar/types.ts
@@ -2,15 +2,24 @@ export interface CalendarProps {
     /** Dates to disable in `YYYY-MM-DD` format. Example: `["2023-04-30"]` */
     disabledDates?: string[] | undefined;
     /** Called when date is selected, returns value in `YYYY-MM-DD` format or `"Invalid Date"` */
-    onSelect?: ((value: string) => void) | undefined;
+    onSelect?: ((value: string, from?: "calendar") => void) | undefined;
+    /** Called when day cell is hover, returns value in `YYYY-MM-DD` */
+    onHover?: ((value: string) => void) | undefined;
     /** Use in input with calendar. */
     isOpen?: boolean | undefined;
-    /** Selected date in `YYYY-MM-DD` format */
+    /** Selected start date in `YYYY-MM-DD` format */
     value?: string | undefined;
+    /** Selected end date in `YYYY-MM-DD` format */
+    endValue?: string | undefined;
     /** The display type of the component. Values `standalone` | `input` */
     type?: CalendarType | undefined;
+
+    // Integrate with date-input component
     /** The display cancel/done button inside component.*/
     withButton?: boolean | undefined;
+    /** Coloring in multiple date selection. */
+    currentFocus?: "start" | "end" | "none" | undefined;
+
     // Basic component props
     /** The class selector */
     className?: string | undefined;
