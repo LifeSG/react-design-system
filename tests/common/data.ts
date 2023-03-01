c1464203d00344eec0ae4cf30d92b75fd25d4d18
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-01 15:38:52 +0800

    [TESTS][JL] Create unit tests for breadcrumbs

--- tests/common/data.ts
+++ tests/common/data.ts
@@ -6,3 +6,6 @@ export const DEFAULT_THEME = {
     textStyleScheme: "base",
     designTokenScheme: "base",
 };
+
+export const LOREM_IPSUM =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies, elit vel aliquet ultrices, mauris nisl porttitor erat, fermentum sodales eros ipsum eget velit. Nam venenatis nibh ut elementum gravida. Aliquam dignissim aliquet neque, id aliquet mi. Nunc sed vestibulum orci. Proin feugiat nulla nunc, in cursus purus ullamcorper sed. Suspendisse aliquet felis nisl, nec dapibus felis finibus non. Pellentesque vel mattis velit. Integer consectetur, ex non eleifend gravida, sapien massa laoreet nulla, interdum sagittis purus augue eu nisl. Proin tincidunt, nunc a elementum molestie, augue turpis lobortis nulla, sit amet feugiat ante velit at magna. Proin nec molestie justo. Aenean maximus erat sed viverra pulvinar. Nulla metus ex, aliquam et posuere et, varius a lacus. Suspendisse consectetur lacus quis justo iaculis consequat.";
