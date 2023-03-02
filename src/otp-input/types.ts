75874c9c4c0ddc0df486f46004266e9547100ca4
Keith Tan <keith_xw_tan@tech.gov.sg>
2023-03-02 16:58:46 +0800

    [BOOKING-3979][KT] Add inline documentation

--- src/otp-input/types.ts
+++ src/otp-input/types.ts
@@ -4,10 +4,15 @@ export interface OtpInputProps extends React.AriaAttributes {
     id?: string | undefined;
     "data-testid"?: string | undefined;
     className?: string | undefined;
+    /** The combined value of the Otp inputs */
     value?: string | undefined;
     errorMessage?: string | undefined;
+    /** The duration to disable the submit button after a click is made */
     cooldownDuration: number;
+    /** The props for the submit button */
     actionButtonProps: ButtonProps;
+    /** The number of characters for the Otp */
     numOfInput: number;
+    /** Called when one of the input is changed. Returns the combined value of all Otp inputs */
     onChange?: ((value: string) => void) | undefined;
 }
