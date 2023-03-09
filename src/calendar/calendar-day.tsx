0bffcadf58f07f30f13fad0f16d128f2576b7536
Wilker <wilker.soh@cognizant.com>
2023-03-09 16:38:53 +0800

    [MOL-3934][WK] Styles range

--- src/calendar/calendar-day.tsx
+++ src/calendar/calendar-day.tsx
@@ -1,24 +1,30 @@
 import dayjs, { Dayjs } from "dayjs";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { DateInputVariant } from "../date-input";
 import { Text } from "../text/text";
 import { CalendarHelper } from "../util/calendar-helper";
 import {
-    DayContainer,
     DayLabel,
     GrowDayCell,
     HeaderCell,
     InteractiveCircle,
     OverflowDisplay,
+    RowDayCell,
     Wrapper,
 } from "./calendar-day.style";
-import { CalendarProps, CalendarType } from "./types";
+import { CalendarProps, CalendarType, FocusType } from "./types";
+import isBetween from "dayjs/plugin/isBetween";
+
+dayjs.extend(isBetween);
 
 export type DayVariant = "default" | "other-month" | "today";
 
-interface CalendarDayProps extends Pick<CalendarProps, "disabledDates"> {
+interface CalendarDayProps
+    extends Pick<CalendarProps, "disabledDates" | "variant"> {
     selectedStartDate: string;
     selectedEndDate: string;
     calendarDate: Dayjs;
+    currentFocus?: FocusType | undefined;
     onSelect: (value: Dayjs) => void;
     onHover: (value: string) => void;
     type: CalendarType;
@@ -26,12 +32,14 @@ interface CalendarDayProps extends Pick<CalendarProps, "disabledDates"> {
 
 export const CalendarDay = ({
     calendarDate,
+    currentFocus,
     disabledDates,
     selectedStartDate,
     selectedEndDate,
     onSelect,
     onHover,
     type,
+    variant,
 }: CalendarDayProps) => {
     // =============================================================================
     // CONST, STATE, REF
@@ -40,6 +48,8 @@ export const CalendarDay = ({
         (): Dayjs[][] => CalendarHelper.generateDays(calendarDate),
         [calendarDate]
     );
+    const [hoverValue, setHoverValue] = useState<string>("");
+
     // =============================================================================
     // EVENT HANDLERS
     // =============================================================================
@@ -50,13 +60,14 @@ export const CalendarDay = ({
     const handleHoverCell = (value: string) => {
         if (type !== "input") return;
 
+        setHoverValue(value);
         onHover(value);
     };
 
     const handleMouseLeaveCell = () => {
         if (type !== "input") return;
 
-        // update displayValues action
+        setHoverValue("");
         onHover("");
     };
 
@@ -64,12 +75,6 @@ export const CalendarDay = ({
     // HELPER FUNCTIONS
     // =============================================================================
     const generateDayStatus = (day: Dayjs) => {
-        const dateStartWithYear = day.format("YYYY-MM-DD");
-        let isDisabled = false;
-
-        if (disabledDates && disabledDates.length) {
-            isDisabled = disabledDates.includes(dateStartWithYear);
-        }
         const variant: DayVariant =
             calendarDate.month() !== day.month()
                 ? "other-month"
@@ -78,8 +83,265 @@ export const CalendarDay = ({
                 : "default";
 
         return {
-            isDisabled,
             variant,
+        };
+    };
+
+    const generateStyleProps = (day: Dayjs) => {
+        const dateStartWithYear = day.format("YYYY-MM-DD");
+        const a = day.format("YYYY-MM-DD");
+
+        const styleLeftProps = {},
+            styleRightProps = {},
+            styleCircleProps = {},
+            styleLabelProps = {},
+            styleDayCellProps = {};
+
+        if (disabledDates && disabledDates.length) {
+            const isDisabled = disabledDates.includes(dateStartWithYear);
+            styleCircleProps["$disabled"] = isDisabled;
+            styleLabelProps["$disabled"] = isDisabled;
+        }
+
+        if ([selectedStartDate, selectedEndDate].includes(dateStartWithYear)) {
+            styleCircleProps["$selectedDate"] = true;
+            styleLabelProps["$selectedDate"] = true;
+        }
+
+        if (variant === "range" && currentFocus === "start") {
+            // disabled before selected end date in current focus in 'start'
+            if (day.isAfter(selectedEndDate) && selectedEndDate !== undefined) {
+                styleCircleProps["$disabled"] = true;
+                styleLabelProps["$disabled"] = true;
+            }
+        } else if (variant === "range" && currentFocus === "end") {
+            // disabled before selected start date in current focus in 'end'
+            if (
+                day.isBefore(selectedStartDate) &&
+                selectedStartDate !== undefined
+            ) {
+                styleCircleProps["$disabled"] = true;
+                styleLabelProps["$disabled"] = true;
+            }
+
+            // exist start date and hover value
+            // get 'between' cell between start value and hover value
+            if (day.isBetween(selectedStartDate, hoverValue, "day", "()")) {
+                styleDayCellProps["$point"] = "middle";
+                styleDayCellProps["$hovered"] = true;
+
+                styleLabelProps["$hovered"] = true;
+                styleLabelProps["$point"] = "end";
+                styleLabelProps["$selected"] = false;
+            }
+
+            // exist start date and hover value
+            // get selected start cell during hovering
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                day.isSame(selectedStartDate) &&
+                hoverValue !== selectedStartDate
+            ) {
+                styleRightProps["$hovered"] = true;
+                styleRightProps["$point"] = "end";
+            }
+
+            // exist selected start and hovering
+            // get hover end cell
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                day.isSame(hoverValue) &&
+                hoverValue !== selectedStartDate
+            ) {
+                styleLeftProps["$hovered"] = true;
+                styleLeftProps["$point"] = "end";
+
+                styleLabelProps["$hovered"] = true;
+                styleLabelProps["$point"] = "end";
+                styleLabelProps["$selected"] = false;
+            }
+
+            // exist selected start and hovering selected start
+            // get selected start
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                day.isSame(selectedStartDate) &&
+                hoverValue === selectedStartDate
+            ) {
+                styleCircleProps["$hovered"] = true;
+                styleCircleProps["$point"] = "start";
+                styleCircleProps["$selected"] = true;
+            }
+
+            // exist selected start and end cell without hover
+            // get selected cell beween start & end
+            if (
+                selectedStartDate &&
+                selectedEndDate &&
+                !hoverValue.length &&
+                day.isBetween(selectedStartDate, selectedEndDate, "day", "()")
+            ) {
+                styleDayCellProps["$point"] = "middle";
+                styleDayCellProps["$selected"] = true;
+            }
+
+            // exist selected start and end
+            if (selectedStartDate && selectedEndDate) {
+                // get between
+                if (
+                    day.isBetween(
+                        selectedStartDate,
+                        selectedEndDate,
+                        "day",
+                        "()"
+                    )
+                ) {
+                    styleDayCellProps["$hovered"] = false;
+                    styleDayCellProps["$point"] = "middle";
+                    styleDayCellProps["$selected"] = true;
+
+                    styleLabelProps["$hovered"] = false;
+                    styleLabelProps["$point"] = "none";
+                    styleLabelProps["$selected"] = true;
+                }
+
+                if (day.isSame(selectedStartDate)) {
+                    // get start
+                    styleRightProps["$hovered"] = false;
+                    styleRightProps["$point"] = "none";
+                    styleRightProps["$selected"] = true;
+                } else if (day.isSame(selectedEndDate)) {
+                    // get end
+                    styleLeftProps["$hovered"] = false;
+                    styleLeftProps["$point"] = "none";
+                    styleLeftProps["$selected"] = true;
+                }
+            }
+
+            // exist selected start and end, hover after end
+            // get end cell
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                selectedEndDate &&
+                dayjs(hoverValue).isAfter(selectedEndDate) &&
+                day.isBetween(selectedEndDate, hoverValue, "day", "[)") &&
+                day.isSame(selectedEndDate)
+            ) {
+                styleLeftProps["$hovered"] = false;
+                styleLeftProps["$selected"] = true;
+                styleLeftProps["$point"] = "after-end";
+
+                styleRightProps["$hovered"] = true;
+                styleRightProps["$selected"] = true;
+                styleRightProps["$point"] = "after-end";
+
+                styleDayCellProps["$hovered"] = true;
+                styleDayCellProps["$point"] = "after-end";
+                styleDayCellProps["$selected"] = false;
+            }
+
+            // exist selected start and end, hover after end
+            // get selected start to end
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                selectedEndDate &&
+                dayjs(hoverValue).isAfter(selectedEndDate) &&
+                day.isBetween(selectedStartDate, selectedEndDate, "day", "[]")
+            ) {
+                // get between
+                if (
+                    ![selectedStartDate, selectedEndDate].includes(
+                        day.format("YYYY-MM-DD")
+                    )
+                ) {
+                    styleDayCellProps["$hovered"] = true;
+                    styleDayCellProps["$point"] = "after-end";
+                    styleDayCellProps["$selected"] = true;
+                }
+
+                // get start
+                if (day.isSame(selectedStartDate)) {
+                    styleRightProps["$hovered"] = true;
+                    styleRightProps["$point"] = "start";
+                    styleRightProps["$selected"] = true;
+
+                    styleCircleProps["$hovered"] = false;
+                    styleCircleProps["$point"] = "selected";
+                    styleCircleProps["$selected"] = true;
+                }
+
+                if (day.isSame(selectedEndDate)) {
+                    styleLeftProps["$hovered"] = true;
+                    styleLeftProps["$point"] = "end";
+                    styleLeftProps["$selected"] = true;
+
+                    styleCircleProps["$hovered"] = false;
+                    styleCircleProps["$point"] = "selected";
+                    styleCircleProps["$selected"] = true;
+                }
+            }
+
+            // exist selected start and end, hover between
+            // get between date from start until hover
+            if (
+                hoverValue.length &&
+                selectedStartDate &&
+                selectedEndDate &&
+                day.isBetween(selectedStartDate, hoverValue, "day", "[]") &&
+                dayjs(hoverValue).isBefore(selectedEndDate)
+            ) {
+                // get between
+                if (
+                    ![selectedStartDate, selectedEndDate].includes(
+                        day.format("YYYY-MM-DD")
+                    )
+                ) {
+                    styleDayCellProps["$hovered"] = true;
+                    styleDayCellProps["$point"] = "middle";
+                    styleDayCellProps["$selected"] = true;
+                }
+
+                // get start
+                if (day.isSame(selectedStartDate)) {
+                    styleRightProps["$hovered"] = true;
+                    styleRightProps["$point"] = "start";
+                    styleRightProps["$selected"] = true;
+
+                    styleCircleProps["$hovered"] = false;
+                    styleCircleProps["$point"] = "selected";
+                    styleCircleProps["$selected"] = true;
+                }
+
+                // get end
+                if (day.isSame(hoverValue)) {
+                    styleLeftProps["$hovered"] = true;
+                    styleLeftProps["$point"] = "end";
+                    styleLeftProps["$selected"] = true;
+
+                    styleRightProps["$hovered"] = true;
+                    styleRightProps["$point"] = "selected";
+                    styleRightProps["$selected"] = true;
+
+                    styleCircleProps["$hovered"] = false;
+                    styleCircleProps["$point"] = "selected";
+                    styleCircleProps["$selected"] = true;
+
+                    styleDayCellProps["$clear"] = true;
+                }
+            }
+        }
+
+        return {
+            styleLeftProps,
+            styleRightProps,
+            styleCircleProps,
+            styleLabelProps,
+            styleDayCellProps,
         };
     };
 
@@ -95,48 +357,65 @@ export const CalendarDay = ({
     };
 
     const renderDayCells = () => {
-        return weeksOfTheMonth.map((week) => {
-            return week.map((day, dayIndex) => {
-                const { isDisabled, variant } = generateDayStatus(day);
-                const formattedDay = day.format("YYYY-MM-DD");
+        return weeksOfTheMonth.map((week, weekIndex) => {
+            return (
+                <RowDayCell key={weekIndex} onMouseLeave={handleMouseLeaveCell}>
+                    {week.map((day, dayIndex) => {
+                        const formattedDay = day.format("YYYY-MM-DD");
+                        const { variant } = generateDayStatus(day);
+                        const {
+                            styleLeftProps,
+                            styleRightProps,
+                            styleCircleProps,
+                            styleLabelProps,
+                            styleDayCellProps,
+                        } = generateStyleProps(day);
 
-                const isSelected = [
-                    selectedStartDate,
-                    selectedEndDate,
-                ].includes(formattedDay);
-
-                return (
-                    <GrowDayCell key={`day-${dayIndex}`}>
-                        <OverflowDisplay $position="left" />
-                        <OverflowDisplay $position="right" />
-                        <InteractiveCircle
-                            $variant={variant}
-                            $selected={isSelected}
-                            $disabled={isDisabled}
-                            onMouseEnter={() => handleHoverCell(formattedDay)}
-                            onClick={() => handleDayClick(day)}
-                        >
-                            <DayLabel
-                                weight={isSelected ? "semibold" : "regular"}
-                                $variant={variant}
-                                $disabled={isDisabled}
-                                $selected={isSelected}
+                        return (
+                            <GrowDayCell
+                                key={`day-${dayIndex}`}
+                                {...styleDayCellProps}
                             >
-                                {day.format("D")}
-                            </DayLabel>
-                        </InteractiveCircle>
-                    </GrowDayCell>
-                );
-            });
+                                <OverflowDisplay
+                                    $position="left"
+                                    {...styleLeftProps}
+                                />
+                                <OverflowDisplay
+                                    $position="right"
+                                    {...styleRightProps}
+                                />
+                                <InteractiveCircle
+                                    $variant={variant}
+                                    onClick={() => handleDayClick(day)}
+                                    onMouseEnter={() =>
+                                        handleHoverCell(formattedDay)
+                                    }
+                                    {...styleCircleProps}
+                                >
+                                    <DayLabel
+                                        weight={
+                                            styleLabelProps["$selected"]
+                                                ? "semibold"
+                                                : "regular"
+                                        }
+                                        $variant={variant}
+                                        {...styleLabelProps}
+                                    >
+                                        {day.format("D")}
+                                    </DayLabel>
+                                </InteractiveCircle>
+                            </GrowDayCell>
+                        );
+                    })}
+                </RowDayCell>
+            );
         });
     };
 
     return (
         <Wrapper $type={type}>
             {renderHeader()}
-            <DayContainer $type={type} onMouseLeave={handleMouseLeaveCell}>
-                {renderDayCells()}
-            </DayContainer>
+            {renderDayCells()}
         </Wrapper>
     );
 };
