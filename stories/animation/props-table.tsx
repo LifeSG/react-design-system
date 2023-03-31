880a6a32e2172367ac3b988995c24e59f50ca3af
Keith Tan <keith_xw_tan@tech.gov.sg>
2023-03-31 17:45:47 +0800

    [MISC][KT] Refactor stories

--- stories/animation/props-table.tsx
+++ stories/animation/props-table.tsx
@@ -1,22 +1,50 @@
 import React from "react";
 import { ApiTable } from "../storybook-common/api-table";
-import { ApiTableSectionProps } from "../storybook-common/api-table/types";
+import {
+    ApiTableAttributeRowProps,
+    ApiTableSectionProps,
+} from "../storybook-common/api-table/types";
+
+const BASE_ATTRIBUTES: ApiTableAttributeRowProps[] = [
+    {
+        name: "id",
+        description: "The unique id of the component",
+        propTypes: ["string"],
+    },
+    {
+        name: "className",
+        description: "The class selector of the component",
+        propTypes: ["string"],
+    },
+    {
+        name: "data-testid",
+        description: "The test identifier for the component",
+        propTypes: ["string"],
+    },
+];
 
 const DATA: ApiTableSectionProps[] = [
     {
+        attributes: [...BASE_ATTRIBUTES],
+    },
+];
+
+export const PropsTable = () => <ApiTable sections={DATA} />;
+
+const CUSTOMISED_DATA: ApiTableSectionProps[] = [
+    {
         attributes: [
+            ...BASE_ATTRIBUTES,
             {
                 name: "color",
-                description: "The color use to replace the default color",
-                propTypes: ["string"],
-            },
-            {
-                name: "data-testid",
-                description: "The test identifier for the component",
+                description:
+                    "The color that is to be applied on the animated elements",
                 propTypes: ["string"],
             },
         ],
     },
 ];
 
-export const PropsTable = () => <ApiTable sections={DATA} />;
+export const CustomisedPropsTable = () => (
+    <ApiTable sections={CUSTOMISED_DATA} />
+);
