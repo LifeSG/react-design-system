fd08271fc587f842188f19f2f98816fdb3b760d1
Vimal9412 <Vimal.Ganesan2@cognizant.com>
2023-04-06 11:32:14 +0800

    [BOOKINGSG-3753][VG] Adding JSDocs

--- src/toast/types.ts
+++ src/toast/types.ts
@@ -1,10 +1,14 @@
 import React from "react";
 
-export type toastType = "success" | "warning" | "error" | "info";
+export type ToastType = "success" | "warning" | "error" | "info";
 
 export interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {
-    type: toastType;
-    label?: string | undefined;
-    secondaryLabel: string;
+    /** Specify the type of status */
+    type: ToastType;
+    /** Specify the title of toast.  */
+    title: string | undefined;
+    /** Specify the content of toast.  */
+    label?: string;
+    /** Specify action if toast will be automatically dismissed */
     autoDismiss?: boolean | undefined;
 }
