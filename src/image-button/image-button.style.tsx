40e307b72909b4ac378fba462217ce1fd3bf3554
v1shva <vishva.sudantha@gmail.com>
2023-05-18 14:56:34 +0800

    [IMAGE-BUTTON][VM|DS] Refactor image button and update styling and add ImageWithFallback component

--- src/image-button/image-button.style.tsx
+++ src/image-button/image-button.style.tsx
@@ -1,60 +1,61 @@
 import { MediaQuery } from "../media";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Color } from "../color/color";
 import { DesignToken } from "src/design-token/design-token";
+import { TextStyleHelper } from "src/text/helper";
 
-interface StyleProps {
-    $outline: "none" | "browser";
-    $disabled?: boolean;
-}
-
-export const Main = styled.button<StyleProps>`
+export const Main = styled.button`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
     justify-content: center;
     align-items: center;
-    padding: 1rem 1.5rem;
-    background-color: transparent;
+    padding: 1.5rem 1rem;
+    background-color: ${Color.Neutral[8]};
     border: none;
     border-radius: 0.5rem;
     cursor: pointer;
     width: 13rem;
-    height: 8.625rem;
     transition: all 200ms ease;
+    h4 {
+        ${TextStyleHelper.getTextStyle("H4", "semibold")}
+    }
+
     img {
         width: 3.5rem;
     }
+
     ${MediaQuery.MaxWidth.tablet} {
-        padding: 0rem 1.5rem;
+        padding: 1.5rem 0rem;
     }
 
     ${MediaQuery.MaxWidth.mobileM} {
-        padding: 0rem 1rem;
+        padding: 1rem 0rem;
     }
-    ${(props) => {
-        if (props.$disabled) {
-            return css`
-                img {
-                    filter: grayscale(100%);
-                }
-                outline: none;
-                cursor: not-allowed;
-            `;
-        } else {
-            return css`
-                :focus:active {
-                    box-shadow: ${DesignToken.ElevationBoxShadow};
-                }
-                :focus {
-                    ${props.$outline === "none" && "outline: none;"}
-                }
-                @media (hover: hover) {
-                    &:hover {
-                        box-shadow: 0px 8px 16px -8px rgba(75, 83, 159, 0.24);
-                    }
-                }
-            `;
+
+    :focus:active {
+        box-shadow: ${DesignToken.ElevationBoxShadow};
+        h4 {
+            ${TextStyleHelper.getTextStyle("H4", "bold")}
         }
-    }}
+    }
+    @media (hover: hover) {
+        &:hover {
+            box-shadow: 0px 8px 16px -8px ${Color.Shadow.Elevation};
+            h4 {
+                ${TextStyleHelper.getTextStyle("H4", "bold")}
+            }
+        }
+    }
+    :disabled {
+        box-shadow: none;
+        img {
+            filter: grayscale(100%);
+        }
+        h4 {
+            ${TextStyleHelper.getTextStyle("H4", "semibold")}
+        }
+        outline: none;
+        cursor: not-allowed;
+    }
 `;
