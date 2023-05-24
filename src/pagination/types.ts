e47f195819794f2730c86b4242b46a3c66695a72
Balakrishnan S <balakrishnan.s5@cognizant.com>
2023-05-24 12:08:55 +0800

    [BOOKINGSG-4231]BS][Part 3] Page paginations Page size dropdown Input Select

--- src/pagination/types.ts
+++ src/pagination/types.ts
@@ -14,12 +14,3 @@ export interface DropdownItemProps {
     value?: number | undefined;
     label?: string | undefined;
 }
-export interface DropdownProps {
-    id?: string | undefined;
-    "data-testid"?: string | undefined;
-    className?: string | undefined;
-    selectedItem?: DropdownItemProps | undefined;
-    options?: DropdownItemProps[];
-    labelExtractor?: ((item: DropdownItemProps) => string) | undefined;
-    onSelectItem?: ((item: DropdownItemProps) => void) | undefined;
-}
