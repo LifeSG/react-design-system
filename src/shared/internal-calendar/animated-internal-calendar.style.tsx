291b309a96e58a549a26aa4094d7c907b76acaa0
Wilker <wilker.soh@cognizant.com>
2023-05-29 19:30:27 +0800

    [MISC][WK] fix 2px off width

--- src/shared/internal-calendar/animated-internal-calendar.style.tsx
+++ src/shared/internal-calendar/animated-internal-calendar.style.tsx
@@ -6,7 +6,7 @@ export const AnimatedDiv = styled(animated.div)`
     position: absolute;
     top: calc(100% + 0.5rem);
     left: 0;
-    width: 100%;
+    width: calc(100% + 2px);
     max-width: 41rem;
     background: ${Color.Neutral[8]};
     overflow: hidden;
