5b63b9e2e95bb8952878e4d0df3b1e7ecb156fbd
v1shva <vishva.sudantha@gmail.com>
2023-05-29 19:35:48 +0800

    [TIME-SLOT-WEEK-VIEW-COMP][VM|DS] Refactor time-slot week calendar to use calendar manager.

--- src/time-slot-week-view/time-slot-week-calendar.tsx
+++ src/time-slot-week-view/time-slot-week-calendar.tsx
@@ -1,150 +1,43 @@
 import dayjs, { Dayjs } from "dayjs";
-import React, { useEffect, useState } from "react";
-import { useResizeDetector } from "react-resize-detector";
-import { CalendarRef, View } from "../shared/internal-calendar";
-import { InternalCalendarMonth } from "../shared/internal-calendar/internal-calendar-month";
-import { InternalCalendarYear } from "../shared/internal-calendar/internal-calendar-year";
+import React, { useState } from "react";
 import { TimeSlot } from "../time-slot-bar";
-import { CalendarHelper } from "../util/calendar-helper";
-import {
-    ArrowLeft,
-    ArrowRight,
-    Container,
-    ContentBody,
-    DropdownButton,
-    DropdownText,
-    Header,
-    HeaderArrowButton,
-    HeaderArrows,
-    HeaderInputDropdown,
-    IconChevronDown,
-    ToggleZone,
-} from "./time-slot-week-calendar.style";
 import { TimeSlotWeekDays } from "./time-slot-week-days";
 import { TimeSlotWeekCalendarProps } from "./types";
+import { CalendarManager } from "../shared/internal-calendar/calendar-manager";
 
-export const Component = (
-    {
-        disabledDates,
-        onWeekDisplayChange,
-        onSelect,
-        value,
-        minDate,
-        maxDate,
-        slots,
-        showNavigationHeader = true,
-        enableSelection,
-        onSlotClick,
-        currentCalendarDate,
-        ...otherProps
-    }: TimeSlotWeekCalendarProps,
-    ref: React.ForwardedRef<CalendarRef>
-) => {
+export const Component = ({
+    disabledDates,
+    onWeekDisplayChange,
+    onSelect,
+    value,
+    minDate,
+    maxDate,
+    slots,
+    showNavigationHeader = true,
+    enableSelection,
+    onSlotClick,
+    currentCalendarDate,
+    ...otherProps
+}: TimeSlotWeekCalendarProps) => {
     // =============================================================================
     // CONST, STATE, REF
     // =============================================================================
-    const [calendarDate, setCalendarDate] = useState<Dayjs>(dayjs());
-    const [currentView, setCurrentView] = useState<View>("default");
     const [selectedDate, setSelectedDate] = useState<string>(); // YYYY-MM-DD
-    const [viewCalendarDate, setViewCalendarDate] = useState<Dayjs>();
     const dateFormat = "YYYY-MM-DD";
-    // =============================================================================
-    // HOOKS
-    // =============================================================================
-    const resizeDetector = useResizeDetector();
-
-    // =============================================================================
-    // EFFECTS
-    // =============================================================================
-    useEffect(() => {
-        if (currentCalendarDate) {
-            setCalendarDate(dayjs(currentCalendarDate));
-            setViewCalendarDate(dayjs(currentCalendarDate));
-        } else {
-            setCalendarDate(dayjs());
-            setViewCalendarDate(dayjs());
-        }
-    }, []);
-
-    useEffect(() => {
-        if (!value) {
-            setSelectedDate(undefined);
-            return;
-        }
-        setSelectedDate(value);
-        setCalendarDate(dayjs(value));
-    }, [value]);
 
     // =============================================================================
     // EVENT HANDLERS
     // =============================================================================
-    const handleLeftArrowClick = () => {
-        switch (currentView) {
-            case "default":
-                setViewCalendarDate((date) => date.subtract(1, "week"));
-                setCalendarDate((date) => {
-                    const prevMonth = date.subtract(1, "week");
-                    performOnCalendarDate(prevMonth);
-                    return prevMonth;
-                });
-                break;
-            case "month-options":
-                setCalendarDate((date) => date.subtract(1, "year"));
-                break;
-            case "year-options":
-                setCalendarDate((date) => date.subtract(10, "year"));
-                break;
-        }
-    };
-
-    const handleRightArrowClick = () => {
-        switch (currentView) {
-            case "default":
-                setViewCalendarDate((date) => date.add(1, "week"));
-                setCalendarDate((date) => {
-                    const nextMonth = date.add(1, "week");
-                    performOnCalendarDate(nextMonth);
-                    return nextMonth;
-                });
-                break;
-            case "month-options":
-                setCalendarDate((date) => date.add(1, "year"));
-                break;
-            case "year-options":
-                setCalendarDate((date) => date.add(10, "year"));
-                break;
-        }
-    };
 
     const handleDateSelect = (value: Dayjs) => {
         const stringValue = value.format(dateFormat);
-
-        setCalendarDate(value);
-        setViewCalendarDate(value);
         setSelectedDate(stringValue);
-
-        performOnSelectHandler(stringValue);
-    };
-
-    const handleMonthYearSelect = (value: Dayjs) => {
-        setCalendarDate(value);
-        setViewCalendarDate(value);
-
-        performOnCalendarDate(value);
+        onSelect(stringValue);
     };
 
     const handleOnSlotClick = (date: string, slot: TimeSlot) => {
         if (onSlotClick) {
             onSlotClick(date, slot);
-        }
-    };
-
-    // =============================================================================
-    // HELPER FUNCTIONS
-    // =============================================================================
-    const performOnSelectHandler = (changeValue: string) => {
-        if (onSelect) {
-            onSelect(changeValue);
         }
     };
 
@@ -163,173 +56,46 @@ export const Component = (
         }
     };
 
-    const handleMonthDropdownClick = () => {
-        if (currentView !== "month-options") {
-            setCurrentView("month-options");
-
-            // Maintain focus when selecting month dropdown
-            resizeDetector.ref.current.focus();
-        } else {
-            setCurrentView("default");
-            setCalendarDate(viewCalendarDate);
-        }
-    };
-
-    const handleYearDropdownClick = () => {
-        /**
-         * If the view is in the month options view,
-         * then clicking will send the view back to
-         * default
-         */
-        if (currentView !== "default") {
-            setCurrentView("default");
-            setCalendarDate(viewCalendarDate);
-        } else {
-            setCurrentView("year-options");
-        }
-    };
-
-    const getYearHeaderText = (): string => {
-        if (currentView === "year-options") {
-            const { beginDecade, endDecade } =
-                CalendarHelper.getStartEndDecade(calendarDate);
-
-            return `${beginDecade} to ${endDecade}`;
-        } else {
-            return dayjs(calendarDate).endOf("week").format("YYYY");
-        }
-    };
-
     // =============================================================================
     // RENDER FUNCTIONS
     // =============================================================================
-    const renderDropdownButtons = () => {
-        return (
-            <>
-                <DropdownButton
-                    type="button"
-                    tabIndex={-1}
-                    $expandedDisplay={currentView === "month-options"}
-                    $visible={currentView === "default"}
-                    id="month-dropdown"
-                    onClick={handleMonthDropdownClick}
-                    style={{ marginRight: 0 }}
-                >
-                    <DropdownText>
-                        {dayjs(calendarDate).endOf("week").format("MMM")}
-                    </DropdownText>
-                    <IconChevronDown />
-                </DropdownButton>
-                <DropdownButton
-                    type="button"
-                    tabIndex={-1}
-                    $expandedDisplay={currentView !== "default"}
-                    id="year-dropdown"
-                    onClick={handleYearDropdownClick}
-                >
-                    <DropdownText>{getYearHeaderText()}</DropdownText>
-                    <IconChevronDown />
-                </DropdownButton>
-            </>
-        );
-    };
-
-    const renderOptionsOverlay = () => {
-        switch (currentView) {
-            case "month-options":
-                return (
-                    <InternalCalendarMonth
-                        type={"input"}
-                        calendarDate={calendarDate}
-                        selectedStartDate={selectedDate}
-                        viewCalendarDate={viewCalendarDate}
-                        minDate={minDate}
-                        maxDate={maxDate}
-                        isNewSelection={true}
-                        onMonthSelect={handleMonthYearSelect}
-                    />
-                );
-            case "year-options":
-                return (
-                    <InternalCalendarYear
-                        type={"input"}
-                        calendarDate={calendarDate}
-                        selectedStartDate={selectedDate}
-                        viewCalendarDate={viewCalendarDate}
-                        minDate={minDate}
-                        maxDate={maxDate}
-                        isNewSelection={true}
-                        onYearSelect={handleMonthYearSelect}
-                    />
-                );
-            default:
-                return null;
-        }
-    };
-
-    const renderHeader = () => {
-        const disableLeftArrow =
-            minDate && dayjs(calendarDate).startOf("week") <= dayjs(minDate);
-        const disableRightArrow =
-            maxDate && dayjs(calendarDate).endOf("week") >= dayjs(maxDate);
-        return (
-            showNavigationHeader && (
-                <Header>
-                    <HeaderInputDropdown>
-                        {renderDropdownButtons()}
-                    </HeaderInputDropdown>
-                    <HeaderArrows>
-                        <HeaderArrowButton
-                            disabled={disableLeftArrow}
-                            tabIndex={-1}
-                            onClick={handleLeftArrowClick}
-                            data-testid={"left-arrow-btn"}
-                        >
-                            <ArrowLeft />
-                        </HeaderArrowButton>
-                        <HeaderArrowButton
-                            disabled={disableRightArrow}
-                            tabIndex={-1}
-                            onClick={handleRightArrowClick}
-                            data-testid={"right-arrow-btn"}
-                        >
-                            <ArrowRight />
-                        </HeaderArrowButton>
-                    </HeaderArrows>
-                </Header>
-            )
-        );
-    };
 
     const renderContent = () => {
         return (
-            <Container
-                ref={resizeDetector.ref}
-                tabIndex={-1}
-                data-id="calendar-container"
+            <CalendarManager
+                initialCalendarDate={currentCalendarDate}
+                selectedStartDate={value}
+                getLeftArrowDate={(day) => day.subtract(1, "week")}
+                getRightArrowDate={(day) => day.add(1, "week")}
+                isLeftArrowDisabled={(calendarDate) =>
+                    minDate &&
+                    dayjs(calendarDate).startOf("week") <= dayjs(minDate)
+                }
+                isRightArrowDisabled={(calendarDate) =>
+                    maxDate &&
+                    dayjs(calendarDate).endOf("week") >= dayjs(maxDate)
+                }
+                onCalendarDate={performOnCalendarDate}
+                showNavigationHeader={showNavigationHeader}
                 {...otherProps}
             >
-                <ContentBody>
-                    {renderHeader()}
-                    <ToggleZone>
-                        {currentView === "default" && (
-                            <TimeSlotWeekDays
-                                calendarDate={calendarDate}
-                                disabledDates={disabledDates}
-                                selectedDate={selectedDate}
-                                minDate={minDate}
-                                maxDate={maxDate}
-                                isNewSelection={true}
-                                onSelect={handleDateSelect}
-                                slots={slots}
-                                enableSelection={enableSelection}
-                                onSlotClick={handleOnSlotClick}
-                            />
-                        )}
-                        {renderOptionsOverlay()}
-                    </ToggleZone>
-                </ContentBody>
-            </Container>
+                {({ calendarDate }) => {
+                    return (
+                        <TimeSlotWeekDays
+                            calendarDate={calendarDate}
+                            disabledDates={disabledDates}
+                            selectedDate={selectedDate}
+                            minDate={minDate}
+                            maxDate={maxDate}
+                            isNewSelection={true}
+                            onSelect={handleDateSelect}
+                            slots={slots}
+                            enableSelection={enableSelection}
+                            onSlotClick={handleOnSlotClick}
+                        />
+                    );
+                }}
+            </CalendarManager>
         );
     };
 
