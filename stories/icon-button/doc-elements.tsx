b1b0143ccf34c287df9f25377e22c46403e386db
Balakrishnan S <balakrishnan.s5@cognizant.com>
2023-06-08 18:19:55 +0800

    [BOOKINGSG-4283] [BS] Icon Button variation PR comments

--- stories/icon-button/doc-elements.tsx
+++ stories/icon-button/doc-elements.tsx
@@ -4,41 +4,19 @@ import styled from "styled-components";
 // STYLING
 // =============================================================================
 export const Container = styled.div`
-    display: flex;
-    justify-content: flex-start;
     align-items: flex-start;
-
-    label {
-        min-width: 5rem;
-    }
+    display: grid;
+    justify-content: flex-start;
+    margin-right: 1rem;
 
     button {
-        flex-direction: row;
-        margin-left: 1rem;
+        margin: 1rem 0;
     }
 
-    span:nth-of-type(1) {
-        margin-left: 6rem;
-    }
-    span:nth-of-type(2) {
-        margin-left: 1.5rem;
-    }
-    span:nth-of-type(3) {
-        margin-left: 0.5rem;
-    }
-    span:nth-of-type(4) {
-        margin-left: 2.5rem;
+    span {
+        margin-bottom: 3.5rem;
     }
 `;
-
-export const Container2 = styled(Container)`
-    button:not(:nth-of-type(1)) {
-        margin-left: 2rem;
-    }
-`;
-
-export const Container3 = styled(Container)`
-    button:not(:nth-of-type(1)) {
-        margin-left: 2.5rem;
-    }
+export const ContainerWrapper = styled(Container)`
+    display: flex;
 `;
