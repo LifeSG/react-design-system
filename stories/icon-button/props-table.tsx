22ad3f22b16644ddc1baadd7459916c4b97f808a
Balakrishnan S <balakrishnan.s5@cognizant.com>
2023-06-08 17:16:06 +0800

    [BOOKINGSG-4283] [BS] Icon Button variation PR comments

--- stories/icon-button/props-table.tsx
+++ stories/icon-button/props-table.tsx
@@ -6,6 +6,21 @@ const DATA: ApiTableSectionProps[] = [
     {
         attributes: [
             {
+                name: "",
+                description: (
+                    <>
+                        This component also inherits props from&nbsp;
+                        <a
+                            href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement"
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            HTMLButtonElement
+                        </a>
+                    </>
+                ),
+            },
+            {
                 name: "data-testid",
                 description: "The test identifier of the component",
                 propTypes: ["string"],
