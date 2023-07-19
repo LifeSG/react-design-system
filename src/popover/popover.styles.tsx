22f6324733ea28fcd8fb4924738e7b2187fb1dfa
jasm <jasmine_cheah@tech.gov.sg>
2023-07-19 13:02:08 +0800

    [MOL-13286][Jasm] fix popover z-index so that it renders on top of LifeSG's navigation banner

--- src/popover/popover.styles.tsx
+++ src/popover/popover.styles.tsx
@@ -29,7 +29,7 @@ const getVisibilityStyle = (visible: boolean) => {
             visibility: visible;
             opacity: 1;
             transition: ${Transition.Base};
-            z-index: 2;
+            z-index: 1000;
         `;
     } else {
         return css`
