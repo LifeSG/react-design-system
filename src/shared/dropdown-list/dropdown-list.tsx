35eb188cf02b3ea86290c516bc88a82802958bdd
Anu Mutyala <mutyala.lakshmi@ufinity.com>
2023-07-27 15:10:42 +0800

    [MOL-13375][AM|HFAH] update search filter to cater for lowercase secondaryLabel

--- src/shared/dropdown-list/dropdown-list.tsx
+++ src/shared/dropdown-list/dropdown-list.tsx
@@ -205,18 +205,9 @@ export const DropdownList = <T, V>({
                     typeof label === "object"
                         ? label.title.toLowerCase()
                         : label.toLowerCase();
+                const secondaryLabel = typeof label === "string" ? undefined : label.secondaryLabel.toLowerCase();;
 
-                // include secondary label filter/search
-                if (typeof label === "object" && label.secondaryLabel) {
-                    return (
-                        title.includes(searchValue.trim().toLowerCase()) ||
-                        label.secondaryLabel.includes(
-                            searchValue.trim().toLowerCase()
-                        )
-                    );
-                }
-
-                return title.includes(searchValue.trim().toLowerCase());
+                return (title.includes(searchValue.trim().toLowerCase()) || (secondaryLabel && secondaryLabel.includes(searchValue.trim().toLowerCase())));
             });
             setDisplayListItems(updated);
         }
