6290d236383a76b10eb6346f4a5d96c3409403ed
Wilker <wilker.soh@cognizant.com>
2023-08-07 11:54:01 +0800

    [BOOKINGSG-4362][WK] update updateSelectedItemFromKey/handleListItemClick fns and selectedKeyPath to plural

---
+++ src/input-nested-multi-select/input-nested-multi-select.tsx
@@ -0,0 +1,297 @@
+import React, { useEffect, useRef, useState } from "react";
+import isEmpty from "lodash/isEmpty";
+import { NestedDropdownList } from "../shared/nested-dropdown-list/nested-dropdown-list";
+import { DropdownWrapper } from "../shared/dropdown-wrapper";
+import { CombinedFormattedOptionProps } from "../shared/nested-dropdown-list/types";
+import {
+    Divider,
+    IconContainer,
+    LabelContainer,
+    PlaceholderLabel,
+    Selector,
+    StyledChevronIcon,
+    ValueLabel,
+} from "../shared/dropdown-wrapper/dropdown-wrapper.styles";
+import { StringHelper } from "../util";
+import { InputNestedMultiSelectProps } from "./types";
+import { CombinedOptionProps } from "../input-nested-select";
+
+interface SelectedItemType<V1, V2, V3> {
+    label: string;
+    value: V1 | V2 | V3;
+}
+
+export const InputNestedMultiSelect = <V1, V2, V3>({
+    placeholder = "Select",
+    options,
+    disabled,
+    error,
+    className,
+    "data-testid": testId,
+    id,
+    selectedKeyPaths: _selectedKeyPaths,
+    mode,
+    valueToStringFunction,
+    enableSearch,
+    searchPlaceholder,
+    hideNoResultsDisplay,
+    listStyleWidth,
+    readOnly,
+    onSearch,
+    onSelectOptions,
+    onShowOptions,
+    onHideOptions,
+    onRetry,
+    optionsLoadState = "success",
+    optionTruncationType = "end",
+    ...otherProps
+}: InputNestedMultiSelectProps<V1, V2, V3>): JSX.Element => {
+    // =============================================================================
+    // CONST, STATE
+    // =============================================================================
+    const [selectedKeyPaths, setSelectedKeyPaths] = useState<string[][]>(
+        _selectedKeyPaths || []
+    );
+    const [selectedItems, setSelectedItems] =
+        useState<SelectedItemType<V1, V2, V3>[]>();
+
+    const [showOptions, setShowOptions] = useState<boolean>(false);
+
+    const selectorRef = useRef<HTMLButtonElement>();
+    const labelContainerRef = useRef<HTMLDivElement>();
+
+    // =============================================================================
+    // EFFECTS
+    // =============================================================================
+    useEffect(() => {
+        const newKeyPath = _selectedKeyPaths || [];
+        setSelectedKeyPaths(newKeyPath);
+
+        updateSelectedItemFromKey(options, newKeyPath);
+    }, [_selectedKeyPaths, options]);
+
+    // =============================================================================
+    // EVENT HANDLERS
+    // =============================================================================
+    const handleSelectorClick = (event: React.MouseEvent) => {
+        event.preventDefault();
+
+        if (disabled || readOnly) {
+            return;
+        }
+
+        setShowOptions(!showOptions);
+        triggerOptionDisplayCallback(!showOptions);
+    };
+
+    const handleListItemClick = (
+        item: CombinedFormattedOptionProps<V1, V2, V3>
+    ) => {
+        const { keyPath, value } = item;
+
+        const isKeyPathExists = selectedKeyPaths.some(
+            (selectedKeyPath) =>
+                JSON.stringify(selectedKeyPath) === JSON.stringify(keyPath)
+        );
+
+        if (!isKeyPathExists) {
+            setSelectedKeyPaths([...selectedKeyPaths, keyPath]);
+            setSelectedItems([...selectedItems, item]);
+        } else {
+            const resultKeyPaths = selectedKeyPaths.filter(
+                (selectedKeyPath) =>
+                    JSON.stringify(selectedKeyPath) !== JSON.stringify(keyPath)
+            );
+
+            setSelectedKeyPaths(resultKeyPaths);
+            // remove item, please
+            // setSelectedItems({ label, value });
+            setSelectedItems([...selectedItems, item]);
+        }
+
+        setShowOptions(false);
+        triggerOptionDisplayCallback(false);
+
+        if (selectorRef.current) {
+            selectorRef.current.focus();
+        }
+
+        if (onSelectOptions) {
+            onSelectOptions(keyPath, value);
+        }
+    };
+
+    const handleListDismiss = (setSelectorFocus?: boolean | undefined) => {
+        if (showOptions) {
+            setShowOptions(false);
+            triggerOptionDisplayCallback(false);
+        }
+
+        if (setSelectorFocus && selectorRef.current) {
+            selectorRef.current.focus();
+        }
+    };
+
+    const handleWrapperBlur = () => {
+        setShowOptions(false);
+        triggerOptionDisplayCallback(false);
+    };
+
+    // =============================================================================
+    // HELPER FUNCTION
+    // =============================================================================
+    const getDisplayValue = (): string => {
+        const { label, value } = selectedItems[0];
+
+        if (selectedItems.length > 1) {
+            return `${selectedItems.length} selected`;
+        } else if (valueToStringFunction) {
+            return valueToStringFunction(value) || value.toString();
+        } else {
+            return label;
+        }
+    };
+
+    const updateSelectedItemFromKey = (
+        options: CombinedOptionProps<V1, V2, V3>[],
+        keyPaths: string[][]
+    ) => {
+        const findSelectedItem = (
+            items: CombinedOptionProps<V1, V2, V3>[],
+            keyPaths: string[]
+        ): CombinedOptionProps<V1, V2, V3> | undefined => {
+            const [currentKey, ...nextKeyPath] = keyPaths;
+
+            if (isEmpty(items) || !currentKey) {
+                return undefined;
+            }
+
+            const item = items.find((item) => item.key === currentKey);
+
+            if (!item || !nextKeyPath.length) {
+                return item;
+            }
+
+            return findSelectedItem(item.subItems, nextKeyPath);
+        };
+
+        const selectedItems = [];
+        for (let i = 0; i < selectedKeyPaths.length; i++) {
+            const item = findSelectedItem(options, keyPaths[i]);
+            selectedItems.push(item);
+        }
+
+        // TODO: condition check for nested array
+        if (selectedItems) {
+            setSelectedItems(selectedItems);
+        } else {
+            setSelectedItems(undefined);
+        }
+    };
+
+    const truncateValue = (value: string) => {
+        if (optionTruncationType === "middle") {
+            let widthOfElement = 0;
+            if (labelContainerRef && labelContainerRef.current) {
+                widthOfElement =
+                    labelContainerRef.current.getBoundingClientRect().width;
+            }
+            return StringHelper.truncateOneLine(value, widthOfElement, 120, 6);
+        }
+
+        return value;
+    };
+
+    const triggerOptionDisplayCallback = (show: boolean) => {
+        if (!show && onHideOptions) {
+            onHideOptions();
+        }
+
+        if (show && onShowOptions) {
+            onShowOptions();
+        }
+    };
+
+    // =============================================================================
+    // RENDER FUNCTION
+    // =============================================================================
+    const renderLabel = () => {
+        if (isEmpty(selectedItems)) {
+            return (
+                <PlaceholderLabel truncateType={optionTruncationType}>
+                    {placeholder}
+                </PlaceholderLabel>
+            );
+        } else {
+            return (
+                <ValueLabel truncateType={optionTruncationType}>
+                    {truncateValue(getDisplayValue())}
+                </ValueLabel>
+            );
+        }
+    };
+
+    const renderSelectorContent = () => (
+        <>
+            <LabelContainer ref={labelContainerRef}>
+                {renderLabel()}
+            </LabelContainer>
+            {!readOnly && (
+                <IconContainer expanded={showOptions}>
+                    <StyledChevronIcon />
+                </IconContainer>
+            )}
+        </>
+    );
+
+    const renderOptionList = () => {
+        if ((options && options.length > 0) || onRetry) {
+            return (
+                <NestedDropdownList
+                    data-testid="dropdown-list"
+                    listItems={options}
+                    listStyleWidth={listStyleWidth}
+                    visible={showOptions}
+                    mode={mode}
+                    selectedKeyPaths={selectedKeyPaths}
+                    itemsLoadState={optionsLoadState}
+                    itemTruncationType={optionTruncationType}
+                    enableSearch={enableSearch}
+                    searchPlaceholder={searchPlaceholder}
+                    hideNoResultsDisplay={hideNoResultsDisplay}
+                    onDismiss={handleListDismiss}
+                    onSelectItem={handleListItemClick}
+                    onSearch={onSearch}
+                    onRetry={onRetry}
+                />
+            );
+        }
+
+        return null;
+    };
+
+    return (
+        <DropdownWrapper
+            className={className}
+            show={showOptions}
+            error={error && !showOptions}
+            disabled={disabled}
+            readOnly={readOnly}
+            testId={testId}
+            onBlur={handleWrapperBlur}
+        >
+            <Selector
+                ref={selectorRef}
+                type="button"
+                data-testid={id || "selector"}
+                disabled={disabled}
+                onClick={handleSelectorClick}
+                {...otherProps}
+            >
+                {renderSelectorContent()}
+            </Selector>
+            {showOptions && <Divider />}
+            {renderOptionList()}
+        </DropdownWrapper>
+    );
+};
