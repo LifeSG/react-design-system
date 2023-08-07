a8584304fa2b397b528256dc44141f9fb4fdbe91
Vimal9412 <Vimal.Ganesan2@cognizant.com>
2023-08-07 11:54:35 +0800

    [BOOKINGSG-4430][VG] Add styledContent doc-element

---
+++ stories/toast/doc-elements.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Layout } from "../../src/layout";
+
+export const StyledContent = styled(Layout.Content)`
+    [data-id="container"] {
+        flex-direction: column;
+    }
+`;
