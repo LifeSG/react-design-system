933e5ec1decbc00aa33c9dc31439a2b092a2f679
Wilker <wilker.soh@cognizant.com>
2023-08-07 11:50:56 +0800

    [BOOKINGSG-4362][WK] update multi nested select types

---
+++ src/input-nested-multi-select/types.ts
@@ -0,0 +1,21 @@
+import { InputNestedSelectProps } from "../input-nested-select";
+
+// =============================================================================
+// INPUT SELECT PROPS
+// =============================================================================
+
+type OmitTypes = "selectedKeyPath" | "onSelectOption" | "selectableCategory";
+export interface InputNestedMultiSelectProps<V1, V2, V3>
+    extends Omit<InputNestedSelectProps<V1, V2, V3>, OmitTypes> {
+    /** Specifies key path to select particular option label */
+    selectedKeyPaths?: string[][] | undefined;
+    onSelectOptions?:
+        | ((keyPath: string[], value: V1 | V2 | V3) => void)
+        | undefined;
+}
+
+/** To be exposed for Form component inheritance */
+export type InputNestedMultiSelectPartialProps<V1, V2, V3> = Omit<
+    InputNestedMultiSelectProps<V1, V2, V3>,
+    "error"
+>;
