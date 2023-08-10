bef4fcca6a844f017b158d632f18505dcdb36225
Balakrishnan S <balakrishnan.s5@cognizant.com>
2023-08-10 17:20:27 +0800

    [BOOKINGSG-4436][BK]  [DS] Table Component (Basic)

---
+++ src/data-table/data-table.styles.tsx
@@ -0,0 +1,152 @@
+import styled, { css } from "styled-components";
+import { Color } from "../color";
+import { TextStyleHelper } from "../text";
+import { Checkbox } from "../checkbox";
+import { ArrowDownIcon, ArrowUpIcon } from "@lifesg/react-icons";
+// =============================================================================
+// STYLES
+// =============================================================================
+
+export const TableWrapper = styled.div`
+    width: 100%;
+    border: 2px solid ${Color.Neutral[6]};
+    border-radius: 0.5rem 0.5rem 0 0;
+    overflow: auto;
+`;
+
+export const Table = styled.table<{
+    $addMarginToFirstColumn?: boolean;
+    $addMarginToLastColumn?: boolean;
+}>`
+    width: 100%;
+    border-collapse: collapse;
+    ${(props) => {
+        if (props.$addMarginToFirstColumn) {
+            return css`
+                th:first-child,
+                td:first-child {
+                    padding-left: 1.5rem;
+                }
+            `;
+        }
+    }}
+    ${(props) => {
+        if (props.$addMarginToLastColumn) {
+            return css`
+                th:last-child,
+                td:last-child {
+                    padding-right: 1.5rem;
+                }
+            `;
+        }
+    }}
+`;
+
+export const HeaderRow = styled.tr`
+    background-color: #f5f5f5;
+    height: 5rem;
+    border-bottom: 2px solid ${Color.Neutral[6]};
+`;
+
+export const HeaderCell = styled.th<{ clickable: boolean; maxWidth?: string }>`
+    padding: 1rem;
+    text-align: left;
+    cursor: ${({ clickable }) => (clickable ? "pointer" : "default")};
+    max-width: ${({ maxWidth }) => maxWidth || "auto"};
+    vertical-align: middle;
+    ${TextStyleHelper.getFontFamily("H5", "bold")}
+    color: ${Color.Neutral[1]}
+`;
+
+export const HeaderCellWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const getBackgroundColor = (isSelected = false, alternating = false) => {
+    if (isSelected) {
+        return Color.Accent.Light[5];
+    } else if (alternating) {
+        return Color.Neutral[7];
+    } else {
+        return Color.Neutral[8];
+    }
+};
+
+export const BodyRow = styled.tr<{
+    alternating: boolean;
+    isSelected?: boolean;
+    isSelectable?: boolean;
+}>`
+    ${({ alternating, isSelected }) => css`
+        background-color: ${getBackgroundColor(isSelected, alternating)};
+    `}
+    border-top: 2px solid ${Color.Neutral[6]};
+    &:hover {
+        ${({ isSelected, isSelectable }) =>
+            !isSelected &&
+            isSelectable &&
+            css`
+                background-color: ${Color.Accent.Light[4]};
+            `}
+    }
+    &:first-child {
+        border-top: none;
+    }
+`;
+
+export const BodyCell = styled.td<{ width?: string }>`
+    padding: 1.25rem 1rem;
+    width: ${({ width: width }) => width || "auto"};
+    vertical-align: middle;
+`;
+
+export const CheckBoxWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const CheckBox = styled(Checkbox)``;
+
+const sortIconStyles = css`
+    color: ${Color.Neutral[1]};
+    font-weight: bold;
+    margin-left: 0.5rem;
+`;
+
+export const SortArrowUpIcon = styled(ArrowUpIcon)`
+    ${sortIconStyles}
+`;
+
+export const SortArrowDownIcon = styled(ArrowDownIcon)`
+    ${sortIconStyles}
+`;
+
+export const LoaderWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const EmptyDataElement = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    opacity: 0;
+    img {
+        width: 400px;
+        height: 280px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    animation: fade-in 1s ease-in forwards;
+    @keyframes fade-in {
+        100% {
+            opacity: 1;
+        }
+    }
+`;
---
+++ src/data-table/data-table.tsx
@@ -0,0 +1,305 @@
+import { Text } from "../text";
+import { Button } from "../button";
+import { LoadingDotsSpinner } from "../animations";
+import {
+    BodyCell,
+    BodyRow,
+    CheckBox,
+    CheckBoxWrapper,
+    EmptyDataElement,
+    HeaderCell,
+    HeaderCellWrapper,
+    HeaderRow,
+    LoaderWrapper,
+    SortArrowDownIcon,
+    SortArrowUpIcon,
+    Table,
+    TableWrapper,
+} from "./data-table.styles";
+import { IDataTableProps, IRow, SortIndicator, THeader } from "./types";
+
+// eslint-disable-next-line max-lines-per-function
+export const DataTable = ({
+    testid,
+    headersConfig,
+    headers,
+    rowsConfig,
+    rows,
+    className,
+    selectionConfig,
+    selection,
+    sortIndicators,
+    actionsConfig,
+    alternatingRows,
+    customEmptyView,
+    isLoadingData = false,
+}: IDataTableProps) => {
+    // ===========================================================================
+    // HELPER FUNCTIONS
+    // ===========================================================================
+    const isAllCheckBoxSelected = (): boolean => {
+        return selection?.length === rows.length;
+    };
+
+    const isRowSelected = (rowId: string): boolean => {
+        return !!selection?.includes(rowId);
+    };
+
+    const isAlternatingRow = (index: number): boolean => {
+        return !!alternatingRows && index % 2 === 0;
+    };
+
+    const getDataTestId = (subStr: string) => {
+        if (!testid) return undefined;
+        return `${testid}-${subStr}`;
+    };
+
+    const getTotalColumns = (): number => {
+        return (
+            headers.length +
+            (selectionConfig?.showCheckboxes ? 1 : 0) +
+            (actionsConfig?.showActions ? 1 : 0)
+        );
+    };
+
+    // =============================================================================
+    // RENDER FUNCTIONS
+    // =============================================================================
+
+    // Render the table headers
+    const renderHeaders = () => (
+        <HeaderRow className={headersConfig?.className}>
+            {/* Checkboxes for selection */}
+            {selectionConfig?.showCheckboxes && renderHeaderCheckBox()}
+            {/* Render each header cell */}
+            {headers.map(renderHeaderCell)}
+            {/* Actions column */}
+            {actionsConfig?.showActions && (
+                <HeaderCell
+                    data-testid={getDataTestId("header-action")}
+                    clickable={false}
+                    maxWidth={actionsConfig.headerWidth}
+                    className={actionsConfig.headerClassName}
+                >
+                    {actionsConfig.headerLabel}
+                </HeaderCell>
+            )}
+        </HeaderRow>
+    );
+
+    const renderHeaderCell = (header: THeader) => {
+        const {
+            colId,
+            label,
+            clickable = false,
+            className: headerClassName,
+            style,
+        } = typeof header === "string"
+            ? {
+                  colId: header,
+                  label: header,
+                  className: undefined,
+                  style: undefined,
+              }
+            : header;
+
+        return (
+            <HeaderCell
+                data-testid={getDataTestId(`header-${colId}`)}
+                key={colId}
+                clickable={clickable}
+                className={headerClassName}
+                onClick={() =>
+                    clickable && headersConfig?.onClickHeader?.(colId)
+                }
+                style={style}
+            >
+                <HeaderCellWrapper>
+                    {label}
+                    {renderSortedArrow(colId)}
+                </HeaderCellWrapper>
+            </HeaderCell>
+        );
+    };
+
+    const renderSortedArrow = (colId: string) => {
+        const isSorted = sortIndicators?.[colId];
+
+        if (!isSorted) {
+            return <></>;
+        } else if (isSorted === SortIndicator.ASC) {
+            return (
+                <SortArrowUpIcon
+                    data-testid={getDataTestId(`header-${colId}-arrowup`)}
+                />
+            );
+        } else {
+            return (
+                <SortArrowDownIcon
+                    data-testid={getDataTestId(`header-${colId}-arrowdown`)}
+                />
+            );
+        }
+    };
+
+    const renderHeaderCheckBox = () => {
+        return (
+            <HeaderCell
+                data-testid={getDataTestId("header-selection")}
+                className={selectionConfig?.headerClassName}
+                clickable={false}
+                maxWidth={selectionConfig?.headerWidth}
+            >
+                <CheckBoxWrapper>
+                    <CheckBox
+                        displaySize="small"
+                        checked={isAllCheckBoxSelected()}
+                        onClick={() => {
+                            selectionConfig?.onClickSelectAll?.(
+                                isAllCheckBoxSelected()
+                            );
+                        }}
+                    />
+                </CheckBoxWrapper>
+            </HeaderCell>
+        );
+    };
+
+    // Render the table rows
+    const renderRows = () => {
+        return (
+            <>
+                {rows.map((row: IRow, index: number) => (
+                    <BodyRow
+                        data-testid={getDataTestId(`row-${row.id.toString()}`)}
+                        key={row.id.toString()}
+                        className={
+                            isAlternatingRow(index)
+                                ? rowsConfig?.alternatingClassName
+                                : rowsConfig?.className
+                        }
+                        alternating={isAlternatingRow(index)}
+                        isSelectable={selectionConfig?.showCheckboxes}
+                        isSelected={isRowSelected(row.id.toString())}
+                    >
+                        {/* Checkboxes for selection */}
+                        {selectionConfig?.showCheckboxes &&
+                            renderRowCheckBox(row.id.toString())}
+
+                        {/* Render each data cell */}
+                        {headers.map((header) => renderRowCell(header, row))}
+                        {/* Actions row */}
+                        {actionsConfig?.showActions &&
+                            actionsConfig.actions && (
+                                <BodyCell
+                                    data-testid={getDataTestId(
+                                        `row-${row.id.toString()}-action`
+                                    )}
+                                    width={actionsConfig.headerWidth}
+                                    className={actionsConfig.className}
+                                >
+                                    {actionsConfig?.actions(
+                                        row,
+                                        isRowSelected(row.id.toString())
+                                    )}
+                                </BodyCell>
+                            )}
+                    </BodyRow>
+                ))}
+            </>
+        );
+    };
+
+    const renderRowCell = (header: THeader, row: IRow) => {
+        const style = typeof header !== "string" ? header.style : undefined;
+        const colId = typeof header === "string" ? header : header.colId;
+        const cellData = row[colId];
+        const cellId = `${row.id.toString()}-${colId}`;
+
+        return (
+            <BodyCell
+                data-testid={getDataTestId(`row-${cellId}`)}
+                key={cellId}
+                style={style}
+            >
+                {cellData}
+            </BodyCell>
+        );
+    };
+
+    const renderRowCheckBox = (rowId: string) => {
+        return (
+            <BodyCell
+                data-testid={getDataTestId(`row-${rowId}-selection`)}
+                className={selectionConfig?.className}
+                width={selectionConfig?.headerWidth}
+            >
+                <CheckBoxWrapper>
+                    <CheckBox
+                        displaySize="small"
+                        checked={isRowSelected(rowId)}
+                        onClick={() => {
+                            selectionConfig?.onClickSelect?.(
+                                "id",
+                                rowId,
+                                !isRowSelected(rowId)
+                            );
+                        }}
+                    />
+                </CheckBoxWrapper>
+            </BodyCell>
+        );
+    };
+
+    const basicEmptyView = () => {
+        return (
+            <EmptyDataElement>
+                {/* <img src="/img/empty-data.svg"></img> */}
+                <Text.H3>No items found</Text.H3>
+                <Text.Body>This is a placeholder text</Text.Body>
+                <Button.Default styleType="secondary"> Trigger</Button.Default>
+            </EmptyDataElement>
+        );
+    };
+
+    const renderEmptyView = () => {
+        if (rows.length === 0)
+            return (
+                <tr>
+                    <td style={{ padding: "4rem" }} colSpan={getTotalColumns()}>
+                        {customEmptyView ? customEmptyView() : basicEmptyView()}
+                    </td>
+                </tr>
+            );
+    };
+
+    const renderLoader = () => {
+        return (
+            <tr>
+                <td style={{ padding: "4rem" }} colSpan={getTotalColumns()}>
+                    <LoaderWrapper>
+                        {isLoadingData && <LoadingDotsSpinner />}
+                    </LoaderWrapper>
+                </td>
+            </tr>
+        );
+    };
+
+    return (
+        <TableWrapper>
+            <Table
+                data-testid={getDataTestId("table")}
+                className={className}
+                $addMarginToFirstColumn={!selectionConfig?.showCheckboxes}
+                $addMarginToLastColumn={!actionsConfig?.showActions}
+            >
+                <tbody>
+                    {renderHeaders()}
+                    {!isLoadingData && renderRows()}
+                    {!isLoadingData && renderEmptyView()}
+                    {isLoadingData && renderLoader()}
+                </tbody>
+            </Table>
+        </TableWrapper>
+    );
+};
