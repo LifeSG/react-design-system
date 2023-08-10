22492ab232167038561eb65315d83b9be8c3cca8
Balakrishnan S <balakrishnan.s5@cognizant.com>
2023-08-11 01:45:52 +0800

    [BOOKINGSG-4436][BK] [DS] Table Component (Basic) new stories added

--- stories/data-table/doc-elements.tsx
+++ stories/data-table/doc-elements.tsx
@@ -1,20 +1,26 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 // =============================================================================
 // STYLING
 // =============================================================================
 
-export const Grid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(5, min-content);
-    gap: 1.5rem;
+export const EmptyDataElement = styled.div`
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-items: center;
-    overflow-x: auto;
-    padding-bottom: 1rem;
-    margin: auto;
-`;
-
-export const Row = styled.div`
-    display: contents;
+    gap: 2rem;
+    opacity: 0;
+    img {
+        width: 10rem;
+        height: 10rem;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    animation: fade-in 1s ease-in forwards;
+    @keyframes fade-in {
+        100% {
+            opacity: 1;
+        }
+    }
 `;
