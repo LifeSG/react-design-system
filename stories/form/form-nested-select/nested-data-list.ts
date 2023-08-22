a35968b3edb33fb566ba8241f94f3cdeb22c6402
Wilker <wilker.soh@cognizant.com>
2023-08-22 12:18:13 +0800

    [BOOKINGSG-4362][WK] remove option without category in L1

--- stories/form/form-nested-select/nested-data-list.ts
+++ stories/form/form-nested-select/nested-data-list.ts
@@ -1,13 +1,5 @@
 export const options = [
     {
-        label: "Sub Category A",
-        value: {
-            id: 820,
-            name: "Sub category a",
-        },
-        key: "820",
-    },
-    {
         label: "Category 1",
         value: { id: 999, name: "category 1" },
         key: "999",
