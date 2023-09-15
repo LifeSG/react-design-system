438c027ee5e442971679009d04830356843834b1
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-09-15 16:10:49 +0800

    [BOOKINGSG-4565][RL] Update prop types

--- stories/alert/props-table.tsx
+++ stories/alert/props-table.tsx
@@ -16,7 +16,7 @@ export const PropsTable = () => (
                     `"warning"`,
                     `"error"`,
                     `"success"`,
-                    `"information"`,
+                    `"info"`,
                     `"description"`,
                 ]}
             >
