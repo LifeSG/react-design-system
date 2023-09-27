a4a7724e073ca2630a5f19b0aa7fe98d5b20fcb0
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-09-27 14:33:05 +0800

    [MISC][RL] Updateprops documentation

--- src/data-table/types.ts
+++ src/data-table/types.ts
@@ -4,31 +4,35 @@ import { ErrorDisplayAttributes } from "../error-display/types";
 export interface DataTableProps {
     id?: string | undefined;
     "data-testid"?: string | undefined;
+    /** Configuration for each header */
     headers: HeaderProps[];
     rows?: RowProps[] | undefined;
-    /** css class to put on table **/
     className?: string | undefined;
     selectedIds?: string[] | undefined;
     disabledIds?: string[] | undefined;
-    /** enable checkboxes */
+    /** Enables a checkbox for each row */
     enableMultiSelect?: boolean | undefined;
-    /** display select all checkbox */
+    /** Enables a select all checkbox */
     enableSelectAll?: boolean | undefined;
-    /** display action bar */
+    /** Specifies if an action bar is visible when one or more rows are selected */
     enableActionBar?: boolean | undefined;
-    /** sticky header */
+    /** Specifies if the header is fixed to the top */
     enableStickyHeader?: boolean | undefined;
+    /** Custom content to be rendered within the action bar */
     actionBarContent?: ReactNode | undefined;
+    loadState?: LoadStateType | undefined;
     onSelect?: ((rowId: string, isSelected: boolean) => void) | undefined;
-    onSelectAll?: ((isSelected: boolean) => void) | undefined;
+    onSelectAll?: ((isAllSelected: boolean) => void) | undefined;
     onHeaderClick?: ((fieldKey: string) => void) | undefined;
     onClearSelectionClick?: (() => void) | undefined;
-    /** columns that want to show a sort indicator **/
+    /** Specifies the sort indicator display state for each column **/
     sortIndicators?: SortIndicatorsProps | undefined;
+    /** Specifies if every alternate row has a different background colour */
     alternatingRows?: boolean | undefined;
-    renderCustomEmptyView?: (() => ReactNode) | undefined;
-    loadState?: LoadStateType | undefined;
+    /** Override props for the empty view display */
     emptyView?: ErrorDisplayAttributes | undefined;
+    /** Returns a custom view to be rendered when the table is empty */
+    renderCustomEmptyView?: (() => ReactNode) | undefined;
 }
 
 export type LoadStateType = "loading" | "success";
@@ -39,11 +43,17 @@ export interface SortIndicatorsProps {
     [fieldKey: string]: SortIndicatorType;
 }
 
-/** label text. Rest defaults to fieldKey=label, clickable=false **/
+/**
+ * When a string is specified, it will be used as the header label and
+ * the key of the value in the row item object.
+ *
+ * For additional customisation, pass a `HeaderItemProps` object
+ */
 export type HeaderProps = string | HeaderItemProps;
+
 interface HeaderItemProps {
     fieldKey: string;
-    /** (technically ReactNode also includes string, but this makes it more obvious for devs) **/
+    // technically ReactNode also includes string, but this is more explicit
     label: string | ReactNode;
     clickable?: boolean | undefined;
     style?: CSSProperties | undefined;
@@ -51,7 +61,7 @@ interface HeaderItemProps {
 
 export interface RowProps {
     id: string | number;
-    /** data with keys matching fieldKey **/
+    /** data with key matching fieldKey in `HeaderProps` */
     [fieldKey: string]:
         | string
         | number
