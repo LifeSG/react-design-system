35f84802205be5c0dbbb07ad260c104bfaaa5df9
Keith Tan <keith_xw_tan@tech.gov.sg>
2023-10-15 16:22:58 +0800

    [CCUBE-489][KT] Create column div component
    [CCUBE-489][KT] Create column div component

--- src/layout/types.ts
+++ src/layout/types.ts
@@ -17,3 +17,44 @@ export interface ContainerProps extends CommonLayoutProps {
 export interface ContentProps extends ContainerProps {}
 
 export type DivRef = React.Ref<HTMLDivElement>;
+
+type MobileCol = 1 | 2 | 3 | 4;
+type MobileColRange = MobileCol | 5;
+
+type TabletCol = MobileCol | 5 | 6 | 7 | 8;
+type TabletColRange = TabletCol | 9;
+
+type DesktopCol = TabletCol | 9 | 10 | 11 | 12;
+type DesktopColRange = DesktopCol | 13;
+
+export interface ColDivProps extends React.HTMLAttributes<HTMLDivElement> {
+    "data-testid"?: string | undefined;
+    /**
+     * Specifies the number of columns to be span across in mobile viewports.
+     * If an array is specified, the format is as such [startCol, endCol].
+     * If `tabletCols` or `desktopCols` are not specified, this
+     * setting will be applied to tablet and desktop viewports.
+     *
+     * If all column props are not specified, the div will span across a single
+     * column.
+     */
+    mobileCols?: MobileCol | MobileColRange[] | undefined;
+    /**
+     * Specifies the number of columns to be span across in tablet viewports.
+     * If an array is specified, the format is as such [startCol, endCol].
+     * If `desktopCols` are not specified, this setting will be
+     * applied to desktop viewports as well.
+     *
+     * If all column props are not specified, the div will span across a single
+     * column.
+     */
+    tabletCols?: TabletCol | TabletColRange[] | undefined;
+    /**
+     * Specifies the number of columns to be span across in desktop viewports.
+     * If an array is specified, the format is as such [startCol, endCol].
+     *
+     * If all column props are not specified, the div will span across a single
+     * column.
+     */
+    desktopCols?: DesktopCol | DesktopColRange[] | undefined;
+}
