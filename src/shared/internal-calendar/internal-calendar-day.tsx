9621a3d4ff3a8c44ee24b80f9a6c65366c55b59b
Quek Ruo Ling <ruoling.quek@ufinity.com>
2023-10-27 13:09:00 +0800

    [CCUBE-303][RL] Refactor calendar day view to use common day cell

--- src/shared/internal-calendar/internal-calendar-day.tsx
+++ src/shared/internal-calendar/internal-calendar-day.tsx
@@ -3,10 +3,9 @@ import isBetween from "dayjs/plugin/isBetween";
 import { useMemo, useState } from "react";
 import { Text } from "../../text/text";
 import { CalendarHelper } from "../../util/calendar-helper";
-import { CommonCalendarProps, FocusType, InternalCalendarProps } from "./types";
-import { CalendarDayCell } from "./calendar-day-cell";
-import { CalendarDayStyleHelper } from "./calendar-day-style-helper";
+import { DayMonthCell } from "./day-of-month/day-of-month-cell";
 import { HeaderCell, RowDayCell, Wrapper } from "./internal-calendar-day.style";
+import { CommonCalendarProps, FocusType, InternalCalendarProps } from "./types";
 
 dayjs.extend(isBetween);
 
@@ -78,79 +77,6 @@ export const InternalCalendarDay = ({
     };
 
     // =============================================================================
-    // HELPER FUNCTIONS
-    // =============================================================================
-    const generateDayStatus = (day: Dayjs) => {
-        const variant: DayVariant =
-            calendarDate.month() !== day.month()
-                ? "other-month"
-                : dayjs().isSame(day, "day")
-                ? "today"
-                : "default";
-
-        return {
-            variant,
-        };
-    };
-
-    const getHoverDirection = (): HoverDirection => {
-        if (!hoverValue || variant === "single") {
-            return null;
-        }
-
-        const hoverDay = dayjs(hoverValue);
-
-        if (selectedStartDate && selectedEndDate) {
-            if (hoverDay.isBefore(selectedStartDate)) {
-                if (currentFocus === "start") {
-                    return "full-overlap-start";
-                } else if (currentFocus === "end") {
-                    return "reset-end";
-                }
-            } else if (hoverDay.isAfter(selectedEndDate)) {
-                if (currentFocus === "end") {
-                    return "full-overlap-end";
-                } else if (currentFocus === "start") {
-                    return "reset-start";
-                }
-            } else if (
-                hoverDay.isBetween(
-                    selectedStartDate,
-                    selectedEndDate,
-                    "day",
-                    "[]"
-                ) &&
-                ![selectedStartDate, selectedEndDate].includes(hoverValue)
-            ) {
-                if (currentFocus === "start") {
-                    return "overlap-start";
-                } else if (currentFocus === "end") {
-                    return "overlap-end";
-                }
-            }
-        }
-
-        // handle hovering to start/end if didn't exist another selected value
-        if (selectedStartDate && !selectedEndDate) {
-            if (hoverDay.isAfter(selectedStartDate)) {
-                if (currentFocus === "end") {
-                    return "hover-end";
-                }
-            }
-        } else if (!selectedStartDate && selectedEndDate) {
-            if (hoverDay.isBefore(selectedEndDate)) {
-                if (currentFocus === "start") {
-                    return "hover-start";
-                }
-            }
-        }
-
-        return null;
-    };
-
-    const hoverDirection: HoverDirection = getHoverDirection();
-
-    // =============================================================================
     // RENDER FUNCTIONS
     // =============================================================================
     const renderHeader = () => {
@@ -166,38 +92,22 @@ export const InternalCalendarDay = ({
             return (
                 <RowDayCell key={weekIndex} onMouseLeave={handleMouseLeaveCell}>
                     {week.map((day, dayIndex) => {
-                        const { variant } = generateDayStatus(day);
-                        const {
-                            styleLeftProps,
-                            styleRightProps,
-                            styleCircleProps,
-                            styleLabelProps,
-                        } = CalendarDayStyleHelper.generateStyleProps(
-                            day,
-                            selectedStartDate,
-                            selectedEndDate,
-                            hoverValue,
-                            hoverDirection,
-                            currentFocus,
-                            minDate,
-                            maxDate,
-                            disabledDates,
-                            allowDisabledSelection,
-                            isNewSelection
-                        );
-
                         return (
-                            <CalendarDayCell
+                            <DayMonthCell
                                 key={`day-${dayIndex}`}
-                                type="regular"
-                                dayDate={day}
-                                variant={variant}
-                                styleLeftProps={styleLeftProps}
-                                styleRightProps={styleRightProps}
-                                styleCircleProps={styleCircleProps}
-                                styleLabelProps={styleLabelProps}
-                                onDayClick={handleDayClick}
-                                onHoverCell={handleHoverCell}
+                                date={day}
+                                calendarDate={calendarDate}
+                                startDate={selectedStartDate}
+                                endDate={selectedEndDate}
+                                hoverDate={hoverValue}
+                                currentFocus={currentFocus}
+                                minDate={minDate}
+                                maxDate={maxDate}
+                                disabledDates={disabledDates}
+                                allowDisabledSelection={allowDisabledSelection}
+                                isNewSelection={isNewSelection}
+                                onSelect={handleDayClick}
+                                onHover={handleHoverCell}
                             />
                         );
                     })}
