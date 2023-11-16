417781585ef271f2ece5198307248285fc53040c
dave <daiproxomhoa@gmail.com>
2023-11-16 09:32:32 +0700

    [CCUBE-681][DV] DateInput component - NaN display

--- src/shared/standalone-date-input/standalone-date-input.tsx
+++ src/shared/standalone-date-input/standalone-date-input.tsx
@@ -276,7 +276,7 @@ export const Component = (
         if (!stringVal) {
             return [undefined, undefined, undefined];
         } else {
-            const day = dayjs(stringVal, "YYYY-MM-DD");
+            const day = dayjs(stringVal);
 
             return [
                 StringHelper.padValue(day.date().toString()),
