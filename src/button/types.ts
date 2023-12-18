1cf2b1d4b5f3223271f7233c30c63f32081ed19d
Choo Chen Wei <chenwei.choo@ufinity.com>
2023-12-18 13:51:35 +0800

    [CCUBE-764][CCW] introduce danger prop for Danger button

--- src/button/types.ts
+++ src/button/types.ts
@@ -2,13 +2,13 @@ import React from "react";
 import { ComponentLoadingSpinnerProps } from "src/shared/component-loading-spinner/component-loading-spinner";
 
 type StyleType = "default" | "secondary" | "light" | "link";
-
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     /** The style type of the button. Values: "default" | "secondary" | "light" | "link" */
     styleType?: StyleType | undefined;
     /** Indicates if a loading spinner is to be displayed */
     loading?: boolean | undefined;
+    danger?: boolean | undefined;
 }
 
 export type ButtonRef = React.Ref<HTMLButtonElement>;
@@ -23,4 +23,5 @@ export type MainButtonSize = "default" | "small";
 export interface MainStyleProps extends ComponentLoadingSpinnerProps {
     $buttonStyle: MainButtonStyle;
     $buttonSizeStyle?: MainButtonSize | undefined;
+    $buttonIsDanger?: boolean;
 }
