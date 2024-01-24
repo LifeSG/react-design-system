de7826c5a9368f45a88b8f94edefd5e3263cf9d7
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-01-24 22:35:05 +0800

    [CCUBE-895][RL] Rework form label addon

--- src/popover-v2/popover-trigger.styles.tsx
+++ src/popover-v2/popover-trigger.styles.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 // STYLING
 // =============================================================================
 export const TriggerContainer = styled.div`
+    display: inline;
     width: fit-content;
     cursor: pointer;
+    position: relative;
 `;
