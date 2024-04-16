508731f7ef4f1df2fd31e9d60711b6abf8a33734
Daniel Sim <daniel_sim@tech.gov.sg>
2024-04-16 17:48:48 +0800

    [RBS-1266][DS] updated NotficationBanner component stories

--- stories/notification-banner/notification-banner.stories.tsx
+++ stories/notification-banner/notification-banner.stories.tsx
@@ -229,3 +229,42 @@ export const CustomContent: StoryObj<Component> = {
         );
     },
 };
+
+export const MaxLines: StoryObj<Component> = {
+    render: () => {
+        return (
+            <div
+                style={{
+                    position: "relative",
+                    paddingTop: "1.5rem",
+                    maxHeight: "21rem",
+                }}
+            >
+                <NotificationBanner maxLines={3}>
+                    This is a notification banner with maxLines set. Lorem ipsum
+                    dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                    ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    dolor in reprehenderit in voluptate velit esse cillum dolore
+                    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim
+                    id est laborum.
+                </NotificationBanner>
+                <StyledContent>
+                    <Text.Body paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </Text.Body>
+                </StyledContent>
+            </div>
+        );
+    },
+};
