48da2506e3f325e4b1c2656593af239e8ac4d7fd
Keith Tan <keith_xw_tan@tech.gov.sg>
2024-05-02 14:46:35 +0800

    [CCUBE-1289][KT] Fix icon shrinking if button content requires more space

--- src/button-with-icon/button-with-icon.style.tsx
+++ src/button-with-icon/button-with-icon.style.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Main } from "../button/button.style";
 import { MainStyleProps } from "../button/types";
 import { ButtonIconPosition } from "./types";
@@ -17,4 +17,27 @@ export const MainButtonWithIcon = styled(Main)<MainStylePropsWithIcon>`
     flex-direction: ${(props) =>
         props.$buttonIconPosition === "right" ? "row-reverse" : "row"};
     gap: 0.5rem;
+
+    svg {
+        flex-shrink: 0;
+    }
+
+    ${(props) => {
+        switch (props.$buttonSizeStyle) {
+            case "small":
+                return css`
+                    svg {
+                        height: 1rem;
+                        width: 1rem;
+                    }
+                `;
+            default:
+                return css`
+                    svg {
+                        height: 1.125rem;
+                        width: 1.125rem;
+                    }
+                `;
+        }
+    }}
 `;
