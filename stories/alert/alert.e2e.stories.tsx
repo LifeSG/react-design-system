cf6b981e2d741e78a5270f1e1d174562f81b8f15
roll <quek.ruoling@gmail.com>
2024-07-19 21:08:07 +0800

    [TESTS][RL] Add alert e2e tests

---
+++ stories/alert/alert.e2e.stories.tsx
@@ -0,0 +1,103 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Alert } from "src/alert";
+
+type Component = typeof Alert;
+
+const meta: Meta<Component> = {
+    title: "e2e-Alert",
+    parameters: { layout: "fullscreen" },
+    decorators: [
+        (Story) => (
+            <div style={{ margin: "1rem", width: "500px", maxWidth: "90vw" }}>
+                <Story />
+            </div>
+        ),
+    ],
+    tags: ["e2e"],
+};
+
+export default meta;
+
+const BODY =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies placerat tortor, at mattis ex congue sollicitudin.";
+
+export const WithoutIcon: StoryObj<Component> = {
+    render: () => {
+        return (
+            <>
+                <Alert data-testid="e2e" type="error">
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="warning">
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="success">
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="info">
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="description">
+                    {BODY}
+                </Alert>
+            </>
+        );
+    },
+};
+
+export const WithIcon: StoryObj<Component> = {
+    render: () => {
+        return (
+            <>
+                <Alert data-testid="e2e" type="error" showIcon>
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="warning" showIcon>
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="success" showIcon>
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="info" showIcon>
+                    {BODY}
+                </Alert>
+                <Alert data-testid="e2e" type="description" showIcon>
+                    {BODY}
+                </Alert>
+            </>
+        );
+    },
+};
+
+export const WithLink: StoryObj<Component> = {
+    render: () => {
+        return (
+            <Alert
+                data-testid="e2e"
+                type="warning"
+                showIcon
+                actionLink={{ children: "Hyperlink" }}
+            >
+                {BODY}
+            </Alert>
+        );
+    },
+};
+
+export const WithMaxCollapsedHeight: StoryObj<Component> = {
+    render: () => {
+        return (
+            <Alert
+                data-testid="e2e"
+                type="warning"
+                showIcon
+                maxCollapsedHeight={50}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+                doloribus porro dolore dolor nulla est. Deleniti quasi
+                consequuntur magni, maxime, fuga molestias vero adipisci
+                quibusdam facere quisquam blanditiis? Corrupti, mollitia?
+            </Alert>
+        );
+    },
+};
