405a60fdbcbadf3ebb75b3a0aa9564400bc7edf2
qroll <quek.ruoling@gmail.com>
2024-07-23 10:15:44 +0800

    [FLOAT][RL] Update prop documentation

--- stories/form/form-multi-select/props-table.tsx
+++ stories/form/form-multi-select/props-table.tsx
@@ -132,6 +132,20 @@ const DATA: ApiTableSectionProps[] = [
                 propTypes: [`"small"`, `"default"`],
                 defaultValue: `"default"`,
             },
+            {
+                name: "alignment",
+                description:
+                    "Specifies if the dropdown is aligned to the left or right of the main field",
+                propTypes: [`"left"`, `"right"`],
+                defaultValue: `"left"`,
+            },
+            {
+                name: "dropdownZIndex",
+                description:
+                    "The custom z-index of the dropdown. Try specifying this if you encounter z-index conflicts.",
+                propTypes: ["number"],
+                defaultValue: "50",
+            },
         ],
     },
     ...SHARED_FORM_PROPS_DATA,
