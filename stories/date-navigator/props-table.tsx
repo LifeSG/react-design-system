daf5f0b92ce3ca79dd9b4714c5f1b39c7ac8c6be
Tan Jing Zhi <jingzhi.t_dcube@scientecpeople.com>
2024-09-26 13:21:51 +0800

    [BOOKINGSG-6090-date-navigator][JZ|FY] Added stories and docs for DateNavigator

---
+++ stories/date-navigator/props-table.tsx
@@ -0,0 +1,62 @@
+import { ApiTable } from "../storybook-common/api-table";
+import { ApiTableSectionProps } from "../storybook-common/api-table/types";
+
+const DATA: ApiTableSectionProps[] = [
+    {
+        attributes: [
+            {
+                name: "data-testid",
+                description: "The test identifier of the component",
+                propTypes: ["string"],
+            },
+            {
+                name: "className",
+                description: "The class selector of the component",
+                propTypes: ["string"],
+            },
+            {
+                name: "selectedDate",
+                mandatory: true,
+                description: (
+                    <>
+                        The date string to display, any format that is
+                        acceptable by <code>Dayjs</code> will work.
+                    </>
+                ),
+                propTypes: ["string"],
+            },
+            {
+                name: "minDate",
+                description:
+                    "The minimum date that is allowed to be navigated to.",
+                propTypes: ["string"],
+            },
+            {
+                name: "maxDate",
+                description:
+                    "The maximum date that is allowed to be navigated to.",
+                propTypes: ["string"],
+            },
+            {
+                name: "isLoading",
+                description: "The loading state to disable navigation buttons.",
+                defaultValue: "false",
+                propTypes: ["boolean"],
+            },
+            {
+                name: "onLeftArrowClick",
+                description:
+                    "The callback function which will be called on left arrow navigation button click.",
+                propTypes: ["(currentDate: string) => void"],
+            },
+            {
+                name: "onRightArrowClick",
+                description:
+                    "The callback function which will be called on right arrow navigation button click.",
+                propTypes: ["(currentDate: string) => void"],
+            },
+        ],
+    },
+];
+
+export const PropsTable = () => <ApiTable sections={DATA} />;
