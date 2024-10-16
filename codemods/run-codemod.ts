fdb825420ccfcbc56515e8020adc79044866f3c4
roll <quek.ruoling@gmail.com>
2024-10-16 22:29:09 +0800

    [V3][RL] Update codemod prompts for clarity

--- codemods/run-codemod.ts
+++ codemods/run-codemod.ts
@@ -1,7 +1,7 @@
+import { checkbox, confirm, input, select } from "@inquirer/prompts";
 import { execSync } from "child_process";
 import * as fs from "fs";
 import * as path from "path";
-import { checkbox, confirm, input, select } from "@inquirer/prompts";
 
 const codemodsDir: string = path.join(process.cwd(), "codemods");
 
@@ -22,23 +22,25 @@ const theme = {
     helpMode: "always" as const,
 };
 
-const codemodDescriptions: { [key in Codemod]?: string } = {
-    [Codemod.DeprecateV2Tokens]: "This is to deprecate previous tokens to V2",
-    [Codemod.MigrateColour]:
-        "This is to migrate V2_Color to its respective Colour tokens and usages.",
-    [Codemod.MigrateLayout]:
-        "This is to migrate V2_Layout to its new Layout component and respective column usages",
+const CodemodDescriptions: { [key in Codemod]: string } = {
+    [Codemod.DeprecateV2Tokens]: "Migrate deprecated V2 imports",
+    [Codemod.MigrateColour]: "Replace V2_Color with new Colour tokens",
+    [Codemod.MigrateLayout]: "Replace V2_Layout with new Layout components",
     [Codemod.MigrateMediaQuery]:
-        "This is to migrate V2 mediaquery helper function to V3 and its new specs.",
-    [Codemod.MigrateText]:
-        "This is to migrate V2_text to its new Typography Component and update its previous usages.",
+        "Replace V2 media queries with new Breakpoint tokens",
+    [Codemod.MigrateText]: "Replace V2_Text with new Typography components",
 };
 
-// Shortcuts
-const predefinedPaths: { [key: string]: string } = {
-    Src: "src",
-    Codebase: ".",
+const TargetDirectoryPaths = {
+    src: "src",
+    codebase: ".",
 };
+
+interface UserSelection {
+    selectedCodemods: string[];
+    selectedTheme: Theme | null;
+    targetPath: string;
+}
 
 // Return codemods in codemod folder
 function listCodemods(): { name: string; value: string }[] {
@@ -49,33 +51,31 @@ function listCodemods(): { name: string; value: string }[] {
     const options = codemods.map((mod) => ({
         name: mod,
         value: mod,
-        description: codemodDescriptions[mod as Codemod],
+        description: CodemodDescriptions[mod as Codemod],
     }));
 
     return options;
 }
 
-function runCodemods(
-    codemodsToRun: string[],
-    outputPath: string,
-    options?: { "migrate-colour"?: Theme }
-): void {
-    codemodsToRun.forEach((codemod) => {
-        const codemodPath = path.join(codemodsDir, codemod, "index.ts");
-        let command = `jscodeshift --parser=tsx -t ${codemodPath} ${outputPath}`;
+function runCodemods(selection: UserSelection): void {
+    const { selectedCodemods, targetPath, selectedTheme } = selection;
 
-        if (codemod === Codemod.MigrateColour && options?.["migrate-colour"]) {
-            command = `jscodeshift --parser=tsx -t ${codemodPath} --mapping=${options["migrate-colour"]} ${outputPath}`;
+    selectedCodemods.forEach((codemod) => {
+        const codemodPath = path.join(codemodsDir, codemod, "index.ts");
+        let command = `jscodeshift --parser=tsx -t ${codemodPath} ${targetPath}`;
+
+        if (codemod === Codemod.MigrateColour && selectedTheme) {
+            command = `jscodeshift --parser=tsx -t ${codemodPath} --mapping=${selectedTheme} ${targetPath}`;
         }
 
         console.log(
-            `Running codemod: ${codemod} on output path: ${outputPath}`
+            `Running codemod: ${codemod} on target path: ${targetPath}`
         );
 
         try {
             execSync(command, { stdio: "inherit" });
             console.log(
-                `Codemod ${codemod} executed successfully on ${outputPath}.`
+                `Codemod ${codemod} executed successfully on ${targetPath}`
             );
         } catch (error) {
             console.error(
@@ -87,35 +87,34 @@ function runCodemods(
     });
 }
 
-// Choose path for codemod use
-async function chooseOutputPath(): Promise<string | null> {
+async function chooseTargetPath(): Promise<string | null> {
     const options = [
-        { name: "src (./src)", value: predefinedPaths.Src },
-        { name: "current directory (./)", value: predefinedPaths.Codebase },
+        {
+            name: "src (./src)",
+            value: TargetDirectoryPaths.src,
+        },
+        {
+            name: "current directory (./)",
+            value: TargetDirectoryPaths.codebase,
+        },
         { name: "custom", value: "custom" },
     ];
 
     const selectedOption = await select({
-        message: "Choose an output path:",
+        message: "Choose a target path:",
         choices: options,
     });
 
     if (selectedOption === "custom") {
         const customPath = await input({
             message: "Enter your custom path:",
+            required: true,
         });
-
-        if (!customPath) {
-            console.log("No custom path provided. Canceling.");
-            return null;
-        }
 
         const resolvedPath = path.resolve(customPath);
 
         if (!fs.existsSync(resolvedPath)) {
-            console.error(
-                `The path "${resolvedPath}" does not exist. Please try again.`
-            );
+            console.error(`The path "${resolvedPath}" does not exist`);
             return null;
         }
 
@@ -132,22 +131,17 @@ async function chooseTheme(): Promise<Theme | null> {
     ];
 
     const selectedTheme = await select({
-        message: "Select the theme to follow for Color migration:",
+        message: "Select the theme that your project is using:",
         choices: themeOptions,
     });
 
     return selectedTheme;
 }
 
-// Get codemod selections
-async function getCodemodSelections(): Promise<{
-    selectedCodemods: string[];
-    selectedTheme: Theme | null;
-    outputPath: string;
-}> {
+async function getCodemodSelections(): Promise<UserSelection> {
     const codemods = listCodemods();
     if (codemods.length === 0) {
-        throw new Error("No codemod scripts found.");
+        throw new Error("No codemod scripts found");
     }
 
     const selectedCodemods = await checkbox({
@@ -162,46 +156,52 @@ async function getCodemodSelections(): Promise<{
         selectedTheme = await chooseTheme();
     }
 
-    const outputPath = await chooseOutputPath();
-    if (!outputPath) {
-        throw new Error("No output path selected or provided.");
+    const targetPath = await chooseTargetPath();
+    if (!targetPath) {
+        throw new Error("No target path selected or provided");
     }
 
-    return { selectedCodemods, selectedTheme, outputPath };
+    return { selectedCodemods, selectedTheme, targetPath };
+}
+
+async function getConfirmation(selection: UserSelection) {
+    const { selectedCodemods, selectedTheme, targetPath } = selection;
+
+    const codemods = selectedCodemods.join(", ");
+    const finalConfirmationMessage =
+        `\nYou are about to run the following codemods: ${codemods}\n` +
+        `Target path: ${targetPath}\n` +
+        `${
+            selectedTheme
+                ? `Selected theme for "migrate-colour": ${selectedTheme}\n`
+                : ""
+        }`;
+
+    console.log(finalConfirmationMessage);
+
+    const finalConfirmation = await confirm({
+        message: "Do you want to proceed?",
+        default: true,
+    });
+
+    return finalConfirmation;
 }
 
 async function main(): Promise<void> {
     try {
-        const { selectedCodemods, selectedTheme, outputPath } =
-            await getCodemodSelections();
+        const selection = await getCodemodSelections();
 
-        const finalConfirmationMessage = `You are about to run the following codemods: ${selectedCodemods.join(
-            ", "
-        )}\nOutput path: ${outputPath}${
-            selectedTheme
-                ? `\nSelected theme for "migrate-colour": ${selectedTheme}`
-                : ""
-        }.\nDo you want to proceed?`;
-
-        const finalConfirmation = await confirm({
-            message: finalConfirmationMessage,
-            default: true,
-        });
+        const finalConfirmation = await getConfirmation(selection);
 
         if (!finalConfirmation) {
             return;
         }
 
-        runCodemods(
-            selectedCodemods,
-            outputPath,
-            selectedTheme ? { "migrate-colour": selectedTheme } : undefined
-        );
+        runCodemods(selection);
     } catch (error) {
         if (error.name === "ExitPromptError") {
-            console.log("Selection process interrupted.");
+            // user exited, do nothing
         } else {
-            console.error("An unexpected error occurred:", error);
             throw error;
         }
     }
