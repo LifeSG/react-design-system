7909c0d9f962ab86572042d669a012999c282286
Anu Mutyala <mutyala.lakshmi@ufinity.com>
2024-11-07 15:51:23 +0800

    [MOL-16700][AM] Add section headers

--- src/local-nav/local-nav-dropdown/local-nav-dropdown.tsx
+++ src/local-nav/local-nav-dropdown/local-nav-dropdown.tsx
@@ -13,189 +13,198 @@ import {
     StyledTickIcon,
 } from "./local-nav-dropdown.styles";
 
-const DropdownNavItem = (props: LocalNavItemComponentProps) => {
-    const { handleClick, isSelected, item, renderItem } = props;
+const Component = (
+    {
+        defaultLabel,
+        stickyOffset = 0,
+        onNavItemSelect,
+        items,
+        selectedItemIndex,
+        id,
+        "data-testid": testId,
+        className,
+        renderItem,
+    }: LocalNavDropdownProps,
+    ref: React.Ref<HTMLElement>
+): JSX.Element => {
+    // =============================================================================
+    // CONST, STATE, REF
+    // =============================================================================
+    const detectStickyRef = useRef<HTMLSpanElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const [isStickied, setIsStickied] = useState<boolean>(false);
+    const [labelText, setLabelText] = useState(defaultLabel);
+    const [isDropdownExpanded, setIsDropdownExpanded] =
+        useState<boolean>(false);
+    const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+    const [dropdowntHeight, setDropdownHeight] = useState(window.innerHeight);
+    const navTestId = testId || "local-nav-dropdown";
 
-    const renderTitle = () => {
-        if (renderItem) {
-            return renderItem(item, { selected: isSelected });
-        } else if (typeof item.title === "string") {
-            return (
-                <>
-                    {isSelected && <StyledTickIcon />}
-                    <LabelText id={`${item.id}-label`} isSelected={isSelected}>
-                        {item.title}
-                    </LabelText>
-                </>
-            );
-        } else if (React.isValidElement(item.title)) {
-            return item.title;
+    // =============================================================================
+    // EFFECTS, EVENT LISTENERS
+    // ============================================================================
+
+    useEffect(() => {
+        if (dropdownRef.current) {
+            const dropdownHeight =
+                dropdownRef.current.getBoundingClientRect().height;
+            setDropdownHeight(dropdownHeight);
         }
-        return null;
+    }, []);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setViewportHeight(window.innerHeight);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        const callback = (entries: IntersectionObserverEntry[]) => {
+            const [entry] = entries;
+            setIsStickied(!entry.isIntersecting);
+        };
+
+        const observer = new IntersectionObserver(callback, {
+            threshold: 1,
+            rootMargin: `-${stickyOffset}px 0px 0px 0px`,
+        });
+
+        if (detectStickyRef.current) {
+            observer.observe(detectStickyRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, [stickyOffset]);
+
+    useEffect(() => {
+        const visibleSectionTitle =
+            selectedItemIndex >= 0
+                ? items[selectedItemIndex].title
+                : defaultLabel;
+        setLabelText(visibleSectionTitle);
+    }, [selectedItemIndex, isStickied, items, defaultLabel]);
+
+    useEffect(() => {
+        document.body.style.overflow =
+            isDropdownExpanded && isStickied ? "hidden" : "auto";
+    }, [isDropdownExpanded, isStickied]);
+
+    const handleToggleDropdown = useCallback(() => {
+        console.log("came int here");
+        setIsDropdownExpanded((prev) => !prev);
+    }, []);
+
+    const handleDismissBackdrop = useCallback(() => {
+        setIsDropdownExpanded(false);
+    }, []);
+
+    // =============================================================================
+    // EVENT HANDLERS
+    // =============================================================================
+
+    const handleNavItemClick = (
+        e: MouseEvent,
+        item: LocalNavItemProps,
+        index: number
+    ) => {
+        if (onNavItemSelect) {
+            onNavItemSelect(e, item, index);
+        }
+        setIsDropdownExpanded(false);
+    };
+
+    // =============================================================================
+    // RENDER FUNCTIONS
+    // =============================================================================
+
+    const renderDropdownNavItem = ({
+        handleClick,
+        isSelected,
+        item,
+        renderItem,
+    }: LocalNavItemComponentProps) => {
+        const { id = "local-nav-dropdown", title } = item;
+
+        const renderTitle = () => {
+            if (renderItem) {
+                return renderItem(item, { selected: isSelected });
+            } else if (typeof title === "string") {
+                return (
+                    <>
+                        {isSelected && <StyledTickIcon />}
+                        <LabelText id={`${id}-label`} isSelected={isSelected}>
+                            {title}
+                        </LabelText>
+                    </>
+                );
+            } else if (React.isValidElement(title)) {
+                return title;
+            }
+            return null;
+        };
+
+        return (
+            <NavItem id={id} isSelected={isSelected} onClick={handleClick}>
+                {renderTitle()}
+            </NavItem>
+        );
     };
 
     return (
-        <NavItem id={item.id} isSelected={isSelected} onClick={handleClick}>
-            {renderTitle()}
-        </NavItem>
+        <>
+            <span
+                ref={detectStickyRef}
+                id={id || "sticky-ref"}
+                data-testid={testId || "sticky-ref"}
+                className={className || "sticky-ref"}
+            />
+            <NavWrapper
+                isStickied={isStickied}
+                stickyOffset={stickyOffset}
+                ref={ref}
+                id={`${navTestId}-wrapper`}
+                data-testid={`${navTestId}-wrapper`}
+                className={`${navTestId}-wrapper`}
+            >
+                <NavLabel
+                    ref={dropdownRef}
+                    onClick={handleToggleDropdown}
+                    data-testid={`${navTestId}-label`}
+                >
+                    <LabelText weight="bold">{labelText}</LabelText>
+                    <NavIcon isDropdownExpanded={isDropdownExpanded} />
+                </NavLabel>
+                {isDropdownExpanded && (
+                    <NavItemList
+                        data-testid={`${navTestId}-dropdown-list`}
+                        viewportHeight={
+                            viewportHeight - dropdowntHeight - stickyOffset
+                        }
+                    >
+                        {items.map((item, i) =>
+                            renderDropdownNavItem({
+                                handleClick: (e) =>
+                                    handleNavItemClick?.(
+                                        e.nativeEvent as MouseEvent,
+                                        { title: item.title, id: item.id },
+                                        i
+                                    ),
+                                isSelected:
+                                    i === selectedItemIndex && isStickied,
+                                item: { title: item.title, id: item.id },
+                                renderItem,
+                            })
+                        )}
+                    </NavItemList>
+                )}
+                {isDropdownExpanded && isStickied && (
+                    <Backdrop onClick={handleDismissBackdrop} />
+                )}
+            </NavWrapper>
+        </>
     );
 };
 
-export const LocalNavDropdown = React.forwardRef<
-    HTMLElement,
-    LocalNavDropdownProps
->(
-    (
-        {
-            defaultLabel,
-            stickyOffset = 0,
-            onNavItemSelect,
-            items,
-            selectedItemIndex,
-            id,
-            "data-testid": testId,
-            className,
-            renderItem,
-        }: LocalNavDropdownProps,
-        ref
-    ) => {
-        const detectStickyRef = useRef<HTMLSpanElement>(null);
-        const dropdownRef = useRef<HTMLDivElement>(null);
-        const [isStickied, setIsStickied] = useState<boolean>(false);
-        const [labelText, setLabelText] = useState(defaultLabel);
-        const [isDropdownExpanded, setIsDropdownExpanded] =
-            useState<boolean>(false);
-        const [viewportHeight, setViewportHeight] = useState(
-            window.innerHeight
-        );
-        const [dropdowntHeight, setDropdownHeight] = useState(
-            window.innerHeight
-        );
-
-        useEffect(() => {
-            if (dropdownRef.current) {
-                const dropdownHeight =
-                    dropdownRef.current.getBoundingClientRect().height;
-                setDropdownHeight(dropdownHeight);
-            }
-        }, []);
-
-        useEffect(() => {
-            const handleResize = () => {
-                setViewportHeight(window.innerHeight);
-            };
-            window.addEventListener("resize", handleResize);
-            return () => window.removeEventListener("resize", handleResize);
-        }, []);
-
-        useEffect(() => {
-            const callback = (entries: IntersectionObserverEntry[]) => {
-                const [entry] = entries;
-                setIsStickied(!entry.isIntersecting);
-            };
-
-            const observer = new IntersectionObserver(callback, {
-                threshold: 1,
-                rootMargin: `-${stickyOffset}px 0px 0px 0px`,
-            });
-
-            if (detectStickyRef.current) {
-                observer.observe(detectStickyRef.current);
-            }
-
-            return () => observer.disconnect();
-        }, [stickyOffset]);
-
-        const handleToggleDropdown = useCallback(() => {
-            setIsDropdownExpanded((prev) => !prev);
-        }, []);
-
-        useEffect(() => {
-            const visibleSectionTitle =
-                selectedItemIndex >= 0
-                    ? items[selectedItemIndex].title
-                    : defaultLabel;
-            setLabelText(visibleSectionTitle);
-        }, [selectedItemIndex, isStickied, items, defaultLabel]);
-
-        useEffect(() => {
-            document.body.style.overflow =
-                isDropdownExpanded && isStickied ? "hidden" : "auto";
-        }, [isDropdownExpanded, isStickied]);
-
-        const handleNavItemClick = (
-            e: MouseEvent,
-            item: LocalNavItemProps,
-            index: number
-        ) => {
-            if (onNavItemSelect) {
-                onNavItemSelect(e, item, index);
-            }
-            setIsDropdownExpanded(false);
-        };
-
-        const handleDismissBackdrop = useCallback(() => {
-            setIsDropdownExpanded(false);
-        }, []);
-
-        const navTestId = testId || "local-nav-dropdown";
-
-        return (
-            <>
-                <span
-                    ref={detectStickyRef}
-                    id={id || "sticky-ref"}
-                    data-testid={testId || "sticky-ref"}
-                    className={className || "sticky-ref"}
-                />
-                <NavWrapper
-                    isStickied={isStickied}
-                    stickyOffset={stickyOffset}
-                    ref={ref}
-                    id={`${navTestId}-wrapper`}
-                    data-testid={`${navTestId}-wrapper`}
-                    className={`${navTestId}-wrapper`}
-                >
-                    <NavLabel
-                        ref={dropdownRef}
-                        onClick={handleToggleDropdown}
-                        data-testid={`${navTestId}-label`}
-                    >
-                        <LabelText weight="bold">{labelText}</LabelText>
-                        <NavIcon isDropdownExpanded={isDropdownExpanded} />
-                    </NavLabel>
-                    {isDropdownExpanded && (
-                        <NavItemList
-                            data-testid={`${navTestId}-dropdown-list`}
-                            viewportHeight={
-                                viewportHeight - dropdowntHeight - stickyOffset
-                            }
-                        >
-                            {items.map((item, i) => (
-                                <DropdownNavItem
-                                    key={i}
-                                    data-testid={`${navTestId}-item--${i}`}
-                                    handleClick={(e) =>
-                                        handleNavItemClick(
-                                            e.nativeEvent as MouseEvent,
-                                            { title: item.title, id },
-                                            i
-                                        )
-                                    }
-                                    isSelected={
-                                        i === selectedItemIndex && isStickied
-                                    }
-                                    item={{ title: item.title, id }}
-                                    renderItem={renderItem}
-                                />
-                            ))}
-                        </NavItemList>
-                    )}
-                    {isDropdownExpanded && isStickied && (
-                        <Backdrop onClick={handleDismissBackdrop} />
-                    )}
-                </NavWrapper>
-            </>
-        );
-    }
-);
+export const LocalNavDropdown = React.forwardRef(Component);
--- src/local-nav/local-nav-menu/local-nav-menu.tsx
+++ src/local-nav/local-nav-menu/local-nav-menu.tsx
@@ -5,84 +5,90 @@ import { LocalNavItemComponentProps } from "../internal-types";
 import { LocalNavMenuProps } from "../types";
 import { Nav, NavItem } from "./local-nav-menu.styles";
 
-const LocalNavItem = (props: LocalNavItemComponentProps) => {
-    const { handleClick, isSelected, item, renderItem } = props;
-    const renderTitle = () => {
-        if (renderItem) {
-            return renderItem(item, { selected: isSelected });
-        } else if (typeof item.title === "string") {
-            return (
-                <Text.Body
-                    style={{ margin: 0 }}
-                    weight={isSelected ? "semibold" : "regular"}
-                    id={item.id + "-title"}
-                >
-                    {item.title}
-                </Text.Body>
-            );
-        } else if (React.isValidElement(item.title)) {
-            return item.title;
-        }
-        return null;
-    };
-    return (
-        <NavItem id={item.id} isSelected={isSelected} onClick={handleClick}>
-            {renderTitle()}
-        </NavItem>
-    );
-};
-
 /**
  * A sidebar navigation element. The currently visible section will be highlighted.
  *
  * This component should be placed inside a container that aligns it to the side.
  * The container can also have `position: sticky` in order to make it a sticky sidebar.
  */
-export const LocalNavMenu = React.forwardRef<
-    HTMLUListElement,
-    LocalNavMenuProps
->(
-    (
-        {
-            onNavItemSelect: onNavItemSelect,
-            items,
-            selectedItemIndex,
-            id,
-            "data-testid": dataTestId,
-            className,
-            renderItem,
-        }: LocalNavMenuProps,
-        ref
-    ) => {
-        const localNavMenuId = dataTestId || "local-nav-menu";
-        return (
-            <Nav
-                ref={ref}
-                id={id}
-                className={className}
-                data-testid={`${localNavMenuId}-container`}
-            >
-                {items.map((item, i) => {
-                    const isSelected = i === selectedItemIndex;
+const Component = (
+    {
+        onNavItemSelect: onNavItemSelect,
+        items,
+        selectedItemIndex,
+        id,
+        "data-testid": dataTestId,
+        className,
+        renderItem,
+    }: LocalNavMenuProps,
+    ref: React.Ref<HTMLUListElement>
+): JSX.Element => {
+    // =============================================================================
+    // CONST, STATE, REF
+    // =============================================================================
+    const localNavMenuId = dataTestId || "local-nav-menu";
 
-                    return (
-                        <LocalNavItem
-                            key={i}
-                            data-testid={`${localNavMenuId}-item--${i}`}
-                            handleClick={(e) =>
-                                onNavItemSelect?.(
-                                    e.nativeEvent as MouseEvent,
-                                    { title: item.title, id },
-                                    i
-                                )
-                            }
-                            isSelected={isSelected}
-                            item={{ title: item.title, id }}
-                            renderItem={renderItem}
-                        />
-                    );
-                })}
-            </Nav>
+    // =============================================================================
+    // RENDER FUNCTIONS
+    // =============================================================================
+
+    const renderLocalNavItem = ({
+        handleClick,
+        isSelected,
+        item,
+        renderItem,
+    }: LocalNavItemComponentProps) => {
+        const { id = "local-nav-menu", title } = item;
+
+        const renderTitle = () => {
+            if (renderItem) {
+                return renderItem(item, { selected: isSelected });
+            } else if (typeof title === "string") {
+                return (
+                    <Text.Body
+                        style={{ margin: 0 }}
+                        weight={isSelected ? "semibold" : "regular"}
+                        id={`${id}-title`}
+                    >
+                        {title}
+                    </Text.Body>
+                );
+            } else if (React.isValidElement(title)) {
+                return title;
+            }
+            return null;
+        };
+
+        return (
+            <NavItem id={id} isSelected={isSelected} onClick={handleClick}>
+                {renderTitle()}
+            </NavItem>
         );
-    }
-);
+    };
+    return (
+        <Nav
+            ref={ref}
+            id={id}
+            className={className}
+            data-testid={`${localNavMenuId}-container`}
+        >
+            {items.map((item, i) => {
+                const isSelected = i === selectedItemIndex;
+
+                return renderLocalNavItem({
+                    handleClick: (e) =>
+                        onNavItemSelect?.(
+                            e.nativeEvent as MouseEvent,
+                            { title: item.title, id: item.id },
+                            i
+                        ),
+                    isSelected,
+                    item: { title: item.title, id: item.id },
+                    renderItem, // Optional, based on whether renderItem is passed
+                });
+            })}
+        </Nav>
+    );
+};
+
+export const LocalNavMenu = React.forwardRef(Component);
