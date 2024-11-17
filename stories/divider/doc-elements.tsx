b42d4e1ac6d209b772c4de1d3c634d2e921423ac
roll <quek.ruoling@gmail.com>
2024-11-18 01:58:13 +0800

    [V3][RL] Update Divider breakpoint logic, update stories

--- stories/divider/doc-elements.tsx
+++ stories/divider/doc-elements.tsx
@@ -1,19 +1,9 @@
+import { Layout } from "src/layout";
 import styled from "styled-components";
-import { V2_Layout } from "../../src/v2_layout";
-import { V2_Text } from "../../src/v2_text";
 
-export const Wrapper = styled(V2_Layout.Content)`
-    padding: 5rem;
+export const LayoutGrid = styled(Layout.Content)`
+    [data-id="container"] {
+        gap: 1rem;
+    }
 `;
-
-export const TextComponent = styled(V2_Text.Body)`
-    margin-bottom: 1rem;
-`;
-
-export const DisplayText = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <V2_Layout.ColDiv desktopCols={12} tabletCols={8} mobileCols={4}>
-            <TextComponent>{children}</TextComponent>
-        </V2_Layout.ColDiv>
-    );
-};
+LayoutGrid.displayName = "Layout.Content";
