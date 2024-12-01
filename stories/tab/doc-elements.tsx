24bcd315c7ca0c2c975a6430f546518ce99835ed
roll <quek.ruoling@gmail.com>
2024-12-01 20:04:26 +0800

    [V3][RL] Update Tab stories

--- stories/tab/doc-elements.tsx
+++ stories/tab/doc-elements.tsx
@@ -1,5 +1,58 @@
+import { Typography } from "src/typography";
 import styled from "styled-components";
 
 export const Content = styled.div`
     padding: 1.5rem;
 `;
+
+export const ContentA = () => (
+    <Content>
+        <Typography.HeaderMD>Section A</Typography.HeaderMD>
+        <br />
+        <Typography.BodyBL>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            commodo eget turpis sit amet luctus. Duis fringilla, libero ac
+            eleifend vehicula, sem arcu mattis diam, eget pellentesque urna
+            libero feugiat sem.
+        </Typography.BodyBL>
+    </Content>
+);
+
+export const ContentB = () => (
+    <Content>
+        <Typography.HeaderMD>Section B</Typography.HeaderMD>
+        <br />
+        <Typography.BodyBL>
+            Donec metus augue, vulputate ut laoreet pretium, cursus sed odio.
+            Aenean imperdiet sapien nec lectus gravida, vitae tincidunt sem
+            feugiat. Nullam sit amet tortor purus. Sed eget nulla sapien. Proin
+            a lacus pellentesque, facilisis augue quis, vestibulum sem.
+        </Typography.BodyBL>
+    </Content>
+);
+
+export const ContentC = () => (
+    <Content>
+        <Typography.HeaderMD>Section C</Typography.HeaderMD>
+        <br />
+        <Typography.BodyBL>
+            Maecenas tempor dolor sit amet turpis interdum convallis. Nunc ut
+            elit vitae justo placerat vulputate. Mauris varius sem in lectus
+            vestibulum, sed porttitor nisi ultricies. Morbi quis commodo ipsum.
+        </Typography.BodyBL>
+    </Content>
+);
+
+export const ContentD = () => (
+    <Content>
+        <Typography.HeaderMD>Section D</Typography.HeaderMD>
+        <br />
+        <Typography.BodyBL>
+            Nullam sit amet tortor purus. Sed eget nulla sapien. Proin a lacus
+            pellentesque, facilisis augue quis, vestibulum sem. Nulla pretium
+            gravida consectetur. Curabitur vestibulum erat nisi. Proin et
+            accumsan purus. Donec blandit tortor risus, vitae tempus magna
+            egestas nec.
+        </Typography.BodyBL>
+    </Content>
+);
--- stories/tab/tab.stories.tsx
+++ stories/tab/tab.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tab } from "src/tab";
-import { V2_Text } from "src/v2_text";
-import { Content } from "./doc-elements";
 import { useState } from "react";
+import { Tab } from "src/tab";
+import { ContentA, ContentB, ContentC, ContentD } from "./doc-elements";
 
 type Component = typeof Tab;
 
 const meta: Meta<Component> = {
-    title: "Modules/Tab",
+    title: "Content/Tab",
     component: Tab,
     parameters: {
         layout: "fullscreen",
@@ -17,62 +16,20 @@ const meta: Meta<Component> = {
 export default meta;
 
 export const Default: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         return (
             <Tab>
                 <Tab.Item title="Section A">
-                    <Content>
-                        <V2_Text.H1>Section A</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nullam commodo eget turpis sit amet luctus.
-                            Duis fringilla, libero ac eleifend vehicula, sem
-                            arcu mattis diam, eget pellentesque urna libero
-                            feugiat sem.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentA />
                 </Tab.Item>
                 <Tab.Item title="Section B">
-                    <Content>
-                        <V2_Text.H1>Section B</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Donec metus augue, vulputate ut laoreet pretium,
-                            cursus sed odio. Aenean imperdiet sapien nec lectus
-                            gravida, vitae tincidunt sem feugiat. Nullam sit
-                            amet tortor purus. Sed eget nulla sapien. Proin a
-                            lacus pellentesque, facilisis augue quis, vestibulum
-                            sem.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentB />
                 </Tab.Item>
                 <Tab.Item title="Section C">
-                    <Content>
-                        <V2_Text.H1>Section C</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Maecenas tempor dolor sit amet turpis interdum
-                            convallis. Nunc ut elit vitae justo placerat
-                            vulputate. Mauris varius sem in lectus vestibulum,
-                            sed porttitor nisi ultricies. Morbi quis commodo
-                            ipsum.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentC />
                 </Tab.Item>
                 <Tab.Item title="Section D">
-                    <Content>
-                        <V2_Text.H1>Section D</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Nullam sit amet tortor purus. Sed eget nulla sapien.
-                            Proin a lacus pellentesque, facilisis augue quis,
-                            vestibulum sem. Nulla pretium gravida consectetur.
-                            Curabitur vestibulum erat nisi. Proin et accumsan
-                            purus. Donec blandit tortor risus, vitae tempus
-                            magna egestas nec.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentD />
                 </Tab.Item>
             </Tab>
         );
@@ -80,48 +37,14 @@ export const Default: StoryObj<Component> = {
 };
 
 export const LongerLabels: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         return (
             <Tab>
                 <Tab.Item title="This is a significantly longer label that will truncate">
-                    <Content>
-                        <V2_Text.H1>Section A</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nullam commodo eget turpis sit amet luctus.
-                            Duis fringilla, libero ac eleifend vehicula, sem
-                            arcu mattis diam, eget pellentesque urna libero
-                            feugiat sem.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentA />
                 </Tab.Item>
                 <Tab.Item title="Shorter label">
-                    <Content>
-                        <V2_Text.H1>Section B</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Donec metus augue, vulputate ut laoreet pretium,
-                            cursus sed odio. Aenean imperdiet sapien nec lectus
-                            gravida, vitae tincidunt sem feugiat. Nullam sit
-                            amet tortor purus. Sed eget nulla sapien. Proin a
-                            lacus pellentesque, facilisis augue quis, vestibulum
-                            sem.
-                        </V2_Text.Body>
-                    </Content>
-                </Tab.Item>
-                <Tab.Item title="Section C">
-                    <Content>
-                        <V2_Text.H1>Section C</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Maecenas tempor dolor sit amet turpis interdum
-                            convallis. Nunc ut elit vitae justo placerat
-                            vulputate. Mauris varius sem in lectus vestibulum,
-                            sed porttitor nisi ultricies. Morbi quis commodo
-                            ipsum.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentB />
                 </Tab.Item>
             </Tab>
         );
@@ -139,44 +62,13 @@ export const ControlledMode: StoryObj<Component> = {
         return (
             <Tab onTabClick={handleTabClick} currentActive={currentIndex}>
                 <Tab.Item title="Section A">
-                    <Content>
-                        <V2_Text.H1>Section A</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nullam commodo eget turpis sit amet luctus.
-                            Duis fringilla, libero ac eleifend vehicula, sem
-                            arcu mattis diam, eget pellentesque urna libero
-                            feugiat sem.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentA />
                 </Tab.Item>
                 <Tab.Item title="Section B">
-                    <Content>
-                        <V2_Text.H1>Section B</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Donec metus augue, vulputate ut laoreet pretium,
-                            cursus sed odio. Aenean imperdiet sapien nec lectus
-                            gravida, vitae tincidunt sem feugiat. Nullam sit
-                            amet tortor purus. Sed eget nulla sapien. Proin a
-                            lacus pellentesque, facilisis augue quis, vestibulum
-                            sem.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentB />
                 </Tab.Item>
                 <Tab.Item title="Section C">
-                    <Content>
-                        <V2_Text.H1>Section C</V2_Text.H1>
-                        <br />
-                        <V2_Text.Body>
-                            Maecenas tempor dolor sit amet turpis interdum
-                            convallis. Nunc ut elit vitae justo placerat
-                            vulputate. Mauris varius sem in lectus vestibulum,
-                            sed porttitor nisi ultricies. Morbi quis commodo
-                            ipsum.
-                        </V2_Text.Body>
-                    </Content>
+                    <ContentC />
                 </Tab.Item>
             </Tab>
         );
