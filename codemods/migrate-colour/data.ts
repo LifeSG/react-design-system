3520abf1b179153d3603163b31f8646d64b53b0f
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-12-03 19:46:42 +0800

    [V3][RL] Update colour mapping

--- codemods/migrate-colour/data.ts
+++ codemods/migrate-colour/data.ts
@@ -1,7 +1,13 @@
-export const lifesgMapping = {
-    Secondary: "primary-40",
-    PrimaryDark: "primary-40",
+const defaultMapping = {
     Primary: "primary-50",
+    PrimaryDark: "primary-40",
+    Secondary: "primary-40",
+    "Brand[1]": "brand-50",
+    "Brand[2]": "brand-60",
+    "Brand[3]": "brand-70",
+    "Brand[4]": "brand-80",
+    "Brand[5]": "brand-90",
+    "Brand[6]": "brand-95",
     "Accent.Light[1]": "primary-60",
     "Accent.Light[2]": "primary-70",
     "Accent.Light[3]": "primary-80",
@@ -11,21 +17,6 @@ export const lifesgMapping = {
     "Accent.Dark[1]": "secondary-40",
     "Accent.Dark[2]": "secondary-50",
     "Accent.Dark[3]": "secondary-60",
-};
-
-export const bookingSgMapping = {
-    PrimaryDark: "primary-40",
-    Primary: "primary-50",
-    "Brand[1]": "brand-50",
-    "Brand[2]": "brand-60",
-    "Brand[3]": "brand-70",
-    "Brand[4]": "brand-80",
-    "Brand[5]": "brand-90",
-    "Brand[6]": "brand-95",
-    "Accent.Light[1]": "primary-60",
-    "Accent.Dark[1]": "secondary-40",
-    "Accent.Dark[2]": "secondary-60",
-    "Accent.Dark[3]": "secondary-70",
     "Neutral[1]": "neutral-20",
     "Neutral[2]": "neutral-30",
     "Neutral[3]": "neutral-50",
@@ -33,27 +24,61 @@ export const bookingSgMapping = {
     "Neutral[5]": "neutral-90",
     "Neutral[6]": "neutral-95",
     "Neutral[7]": "neutral-100",
-    "Blue[1]": "information-40",
-    "Blue[2]": "information-50",
-    "Blue[3]": "information-70",
+    "Neutral[8]": "white",
+    "Validation.Green.Text": "success-40",
+    "Validation.Green.Icon": "success-70",
+    "Validation.Green.Border": "success-80",
+    "Validation.Green.Background": "success-100",
+    "Validation.Orange.Text": "warning-50",
+    "Validation.Orange.Icon": "warning-70",
+    "Validation.Orange.Border": "warning-80",
+    "Validation.Orange.Background": "warning-100",
+    "Validation.Orange.Badge": "warning-100",
+    "Validation.Red.Text": "error-50",
+    "Validation.Red.Icon": "error-50",
+    "Validation.Red.Border": "error-60",
+    "Validation.Red.Background": "error-100",
+    "Validation.Blue.Text": "info-40",
+    "Validation.Blue.Icon": "info-50",
+    "Validation.Blue.Border": "info-70",
+    "Validation.Blue.Background": "info-95",
+};
+
+export const lifesgMapping = {
+    ...defaultMapping,
+    Primary: "primary-50",
+    PrimaryDark: "primary-40",
+    Secondary: "primary-40",
+};
+
+export const bookingSgMapping = {
+    ...defaultMapping,
+    Primary: "primary-50",
+    PrimaryDark: "primary-40",
+    Secondary: "primary-40",
+    "Accent.Dark[1]": "secondary-40",
+    "Accent.Dark[2]": "secondary-60",
+    "Accent.Dark[3]": "secondary-70",
 };
 
 export const mylegacyMapping = {
+    ...defaultMapping,
     PrimaryDark: "primary-40",
     Primary: "primary-50",
-    "Accent.Light[1]": "primary-60",
-    "Accent.Light[2]": "primary-80",
-    "Accent.Light[3]": "primary-90",
-    "Accent.Light[4]": "primary-95",
-    "Accent.Light[5]": "primary-100",
-    "Accent.Light[6]": "primary-100",
+    Secondary: "primary-40",
+    "Validation.Green.Text": "success-50",
+    "Validation.Red.Text": "success-40",
 };
 
 export const ccubeMapping = {
-    "Brand[2]": "brand-50",
-    "Brand[1]": "brand-60",
+    ...defaultMapping,
     Primary: "primary-50",
-    Secondary: "secondary-50",
+    PrimaryDark: "primary-40",
+    Secondary: "secondary-40",
+    "Brand[1]": "brand-60",
+    "Brand[2]": "brand-50",
 };
 
-export const rbsMapping = {};
+export const rbsMapping = {
+    ...defaultMapping,
+};
