1d91386f8a6ee4702ba8111bcdfdddfd4c709ec9
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-12-09 21:55:40 +0800

    [CCUBE-1626][MAHI|RL] Update month and year styles

--- src/shared/internal-calendar/internal-calendar-year.style.tsx
+++ src/shared/internal-calendar/internal-calendar-year.style.tsx
@@ -1,7 +1,5 @@
 import styled, { css } from "styled-components";
-import { V2_Color } from "../../v2_color";
-import { V2_TextStyleHelper } from "../../v2_text/helper";
-import { V2_Text } from "../../v2_text/text";
+import { Border, Colour, Font, FontSpec, Motion, Radius } from "../../theme";
 import { YearVariant } from "./internal-calendar-year";
 import { CalendarType } from "./types";
 
@@ -27,6 +25,7 @@ export const Wrapper = styled.div<WrapperStyleProps>`
     height: 100%;
     display: grid;
     align-content: center;
+    align-items: center;
     grid-template-columns: repeat(3, 1fr);
 
     ${(props) => {
@@ -49,70 +48,96 @@ export const YearCell = styled.div<StyleProps>`
     display: flex;
     justify-content: center;
     align-items: center;
-    cursor: default;
-    border-radius: 0.5rem;
     margin: 0 0.5rem;
+    transition: ${Motion["duration-150"]} ${Motion["ease-default"]};
+    padding: 0.5rem;
 
-    ${(props) => {
-        if (props.$interactive) {
+    // default styles
+    ${Font["body-md-regular"]}
+    border-radius: ${Radius.md};
+    border: ${Border["width-010"]} ${Border.solid} transparent;
+    background-clip: border-box;
+    color: ${Colour["text"]};
+    cursor: default;
+
+    // cursor style
+    ${({ $interactive, $disabledDisplay }) => {
+        if ($interactive) {
             return css`
                 cursor: pointer;
-                &:hover {
-                    box-shadow: 0px 0px 4px 1px ${V2_Color.Shadow.Accent};
-                    border: 1px solid ${V2_Color.Accent.Light[1]};
-                }
             `;
         }
 
-        if (props.$disabledDisplay) {
+        if ($disabledDisplay) {
             return css`
                 cursor: not-allowed;
             `;
         }
     }}
 
-    ${(props) => {
-        switch (props.$variant) {
-            case "current-year":
-                return css`
-                    background: ${V2_Color.Accent.Light[6](props)};
-                `;
-            case "selected-year":
-                return css`
-                    background: ${V2_Color.Accent.Light[5](props)};
-                    border: 1px solid ${V2_Color.Primary(props)};
-                `;
-            case "other-decade":
-            case "default":
-                break;
-        }
-    }};
-`;
-
-export const CellLabel = styled(V2_Text.H5)<StyleProps>`
-    ${(props) => {
-        if (props.$disabledDisplay) {
+    // background, border and text styles
+    ${({ $variant, $interactive, $disabledDisplay }) => {
+        if ($variant === "selected-year") {
             return css`
-                color: ${V2_Color.Neutral[4]};
+                background: ${Colour["bg-selected"]};
+                border-color: ${Colour["border-selected"]};
+                color: ${Colour["text-selected"]};
+                font-weight: ${FontSpec["weight-semibold"]};
+
+                ${$interactive &&
+                css`
+                    &:hover {
+                        background: ${Colour["bg-selected-hover"]};
+                        border-color: ${Colour["border-selected-hover"]};
+                        color: ${Colour["text-selected-hover"]};
+                    }
+                `}
             `;
         }
 
-        switch (props.$variant) {
-            case "current-year":
-                return css`
-                    color: ${V2_Color.Neutral[3](props)};
-                `;
-            case "selected-year":
-                return css`
-                    ${V2_TextStyleHelper.getTextStyle("H5", "semibold")}
-                    color: ${V2_Color.Primary(props)};
-                `;
-            case "other-decade":
-                return css`
-                    color: ${V2_Color.Neutral[4](props)};
-                `;
-            case "default":
-                break;
+        if ($variant === "current-year") {
+            return css`
+                color: ${Colour["text-primary"]};
+                font-weight: ${FontSpec["weight-semibold"]};
+            `;
         }
+
+        if ($variant === "other-decade") {
+            return css`
+                color: ${Colour["text-disabled-subtlest"]};
+            `;
+        }
+
+        if ($disabledDisplay) {
+            return css`
+                color: ${Colour["text-disabled-subtlest"]};
+            `;
+        }
+    }}
+
+    // hover styles
+    ${({ $variant, $interactive }) => {
+        if (!$interactive) {
+            return;
+        }
+
+        if ($variant === "selected-year") {
+            return css`
+                &:hover {
+                    background: ${Colour["bg-selected-hover"]};
+                    border-color: ${Colour["border-selected-hover"]};
+                    color: ${Colour["text-selected-hover"]};
+                    font-weight: ${FontSpec["weight-semibold"]};
+                }
+            `;
+        }
+
+        return css`
+            &:hover {
+                background: ${Colour["bg-hover"]};
+                color: ${Colour["text-hover"]};
+                font-weight: ${FontSpec["weight-semibold"]};
+            }
+        `;
     }}
 `;
