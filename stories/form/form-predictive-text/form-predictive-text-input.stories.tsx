df50436b126cc337162980827ef84ad22b4c6022
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-02-05 15:29:30 +0800

    [V3][RL] Migrate PredictiveTextInput

--- stories/form/form-predictive-text/form-predictive-text-input.stories.tsx
+++ stories/form/form-predictive-text/form-predictive-text-input.stories.tsx
@@ -1,14 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
 import { Form } from "src/form";
-import { V2_Layout } from "src/v2_layout";
+import { Layout } from "src/layout";
 import { PredictiveTextInput } from "src/predictive-text-input";
-import { Container } from "../shared-doc-elements";
+import {
+    FullWidthStoryDecorator,
+    StoryDecorator,
+} from "stories/storybook-common";
 import {
     mockFetchOptions,
     mockFetchOptionsWithSecondaryLabel,
 } from "./mock-fetch-options";
-import { StoryContainer } from "../../storybook-common";
 
 type Component = typeof Form.PredictiveTextInput;
 type StandaloneComponent = typeof PredictiveTextInput;
@@ -25,90 +27,88 @@ export const Default: StoryObj<Component> = {
         const [selectedOption, setSelectedOption] = useState(undefined);
         const [selectedOption2, setSelectedOption2] = useState(undefined);
         return (
-            <StoryContainer>
-                <Container>
-                    <Form.PredictiveTextInput
-                        label="This is the predictive text input"
-                        placeholder="Start typing..."
-                        selectedOption={selectedOption}
-                        fetchOptions={mockFetchOptions}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => item.label}
-                        displayValueExtractor={(item) => item.label}
-                        onSelectOption={(option, extractedValue) =>
-                            setSelectedOption(option)
-                        }
-                    />
-                    <Form.PredictiveTextInput
-                        label="This is the error state"
-                        placeholder="Start typing..."
-                        selectedOption={selectedOption2}
-                        fetchOptions={mockFetchOptions}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => item.label}
-                        displayValueExtractor={(item) => item.label}
-                        onSelectOption={(option, extractedValue) =>
-                            setSelectedOption2(option)
-                        }
-                        errorMessage="Input is required"
-                    />
-                    <Form.PredictiveTextInput
-                        label="This is the disabled state"
-                        placeholder="Start typing..."
-                        fetchOptions={mockFetchOptions}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => item.label}
-                        displayValueExtractor={(item) => item.label}
-                        disabled
-                    />
-                    <Form.PredictiveTextInput
-                        label="This is the readonly state"
-                        placeholder="Start typing..."
-                        fetchOptions={mockFetchOptions}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => item.label}
-                        displayValueExtractor={(item) => item.label}
-                        readOnly
-                    />
-                </Container>
-            </StoryContainer>
+            <>
+                <Form.PredictiveTextInput
+                    label="This is the predictive text input"
+                    placeholder="Start typing..."
+                    selectedOption={selectedOption}
+                    fetchOptions={mockFetchOptions}
+                    valueExtractor={(item) => item.value}
+                    listExtractor={(item) => item.label}
+                    displayValueExtractor={(item) => item.label}
+                    onSelectOption={(option, _extractedValue) =>
+                        setSelectedOption(option)
+                    }
+                />
+                <Form.PredictiveTextInput
+                    label="This is the error state"
+                    placeholder="Start typing..."
+                    selectedOption={selectedOption2}
+                    fetchOptions={mockFetchOptions}
+                    valueExtractor={(item) => item.value}
+                    listExtractor={(item) => item.label}
+                    displayValueExtractor={(item) => item.label}
+                    onSelectOption={(option, _extractedValue) =>
+                        setSelectedOption2(option)
+                    }
+                    errorMessage="Input is required"
+                />
+                <Form.PredictiveTextInput
+                    label="This is the disabled state"
+                    placeholder="Start typing..."
+                    fetchOptions={mockFetchOptions}
+                    valueExtractor={(item) => item.value}
+                    listExtractor={(item) => item.label}
+                    displayValueExtractor={(item) => item.label}
+                    disabled
+                />
+                <Form.PredictiveTextInput
+                    label="This is the readonly state"
+                    placeholder="Start typing..."
+                    fetchOptions={mockFetchOptions}
+                    valueExtractor={(item) => item.value}
+                    listExtractor={(item) => item.label}
+                    displayValueExtractor={(item) => item.label}
+                    readOnly
+                />
+            </>
         );
     },
+    decorators: [StoryDecorator({ maxWidth: true })],
 };
 
 export const WithSecondaryLabel: StoryObj<Component> = {
     render: () => {
         const [selectedOption, setSelectedOption] = useState(undefined);
         return (
-            <StoryContainer>
-                <Container>
-                    <Form.PredictiveTextInput
-                        label="With secondary labels"
-                        selectedOption={selectedOption}
-                        placeholder="Start typing..."
-                        fetchOptions={mockFetchOptionsWithSecondaryLabel}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => {
-                            return {
-                                title: item.title,
-                                secondaryLabel: item?.secondaryLabel,
-                            };
-                        }}
-                        displayValueExtractor={(item) => item.title}
-                        onSelectOption={(option, extractedValue) =>
-                            setSelectedOption(option)
-                        }
-                    />
-                </Container>
-            </StoryContainer>
+            <>
+                <Form.PredictiveTextInput
+                    label="With secondary labels"
+                    selectedOption={selectedOption}
+                    placeholder="Start typing..."
+                    fetchOptions={mockFetchOptionsWithSecondaryLabel}
+                    valueExtractor={(item) => item.value}
+                    listExtractor={(item) => {
+                        return {
+                            title: item.title,
+                            secondaryLabel: item?.secondaryLabel,
+                        };
+                    }}
+                    displayValueExtractor={(item) => item.title}
+                    onSelectOption={(option, _extractedValue) =>
+                        setSelectedOption(option)
+                    }
+                />
+            </>
         );
     },
+    decorators: [StoryDecorator({ maxWidth: true })],
 };
 
 export const RenderingInGridLayout: StoryObj<Component> = {
     render: () => {
         return (
-            <V2_Layout.Content type="grid" style={{ padding: "2rem" }}>
+            <Layout.Content type="grid">
                 <Form.PredictiveTextInput
                     label="A shorter form input"
                     placeholder="Start typing..."
@@ -116,7 +116,8 @@ export const RenderingInGridLayout: StoryObj<Component> = {
                     valueExtractor={(item) => item.value}
                     listExtractor={(item) => item.label}
                     displayValueExtractor={(item) => item.label}
-                    mobileCols={[1, 5]}
+                    xxsCols={[1, 9]}
+                    lgCols={[1, 5]}
                 />
                 <Form.PredictiveTextInput
                     label="A longer form input"
@@ -125,30 +126,24 @@ export const RenderingInGridLayout: StoryObj<Component> = {
                     valueExtractor={(item) => item.value}
                     listExtractor={(item) => item.label}
                     displayValueExtractor={(item) => item.label}
-                    mobileCols={[1, 5]}
-                    tabletCols={[1, 9]}
+                    xxsCols={[1, 9]}
                 />
-            </V2_Layout.Content>
+            </Layout.Content>
         );
     },
-    parameters: {
-        layout: "fullscreen",
-    },
+    decorators: [FullWidthStoryDecorator()],
 };
 
 export const StandaloneUsage: StoryObj<StandaloneComponent> = {
     render: () => {
         return (
-            <StoryContainer>
-                <Container>
-                    <PredictiveTextInput
-                        placeholder="Start typing..."
-                        fetchOptions={mockFetchOptions}
-                        valueExtractor={(item) => item.value}
-                        listExtractor={(item) => item.label}
-                    />
-                </Container>
-            </StoryContainer>
+            <PredictiveTextInput
+                placeholder="Start typing..."
+                fetchOptions={mockFetchOptions}
+                valueExtractor={(item) => item.value}
+                listExtractor={(item) => item.label}
+            />
         );
     },
+    decorators: [StoryDecorator({ maxWidth: true })],
 };
