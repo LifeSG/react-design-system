69ac7d8052e66a3d2871de2f35fec77fda05b12e
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-03-04 09:32:30 +0800

    [V3][RL] Rename typography token docs to avoid confusion with component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -38,7 +38,7 @@ const preview: Preview = {
                         "Introduction",
                         "Themes",
                         "Colours",
-                        "Typography",
+                        "Font",
                         "Breakpoint",
                         "Spacing",
                         "Motion",
