70fbfe4ae1945f9864dab1f882b252ec794adc7a
shawn wee <shawn.wee@ufinity.com>
2025-03-19 22:23:22 +0800

    [MISC][SW] improve overlay typing

--- src/modal/types.ts
+++ src/modal/types.ts
@@ -2,19 +2,27 @@ import React from "react";
 
 export type ModalAnimationDirection = "top" | "bottom" | "left" | "right";
 
-export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+type OverlayClickProps =
+    | {
+          enableOverlayClick: false;
+          onOverlayClick?: (() => void) | undefined;
+      }
+    | {
+          enableOverlayClick?: true | undefined;
+          onOverlayClick: () => void;
+      };
+
+export type ModalProps = React.HTMLAttributes<HTMLDivElement> & {
     show: boolean;
     children: JSX.Element | JSX.Element[];
     /** Animation direction of appearance and dismissal. Values: "top" | "bottom" | "left" | "right" */
     animationFrom?: ModalAnimationDirection | undefined;
-    enableOverlayClick?: boolean | undefined;
     /** The identifier of the element to inject the Modal into */
     rootComponentId?: string | undefined;
     zIndex?: number | undefined;
-    onOverlayClick?: (() => void) | undefined;
     /** Dismiss keyboard to keep modal in fullscreen */
     dismissKeyboardOnShow?: boolean | undefined;
-}
+} & OverlayClickProps;
 
 export interface ModalBoxProps extends React.HTMLAttributes<HTMLDivElement> {
     children: JSX.Element | JSX.Element[];
