5f117bb0269da2cf8a69e6d4bfef73304322031a
gohguancheng <gohguancheng01@gmail.com>
2025-03-21 10:26:49 +0800

    [BOOKINGSG-6938][GC] disable ligature for Lato font based on PR comment

--- src/theme/font/specs/pa-font-set.ts
+++ src/theme/font/specs/pa-font-set.ts
@@ -1,4 +1,4 @@
 import { FontSet } from "../types";
 import { getFontSet } from "../utils";
 
-export const PAFontSet: FontSet = getFontSet();
+export const PAFontSet: FontSet = getFontSet({ disableLigatures: true });
