893af25691895e7f4501c5b38f63f120d4d2f343
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-04-14 08:41:12 +0800

    [V3][RL] Clean up deprecated elements

--- stories/form/form-custom-field/form-custom-field.stories.tsx
+++ stories/form/form-custom-field/form-custom-field.stories.tsx
@@ -2,8 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
 import { Checkbox } from "src/checkbox";
 import { Form } from "src/form";
-import { StoryContainer } from "../../storybook-common";
-import { Container } from "../shared-doc-elements";
+import { StoryDecorator } from "stories/storybook-common";
 
 type Component = typeof Form.CustomField;
 
@@ -15,34 +14,30 @@ const meta: Meta<Component> = {
 export default meta;
 
 export const Default: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         const [cb1, setCb1] = useState(false);
         const [cb2, setCb2] = useState(false);
         const [cb3, setCb3] = useState(false);
         return (
-            <StoryContainer>
-                <Container>
-                    <Form.CustomField label="This is a custom field">
-                        <Checkbox checked={cb1} onClick={() => setCb1(!cb1)} />
-                    </Form.CustomField>
-                    <Form.CustomField
-                        label="This is the disabled state"
+            <>
+                <Form.CustomField label="This is a custom field">
+                    <Checkbox checked={cb1} onClick={() => setCb1(!cb1)} />
+                </Form.CustomField>
+                <Form.CustomField label="This is the disabled state" disabled>
+                    <Checkbox
+                        checked={cb2}
+                        onClick={() => setCb2(!cb2)}
                         disabled
-                    >
-                        <Checkbox
-                            checked={cb2}
-                            onClick={() => setCb2(!cb2)}
-                            disabled
-                        />
-                    </Form.CustomField>
-                    <Form.CustomField
-                        label="This is the error state"
-                        errorMessage="Date is required"
-                    >
-                        <Checkbox checked={cb3} onClick={() => setCb3(!cb3)} />
-                    </Form.CustomField>
-                </Container>
-            </StoryContainer>
+                    />
+                </Form.CustomField>
+                <Form.CustomField
+                    label="This is the error state"
+                    errorMessage="Date is required"
+                >
+                    <Checkbox checked={cb3} onClick={() => setCb3(!cb3)} />
+                </Form.CustomField>
+            </>
         );
     },
+    decorators: [StoryDecorator({ maxWidth: true })],
 };
--- stories/form/form-label/form-label.stories.tsx
+++ stories/form/form-label/form-label.stories.tsx
@@ -1,10 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
 import { Checkbox } from "src/checkbox";
-import { Input } from "src/input";
 import { Form } from "src/form";
-import { Wrapper } from "./doc-elements";
-import { StoryContainer } from "../../storybook-common";
+import { Input } from "src/input";
+import { StackDecorator, StoryDecorator } from "stories/storybook-common";
 
 type Component = typeof Form.Label;
 
@@ -16,79 +15,73 @@ const meta: Meta<Component> = {
 export default meta;
 
 export const Default: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         return (
-            <StoryContainer>
-                <Wrapper>
-                    <Form.Label>The form label</Form.Label>
-                    <Form.Label
-                        addon={{
-                            content:
-                                "This is the form label's popover. And this will only be visible if you specify the addon prop",
-                            type: "popover",
-                        }}
-                    >
-                        The form label with an addon
-                    </Form.Label>
-                    <Form.Label subtitle="This is the subtitle">
-                        The form label with a subtitle
-                    </Form.Label>
-                </Wrapper>
-            </StoryContainer>
+            <>
+                <Form.Label>The form label</Form.Label>
+                <Form.Label
+                    addon={{
+                        content:
+                            "This is the form label's popover. And this will only be visible if you specify the addon prop",
+                        type: "popover",
+                    }}
+                >
+                    The form label with an addon
+                </Form.Label>
+                <Form.Label subtitle="This is the subtitle">
+                    The form label with a subtitle
+                </Form.Label>
+            </>
         );
     },
+    decorators: [StackDecorator(), StoryDecorator({ maxWidth: true })],
 };
 
 export const RealWorldUsage: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         const [checked, setChecked] = useState(false);
         return (
-            <StoryContainer>
-                <Wrapper>
-                    <div style={{ display: "flex", flexDirection: "column" }}>
-                        <Form.Label htmlFor="input">
-                            This is a descriptive label
-                        </Form.Label>
-                        <Input id="input" placeholder="Enter something" />
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            gap: "0.5rem",
-                        }}
-                    >
-                        <Checkbox
-                            id="checkbox"
-                            checked={checked}
-                            onClick={() => setChecked(!checked)}
-                        />
-                        <Form.Label
-                            htmlFor="checkbox"
-                            style={{ marginBottom: 0 }}
-                        >
-                            Label for checkbox
-                        </Form.Label>
-                    </div>
-                </Wrapper>
-            </StoryContainer>
-        );
-    },
-};
-
-export const NestedChildren: StoryObj<Component> = {
-    render: () => {
-        return (
-            <StoryContainer>
+            <>
                 <div style={{ display: "flex", flexDirection: "column" }}>
                     <Form.Label htmlFor="input">
-                        <span>Label with a</span>&nbsp;<a href="">hyperlink</a>
-                        <br />
-                        <p>and some additional text on a new line</p>
+                        This is a descriptive label
                     </Form.Label>
                     <Input id="input" placeholder="Enter something" />
                 </div>
-            </StoryContainer>
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "0.5rem",
+                    }}
+                >
+                    <Checkbox
+                        id="checkbox"
+                        checked={checked}
+                        onClick={() => setChecked(!checked)}
+                    />
+                    <Form.Label htmlFor="checkbox" style={{ marginBottom: 0 }}>
+                        Label for checkbox
+                    </Form.Label>
+                </div>
+            </>
         );
     },
+    decorators: [StackDecorator(), StoryDecorator({ maxWidth: true })],
+};
+
+export const NestedChildren: StoryObj<Component> = {
+    render: (_args) => {
+        return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <Form.Label htmlFor="input">
+                    Label with a&nbsp;<a href="">hyperlink</a>
+                    <br />
+                    and some additional text on a new line
+                </Form.Label>
+                <Input id="input" placeholder="Enter something" />
+            </div>
+        );
+    },
+    decorators: [StoryDecorator({ maxWidth: true })],
 };
