b7a58871d635066528cd6f94684de6d7227a4758
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-05-20 13:46:20 +0800

    [CCUBE-1769][RL] Align masthead with v3 layout

--- stories/masthead/masthead.stories.tsx
+++ stories/masthead/masthead.stories.tsx
@@ -1,17 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Masthead } from "src/masthead";
+import { FullWidthStoryDecorator } from "stories/storybook-common";
 
 type Component = typeof Masthead;
 
 const meta: Meta<Component> = {
     title: "Navigation/Masthead",
     component: Masthead,
+    decorators: [FullWidthStoryDecorator({})],
+    parameters: { layout: "fullscreen" },
 };
 
 export default meta;
 
 export const Default: StoryObj<Component> = {
-    render: () => {
+    render: (_args) => {
         return <Masthead />;
     },
 };
