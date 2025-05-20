390ae46776d3cc5faca4ccbd90790e31a24cb645
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-05-20 20:00:32 +0800

    [CCUBE-1769][RL] Support stretched layout

--- src/masthead/masthead.style.tsx
+++ src/masthead/masthead.style.tsx
@@ -1,39 +1,72 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Breakpoint, MediaQuery } from "../theme";
 
-export const Wrapper = styled.div`
-    max-width: 1440px;
+// =============================================================================
+// STYLE INTERFACES
+// =============================================================================
+interface WrapperStyleProps {
+    $stretch: boolean;
+}
 
-    --sgds-mainnav-padding-x: ${Breakpoint["xxl-margin"]}px;
-    --sgds-mainnav-mobile-padding-x: ${Breakpoint["xxl-margin"]}px;
+// =============================================================================
+// STYLING
+// =============================================================================
+export const Wrapper = styled.div<WrapperStyleProps>`
+    ${(props) => {
+        if (props.$stretch) {
+            return css`
+                --sgds-mainnav-padding-x: ${Breakpoint["xxl-margin"]}px;
+                --sgds-mainnav-mobile-padding-x: ${Breakpoint["xxl-margin"]}px;
+            `;
+        } else {
+            return css`
+                max-width: 1440px;
 
-    ${MediaQuery.MaxWidth.xl} {
-        --sgds-mainnav-padding-x: ${Breakpoint["xl-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["xl-margin"]}px;
-    }
+                --sgds-mainnav-padding-x: ${Breakpoint["xxl-margin"]}px;
+                --sgds-mainnav-mobile-padding-x: ${Breakpoint["xxl-margin"]}px;
 
-    ${MediaQuery.MaxWidth.lg} {
-        --sgds-mainnav-padding-x: ${Breakpoint["lg-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["lg-margin"]}px;
-    }
+                ${MediaQuery.MaxWidth.xl} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["xl-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "xl-margin"
+                    ]}px;
+                }
 
-    ${MediaQuery.MaxWidth.md} {
-        --sgds-mainnav-padding-x: ${Breakpoint["md-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["md-margin"]}px;
-    }
+                ${MediaQuery.MaxWidth.lg} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["lg-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "lg-margin"
+                    ]}px;
+                }
 
-    ${MediaQuery.MaxWidth.sm} {
-        --sgds-mainnav-padding-x: ${Breakpoint["sm-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["sm-margin"]}px;
-    }
+                ${MediaQuery.MaxWidth.md} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["md-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "md-margin"
+                    ]}px;
+                }
 
-    ${MediaQuery.MaxWidth.xs} {
-        --sgds-mainnav-padding-x: ${Breakpoint["xs-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["xs-margin"]}px;
-    }
+                ${MediaQuery.MaxWidth.sm} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["sm-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "sm-margin"
+                    ]}px;
+                }
 
-    ${MediaQuery.MaxWidth.xxs} {
-        --sgds-mainnav-padding-x: ${Breakpoint["xxs-margin"]}px;
-        --sgds-mainnav-mobile-padding-x: ${Breakpoint["xxs-margin"]}px;
-    }
+                ${MediaQuery.MaxWidth.xs} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["xs-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "xs-margin"
+                    ]}px;
+                }
+
+                ${MediaQuery.MaxWidth.xxs} {
+                    --sgds-mainnav-padding-x: ${Breakpoint["xxs-margin"]}px;
+                    --sgds-mainnav-mobile-padding-x: ${Breakpoint[
+                        "xxs-margin"
+                    ]}px;
+                }
+            `;
+        }
+    }}
 `;
