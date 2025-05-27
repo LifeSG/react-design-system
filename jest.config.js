77ef0a672a79de0ed8fcb6cd2a47776528c5ec04
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-05-27 22:11:47 +0800

    [V3][RL] Configure alias

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/src/__mocks__/fileMock.js",
         "\\.(css|less)$": "identity-obj-proxy",
+        "^@\\/theme$": "<rootDir>/src/theme",
     },
     moduleDirectories: ["node_modules", "<rootDir>"],
     setupFilesAfterEnv: ["jest-canvas-mock", "@testing-library/jest-dom"],
