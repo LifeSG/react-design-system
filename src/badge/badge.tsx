eb145196f2ab0f92fdde238c073aa290e1ef1cc1
Anu Mutyala <mutyala.lakshmi@ufinity.com>
2025-07-14 13:07:05 +0800

    [MOL-19014][AM] reorganise

--- src/badge/badge.tsx
+++ src/badge/badge.tsx
@@ -1,15 +1,6 @@
 import { BadgeWrapper, StyledBadge } from "./badge.style";
 import { BadgeProps } from "./types";
 
-// =============================================================================
-// HELPER FUNCTIONS
-// =============================================================================
-const getDisplayCount = (count: number): string => {
-    if (count <= 999) return count.toString();
-    if (count === 1000) return "1K";
-    return "1K+";
-};
-
 export const Badge = ({
     count = 0,
     variant = "number",
@@ -24,6 +15,15 @@ export const Badge = ({
 
     const shouldShowCount =
         variant === "number" || variant === "number-with-border";
+
+    // =============================================================================
+    // HELPER FUNCTIONS
+    // =============================================================================
+    function getDisplayCount(count: number) {
+        if (count <= 999) return count.toString();
+        if (count === 1000) return "1K";
+        return "1K+";
+    }
 
     return (
         <BadgeWrapper $isOverlay={isOverlay}>
