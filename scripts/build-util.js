db52728eb3d74aa5921dc8955ae18a50b9442cb7
PhamQuocHuy12 <huygreen6915@gmail.com>
2025-07-18 15:16:50 +0800

    Update package-lock and tsconfig

--- scripts/build-util.js
+++ scripts/build-util.js
@@ -11,11 +11,13 @@ export const getFolders = (entry) => {
         "util",
         "v2_spec",
         "__mocks__",
+        "spec", 
     ];
 
     const dirsToUse = dirs
         .filter((dirName) => path.extname(dirName) === "") // exclude non-folders
-        .filter((dirName) => dirsToIgnore.indexOf(dirName) === -1);
+        .filter((dirName) => dirsToIgnore.indexOf(dirName) === -1)
+        .filter((dirName) => !dirName.startsWith(".")); // Exclude hidden files/folders like .DS_Store
 
     return dirsToUse;
 };
