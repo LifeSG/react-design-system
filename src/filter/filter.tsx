b2b161663594dba5b59e60bc20082beeced61a86
Nobel <nteo@gt.tech.gov.sg>
2025-07-18 21:42:07 +0800

    [BOOKINGSG-7797][NT] Addeed aria-labels to filter components

--- src/filter/filter.tsx
+++ src/filter/filter.tsx
@@ -95,7 +95,7 @@ const FilterBase = ({
                         onClick={handleDismissFilter}
                         focusOutline="browser"
                         focusHighlight={false}
-                        aria-label="Dismiss"
+                        aria-label={`close ${headerTitle}`}
                     >
                         <CrossIcon />
                     </FilterHeaderButton>
@@ -106,6 +106,7 @@ const FilterBase = ({
                     type="button"
                     onClick={handleClearClick}
                     disabled={clearButtonDisabled}
+                    aria-label={`clear ${headerTitle}`}
                 >
                     Clear
                 </FilterClearButton>
