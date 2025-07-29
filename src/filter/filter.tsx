fe489d8aa82cdab4a62e3f59f173b10929b45a17
Nobel <nteo@gt.tech.gov.sg>
2025-07-29 18:07:24 +0800

    [BOOKINGSG-7797][NT] Trapped focus within floating element for mobile filter

--- src/filter/filter.tsx
+++ src/filter/filter.tsx
@@ -22,6 +22,7 @@ import {
     MobileOverlayContainer,
 } from "./filter.styles";
 import { FilterProps, Mode } from "./types";
+import { FloatingFocusManager, useFloating } from "@floating-ui/react";
 
 const FilterBase = ({
     toggleFilterButtonLabel = "Filters",
@@ -43,6 +44,7 @@ const FilterBase = ({
     const theme = useTheme();
     const mobileBreakpoint = Breakpoint["lg-max"]({ theme });
     const isMobile = useMediaQuery({ maxWidth: mobileBreakpoint });
+    const { context } = useFloating();
 
     // =============================================================================
     // EFFECTS
@@ -126,25 +128,31 @@ const FilterBase = ({
                 >
                     {toggleFilterButtonLabel}
                 </FilterButton>
-                <Overlay show={visible} disableTransition>
-                    <MobileOverlayContainer
-                        data-id="filter-mobile"
-                        data-testid="filter-mobile"
-                    >
-                        <MobileContainer ref={mobileNodeRef}>
-                            {renderHeader("mobile")}
-                            <FilterBody>{renderChildren("mobile")}</FilterBody>
-                            <FilterFooter>
-                                <FilterDoneButton
-                                    onClick={handleDoneClick}
-                                    type="button"
-                                >
-                                    Done
-                                </FilterDoneButton>
-                            </FilterFooter>
-                        </MobileContainer>
-                    </MobileOverlayContainer>
-                </Overlay>
+                {visible && (
+                    <FloatingFocusManager context={context} modal={false}>
+                        <Overlay show={visible} disableTransition>
+                            <MobileOverlayContainer
+                                data-id="filter-mobile"
+                                data-testid="filter-mobile"
+                            >
+                                <MobileContainer ref={mobileNodeRef}>
+                                    {renderHeader("mobile")}
+                                    <FilterBody>
+                                        {renderChildren("mobile")}
+                                    </FilterBody>
+                                    <FilterFooter>
+                                        <FilterDoneButton
+                                            onClick={handleDoneClick}
+                                            type="button"
+                                        >
+                                            Done
+                                        </FilterDoneButton>
+                                    </FilterFooter>
+                                </MobileContainer>
+                            </MobileOverlayContainer>
+                        </Overlay>
+                    </FloatingFocusManager>
+                )}
             </>
         );
     };
