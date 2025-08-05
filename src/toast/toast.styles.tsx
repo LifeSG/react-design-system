34579b5ea998ce61a0f7313257eb078d461a1e9e
Yip Jun Han <junhan.y_dcube@scientecpeople.com>
2025-08-06 01:29:07 +0800

    [MISC][JH] set title to h2 to avoid disrupting page heading hierarchy

--- src/toast/toast.styles.tsx
+++ src/toast/toast.styles.tsx
@@ -2,7 +2,7 @@ import { animated } from "react-spring";
 import styled, { css } from "styled-components";
 import { Button } from "../button";
 import { ClickableIcon } from "../shared/clickable-icon";
-import { Colour, MediaQuery, Radius } from "../theme";
+import { Colour, Font, MediaQuery, Radius } from "../theme";
 import { SemanticColourSet } from "../theme/types";
 import { Typography } from "../typography/typography";
 import { ToastType } from "./types";
@@ -126,8 +126,9 @@ export const TextContainer = styled.div`
     flex-direction: column;
 `;
 
-export const Title = styled(Typography.HeadingXS)<StyleProps>`
+export const Title = styled(Typography.HeadingXL)<StyleProps>`
     display: flex;
+    ${Font["body-baseline-semibold"]}
 
     ${(props) => {
         return css`
