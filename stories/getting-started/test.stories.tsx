c07d60337edbda23fc9ae1f10767c65a1fd3c215
Quek Ruo Ling <ruoling.quek@ufinity.com>
2025-09-03 18:26:05 +0800

    Temp stories

---
+++ stories/getting-started/test.stories.tsx
@@ -0,0 +1,130 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+import { Filter } from "src/filter";
+import { Button } from "../../src";
+import { Input } from "../../src/input";
+import { Modal } from "../../src/modal";
+import { Typography } from "../../src/typography";
+
+const meta: Meta = {
+    title: "Getting started/Test",
+    parameters: {
+        layout: "fullscreen",
+    },
+};
+
+export default meta;
+
+const ExampleFilter = () => (
+    <Filter _dangerouslyForceMobile>
+        {() => (
+            <>
+                <Filter.Item>
+                    <Input />
+                </Filter.Item>
+                <Filter.Item>
+                    <Typography.BodyBL>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. In rerum porro eius culpa. Reprehenderit dolor
+                        dolore aspernatur maiores quis veritatis illum assumenda
+                        fugit quam, error quibusdam quaerat earum tenetur dicta?
+                    </Typography.BodyBL>
+                </Filter.Item>
+                <Filter.Item title="Text">
+                    <Input />
+                </Filter.Item>
+                <Filter.Item title="Just an example of long content that may wrap to the next line">
+                    <Typography.BodyBL>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. In rerum porro eius culpa. Reprehenderit dolor
+                        dolore aspernatur maiores quis veritatis illum assumenda
+                        fugit quam, error quibusdam quaerat earum tenetur dicta?
+                    </Typography.BodyBL>
+                </Filter.Item>
+                <Filter.Item>
+                    <Input />
+                </Filter.Item>
+                <Filter.Item>
+                    <Typography.BodyBL>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. In rerum porro eius culpa. Reprehenderit dolor
+                        dolore aspernatur maiores quis veritatis illum assumenda
+                        fugit quam, error quibusdam quaerat earum tenetur dicta?
+                    </Typography.BodyBL>
+                </Filter.Item>
+            </>
+        )}
+    </Filter>
+);
+
+export const ScrollablePage: StoryObj = {
+    render: (_args) => {
+        const [show, setShow] = useState(false);
+        return (
+            <>
+                <ExampleFilter />
+                <Button.Default onClick={() => setShow(true)}>
+                    Open modal
+                </Button.Default>
+                <div style={{ background: "#eee" }}>
+                    {Array(20)
+                        .fill(null)
+                        .map((_, i) => (
+                            <Typography.BodyBL paragraph key={i}>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Blanditiis aliquid temporibus
+                                tempore consequuntur distinctio. Temporibus
+                                laborum sit illo magni aliquam, veritatis
+                                tenetur fugiat, cumque harum odit, repellat
+                                quaerat minima ad!
+                            </Typography.BodyBL>
+                        ))}
+                </div>
+                <Modal show={show} onOverlayClick={() => setShow(false)}>
+                    <Modal.Box
+                        style={{ height: "100%" }}
+                        onClose={() => setShow(false)}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </Modal.Box>
+                </Modal>
+            </>
+        );
+    },
+};
+
+export const NonScrollablePage: StoryObj = {
+    render: (_args) => {
+        const [show, setShow] = useState(false);
+        return (
+            <>
+                <ExampleFilter />
+                <Button.Default onClick={() => setShow(true)}>
+                    Open modal
+                </Button.Default>
+                <div style={{ background: "#eee" }}>
+                    {Array(1)
+                        .fill(null)
+                        .map((_, i) => (
+                            <Typography.BodyBL paragraph key={i}>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Blanditiis aliquid temporibus
+                                tempore consequuntur distinctio. Temporibus
+                                laborum sit illo magni aliquam, veritatis
+                                tenetur fugiat, cumque harum odit, repellat
+                                quaerat minima ad!
+                            </Typography.BodyBL>
+                        ))}
+                </div>
+                <Modal show={show} onOverlayClick={() => setShow(false)}>
+                    <Modal.Box
+                        style={{ height: "100%" }}
+                        onClose={() => setShow(false)}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </Modal.Box>
+                </Modal>
+            </>
+        );
+    },
+};
