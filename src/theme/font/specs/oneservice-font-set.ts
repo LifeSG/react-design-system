3eac00ed9a0b134750496f58ace65f0556b984fb
Tien Tran <tien.tran.ngoc@ncs-sdc.com>
2026-01-19 10:45:04 +0700

    [os-font-specs][TIEN] add font specs to OneserviceThemeBase and update storybook

--- src/theme/font/specs/oneservice-font-set.ts
+++ src/theme/font/specs/oneservice-font-set.ts
@@ -1,6 +1,4 @@
 import { FontSet } from "../types";
 import { getFontSet } from "../utils";
 
-export const OneServiceFontSet: FontSet = getFontSet({
-    disableLigatures: true,
-});
+export const OneServiceFontSet: FontSet = getFontSet();
