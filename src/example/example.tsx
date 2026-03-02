1b0e3598d46c84fc0878b66fec4f61a3e8f03b7f
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-03-02 21:48:22 +0800

    [BOOKINGSG-8941][RL] Set up Linaria for component

--- src/example/example.tsx
+++ src/example/example.tsx
@@ -1,4 +1,5 @@
-import { useLayoutEffect, useRef } from "react";
+import { css } from "@linaria/core";
+import { useLayoutEffect, useRef, useState } from "react";
 import { ExampleProps } from "./types";
 
 // TODO: remove after v4 migration is complete
@@ -6,6 +7,7 @@ export const Example = ({
     children,
     ...otherProps
 }: ExampleProps): JSX.Element => {
+    const [show, setShow] = useState(false);
     const ref = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
@@ -19,8 +21,13 @@ export const Example = ({
     }, []);
 
     return (
-        <div ref={ref} {...otherProps}>
-            {children}
+        <div ref={ref} className={wrapper} {...otherProps}>
+            {show && <div>{children}</div>}
+            <button onClick={() => setShow(!show)}>click me</button>
         </div>
     );
 };
+
+const wrapper = css`
+    background: transparent;
+`;
