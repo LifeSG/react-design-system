b1e0fab2f3543a397856144b765b416c29340d89
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-03-02 17:26:31 +0700

    [BOOKINGSG-8935][ZZ] refactor: separate tokens and adjust names

---
+++ src/theme/shadow.ts
@@ -0,0 +1,12 @@
+export const Shadow = {
+    "xs-subtle": "var(--fds-shadow-xs-subtle)",
+    "xs-strong": "var(--fds-shadow-xs-strong)",
+    "xs-focus-strong": "var(--fds-shadow-xs-focus-strong)",
+    "xs-error-strong": "var(--fds-shadow-xs-error-strong)",
+    "sm-subtle": "var(--fds-shadow-sm-subtle)",
+    "sm-strong": "var(--fds-shadow-sm-strong)",
+    "md-subtle": "var(--fds-shadow-md-subtle)",
+    "md-strong": "var(--fds-shadow-md-strong)",
+    "lg-subtle": "var(--fds-shadow-lg-subtle)",
+    "lg-strong": "var(--fds-shadow-lg-strong)",
+};
