d3cb0298039ca158b9250768ae197595d8894457
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-03-04 16:53:32 +0700

    [BOOKINGSG-8935][ZZ] chore: remove no-common-ligatures and sort exports

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,8 @@
+export { Border } from "./tokens/border";
+export { Breakpoint } from "./tokens/breakpoint";
 export { Colour } from "./tokens/colour";
 export { Font } from "./tokens/font";
-export { Border } from "./tokens/border";
-export { Radius } from "./tokens/radius";
-export { Spacing } from "./tokens/spacing";
-export { Shadow } from "./tokens/shadow";
 export { Motion } from "./tokens/motion";
-export { Breakpoint } from "./tokens/breakpoint";
+export { Radius } from "./tokens/radius";
+export { Shadow } from "./tokens/shadow";
+export { Spacing } from "./tokens/spacing";
