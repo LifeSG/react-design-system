d4de2fab9a5119f18b2926884ee62ff8ee166232
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-03-04 16:53:32 +0700

    [BOOKINGSG-8935][ZZ] chore: remove no-common-ligatures and sort exports

--- src/theme/tokens/font.ts
+++ src/theme/tokens/font.ts
@@ -64,8 +64,6 @@ export const Font = {
     Spec: {
         "font-family": "var(--fds-font-family)",
         "font-variant": "var(--fds-font-variant)",
-        "font-variant-no-common-ligatures":
-            "var(--fds-font-variant) no-common-ligatures",
 
         "weight-light": "var(--fds-font-weight-light)",
         "weight-regular": "var(--fds-font-weight-regular)",
