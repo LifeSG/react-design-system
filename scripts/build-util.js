48d696f5f9f99f7058cfa432bacb74e42789c7a5
Ruo Ling <quek.ruoling@gmail.com>
2026-03-16 14:01:44 +0800

    [MISC][RL] Backport build optimisations

--- scripts/build-util.js
+++ scripts/build-util.js
@@ -1,24 +1,4 @@
-const fs = require("fs");
 const path = require("path");
-
-export const getFolders = (entry) => {
-    const dirs = fs.readdirSync(entry);
-
-    // folders without an index.ts
-    const dirsToIgnore = [
-        "custom-types",
-        "shared",
-        "util",
-        "v2_spec",
-        "__mocks__",
-    ];
-
-    const dirsToUse = dirs
-        .filter((dirName) => path.extname(dirName) === "") // exclude non-folders
-        .filter((dirName) => dirsToIgnore.indexOf(dirName) === -1);
-
-    return dirsToUse;
-};
 
 export const injectCss = (cssVariableName, fileId) => {
     const module = path.relative(".", fileId);
