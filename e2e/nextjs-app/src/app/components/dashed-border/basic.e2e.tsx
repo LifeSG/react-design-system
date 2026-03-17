d0a8b9137e2784ff7fd08b1a38576ec6798ff833
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-03-17 12:35:25 +0700

    [BOOKINGSG-9001][GZ] remove component casting

--- e2e/nextjs-app/src/app/components/dashed-border/basic.e2e.tsx
+++ e2e/nextjs-app/src/app/components/dashed-border/basic.e2e.tsx
@@ -1,29 +1,24 @@
 "use client";
 import {
     DashedBorder,
-    DashedBorderProps,
     Border,
     Colour,
     Radius,
 } from "@lifesg/react-design-system";
-import { ComponentType } from "react";
-
-const DashedBorderComponent =
-    DashedBorder as unknown as ComponentType<DashedBorderProps>;
 
 export default function Story() {
     return (
         <div>
-            <DashedBorderComponent
+            <DashedBorder
                 data-testid="dashed-border-enabled"
                 thickness={Border["width-020"]}
                 radius={Radius.sm}
                 colour={Colour["border-primary"]}
             >
                 Enabled dashed border
-            </DashedBorderComponent>
+            </DashedBorder>
 
-            <DashedBorderComponent
+            <DashedBorder
                 data-testid="dashed-border-disabled"
                 enabled={false}
                 thickness={Border["width-020"]}
@@ -31,7 +26,7 @@ export default function Story() {
                 colour={Colour["border-primary"]}
             >
                 Disabled dashed border
-            </DashedBorderComponent>
+            </DashedBorder>
         </div>
     );
 }
