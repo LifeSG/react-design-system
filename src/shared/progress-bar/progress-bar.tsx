a03cb1433682f768d83e9c3ab05890dbd80b5e13
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-03-25 12:57:34 +0700

    [BOOKINGSG-9033][ZZ] refactor: change progress bar to linaria

--- src/shared/progress-bar/progress-bar.tsx
+++ src/shared/progress-bar/progress-bar.tsx
@@ -1,49 +1,20 @@
-import styled, { css } from "styled-components";
+import { css } from "@linaria/core";
+import clsx from "clsx";
 
-import { V3_Border, V3_Colour, V3_Radius } from "../../v3_theme";
-import type { getPrimitiveColour } from "../../v3_theme/colour-primitive/theme-helper";
-import type { getSemanticColour } from "../../v3_theme/colour-semantic/theme-helper";
-
-type ThemeColorFunctions = ReturnType<
-    typeof getSemanticColour | typeof getPrimitiveColour
->;
+import { Border, Colour, Radius } from "../../theme";
 
 interface Props {
     className?: string | undefined;
     /** Progress from 0 to 1 */
     progress: number;
-    color?: string | ThemeColorFunctions | undefined;
+    color?: string | undefined;
     "data-testid"?: string | undefined;
-}
-
-export const ProgressBar = ({
-    className,
-    progress,
-    color,
-    "data-testid": testId,
-}: Props) => (
-    <Bar
-        className={className}
-        $innerWidth={progress}
-        $color={color}
-        data-testid={testId}
-    >
-        <progress value={progress * 100} max={100} />
-    </Bar>
-);
-
-// =============================================================================
-// STYLE INTERFACE
-// =============================================================================
-interface StyleProps {
-    $color?: string | ThemeColorFunctions | undefined;
-    $innerWidth: number;
 }
 
 // =============================================================================
 // STYLING
 // =============================================================================
-const Bar = styled.div<StyleProps>`
+const barBaseClass = css`
     position: relative;
     width: 100%;
     height: 8px;
@@ -57,33 +28,60 @@ const Bar = styled.div<StyleProps>`
         height: 100%;
         opacity: 0;
     }
-
-    ${(props) => {
-        const { $color: color } = props;
-        let colorToUse: string;
-        if (color && typeof color === "string") {
-            colorToUse = color;
-        } else if (color) {
-            colorToUse = (color as ThemeColorFunctions)(props);
-        } else {
-            colorToUse = V3_Colour["icon-primary-subtle"](props);
-        }
-
-        return css`
-            border: ${V3_Border["width-010"]} ${V3_Border["solid"]}
-                ${colorToUse};
-            border-radius: ${V3_Radius["sm"]};
-
-            &:after {
-                content: "";
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                left: 0;
-                border-radius: inherit;
-                background: ${colorToUse};
-                width: ${props.$innerWidth * 100}%;
-            }
-        `;
-    }}
 `;
+
+const dynamicBarBaseClass = css`
+    &:after {
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        border-radius: inherit;
+        background: var(--bar-color);
+        width: var(--bar-width);
+    }
+`;
+
+const barContainerClass = css`
+    border-style: solid;
+    border-width: ${Border["width-010"]};
+    border-radius: ${Radius["sm"]};
+`;
+
+export const ProgressBar = ({
+    className,
+    progress,
+    color,
+    "data-testid": testId,
+}: Props) => {
+    const colorToUse = color ?? Colour["icon-primary-subtle"];
+
+    return (
+        <div
+            className={clsx(
+                barBaseClass,
+                dynamicBarBaseClass,
+                barContainerClass,
+                className
+            )}
+            style={
+                {
+                    borderColor: colorToUse,
+                    "--bar-color": colorToUse,
+                    "--bar-width": `${progress * 100}%`,
+                } as React.CSSProperties
+            }
+            data-testid={testId}
+        >
+            <progress
+                value={progress * 100}
+                max={100}
+                style={{
+                    background: colorToUse,
+                    width: `${progress * 100}%`,
+                }}
+            />
+        </div>
+    );
+};
