dac54e228d914e0d1ae8469238393bf66494bd19
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-03-26 15:01:12 +0800

    [BOOKINGSG-9044][RL] Fix flaky fallback image

--- src/shared/image-with-fallback/image-with-fallback.tsx
+++ src/shared/image-with-fallback/image-with-fallback.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import image from "./default.png";
 
 export interface ImageWithFallbackProps {
@@ -13,15 +15,35 @@ export const ImageWithFallback = ({
     className,
     "data-testid": testId,
 }: ImageWithFallbackProps) => {
+    const ref = useRef<HTMLImageElement>(null);
+
+    useEffect(() => {
+        // onError does not run if image is loaded from cache, so we need to
+        // check if the image is actually able to render using naturalWidth and
+        // naturalHeight
+        const el = ref.current;
+        if (
+            el &&
+            el.complete &&
+            el.naturalHeight === 0 &&
+            el.naturalWidth === 0
+        ) {
+            el.onerror = null;
+            el.src = image;
+        }
+    }, []);
+
     const handleError = (
         event: React.SyntheticEvent<HTMLImageElement, Event>
     ) => {
-        (event.target as HTMLImageElement).onerror = null;
-        (event.target as HTMLImageElement).src = image;
+        const el = event.target as HTMLImageElement;
+        el.onerror = null;
+        el.src = image;
     };
 
     return (
         <img
+            ref={ref}
             src={src}
             alt={alt || ""}
             className={className}
