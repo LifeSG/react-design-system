301faff304cd12c87295a21b0d314425f94b1e78
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-03-26 12:00:41 +0800

    [BOOKINGSG-9044][RL] ImageButton - Rename style file extension

--- src/image-button/image-button.tsx
+++ src/image-button/image-button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import type { ButtonRef } from "../button/types";
 import { ImageWithFallback } from "../shared/image-with-fallback/image-with-fallback";
-import { Main } from "./image-button.style";
+import { Main } from "./image-button.styles";
 import type { ImageButtonProps } from "./types";
 
 const Component = (
