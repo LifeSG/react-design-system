b2e0f768eeb4871aad190cb73c7afe6dd600e83d
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-03-27 09:43:29 +0800

    [V4][RL] Temporarily disable style import autofix

--- eslint-local-rules/import-path-preferences.mjs
+++ eslint-local-rules/import-path-preferences.mjs
@@ -300,14 +300,14 @@ const styleNamespaceImportRule = {
                 context.report({
                     node,
                     messageId: "namespaceStyleImport",
-                    // Autofix is disabled until linaria migration is complete.
+                    // TODO: Autofix is disabled until linaria migration is complete.
 
-                    fix(fixer) {
-                        const quote = getQuote(node.source.raw);
-                        const localName = getStyleNamespaceLocalName(node);
-                        const fixedImport = `import * as ${localName} from ${quote}${importSource}${quote};`;
-                        return fixer.replaceText(node, fixedImport);
-                    },
+                    // fix(fixer) {
+                    //     const quote = getQuote(node.source.raw);
+                    //     const localName = getStyleNamespaceLocalName(node);
+                    //     const fixedImport = `import * as ${localName} from ${quote}${importSource}${quote};`;
+                    //     return fixer.replaceText(node, fixedImport);
+                    // },
                 });
             },
         };
