78de413162137e51107bf0b2c1e46fcae3fb7a2b
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-03-30 15:42:02 +0700

    [BOOKINGSG-9046][GZ] add visual testing

---
+++ e2e/tests/components/typography/typography.e2e.spec.ts
@@ -0,0 +1,67 @@
+import { test as base, expect, Locator, Page } from "@playwright/test";
+import { AbstractStoryPage, compareScreenshot } from "../../utils";
+
+class StoryPage extends AbstractStoryPage {
+    protected readonly component = "typography";
+
+    public readonly locators: {
+        lightWeights: Locator;
+        semiboldWeights: Locator;
+        boldWeights: Locator;
+        paragraph: Locator;
+        clampOneLine: Locator;
+        clampTwoLines: Locator;
+        underlineLink: Locator;
+        noUnderlineLink: Locator;
+    };
+
+    constructor(page: Page) {
+        super(page);
+
+        this.locators = {
+            lightWeights: page.locator("[data-testid$='-weight-light']"),
+            semiboldWeights: page.locator("[data-testid$='-weight-semibold']"),
+            boldWeights: page.locator("[data-testid$='-weight-bold']"),
+            paragraph: page.getByTestId("typography-paragraph"),
+            clampOneLine: page.getByTestId("typography-clamp-1"),
+            clampTwoLines: page.getByTestId("typography-clamp-2"),
+            underlineLink: page.getByTestId("typography-link-underline"),
+            noUnderlineLink: page.getByTestId("typography-link-no-underline"),
+        };
+    }
+}
+
+const test = base.extend<{ story: StoryPage }>({
+    story: async ({ page }, use) => {
+        const story = new StoryPage(page);
+        await use(story);
+    },
+});
+
+test.describe("Typography", () => {
+    test("Variants", async ({ story }) => {
+        await story.init("basic");
+
+        await test.step("Component mounts with all variants", async () => {
+            await expect(story.locators.lightWeights).toHaveCount(12);
+            await expect(story.locators.semiboldWeights).toHaveCount(12);
+            await expect(story.locators.boldWeights).toHaveCount(12);
+
+            await compareScreenshot(story, "mount");
+        });
+    });
+
+    test("Styles", async ({ story }) => {
+        await story.init("styles");
+
+        await test.step("Component mounts with style variants", async () => {
+            await expect(story.locators.paragraph).toBeVisible();
+            await expect(story.locators.clampOneLine).toBeVisible();
+            await expect(story.locators.clampTwoLines).toBeVisible();
+            await expect(story.locators.underlineLink).toBeVisible();
+            await expect(story.locators.noUnderlineLink).toBeVisible();
+
+            await compareScreenshot(story, "mount");
+        });
+    });
+});
