32de3a7037d59628e1762f4f33121a2397566b65
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-02 11:18:30 +0700

    [BOOKINGSG-9047][GZ] remove styling and separator props from nested counter page

--- e2e/nextjs-app/src/app/components/text-list/nested-counter.e2e.tsx
+++ e2e/nextjs-app/src/app/components/text-list/nested-counter.e2e.tsx
@@ -6,20 +6,20 @@ import styles from "./text-list.module.css";
 export default function Story() {
     return (
         <div data-testid="text-list-advanced-nested-counter">
-            <TextList.Ol counterSeparator=".">
+            <TextList.Ol>
                 <li>Item A</li>
                 <li>
                     Item B
-                    <TextList.Ol className={styles.customOrderedList}>
+                    <TextList.Ol>
                         <li>Nested 1</li>
                         <li>Nested 2</li>
                     </TextList.Ol>
                 </li>
                 <li>
                     Item C
-                    <TextList.Ol className={styles.customOrderedList}>
-                        <li>Nested 1</li>
-                        <li>Nested 2</li>
+                    <TextList.Ol className={styles["custom-ordered-list"]}>
+                        <li>Custom nested 1</li>
+                        <li>Custom nested 2</li>
                     </TextList.Ol>
                 </li>
                 <li>Item D</li>
