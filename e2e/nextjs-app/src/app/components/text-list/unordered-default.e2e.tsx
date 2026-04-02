f6a93998a43dde9e57dc033018674ed25a32cec4
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-02 11:46:02 +0700

    [BOOKINGSG-9047][GZ] include all bullet type variant

--- e2e/nextjs-app/src/app/components/text-list/unordered-default.e2e.tsx
+++ e2e/nextjs-app/src/app/components/text-list/unordered-default.e2e.tsx
@@ -5,11 +5,38 @@ import { TextList } from "@lifesg/react-design-system/text-list";
 export default function Story() {
     return (
         <div data-testid="text-list-unordered-default">
-            <TextList.Ul>
-                <li>First</li>
-                <li>Second</li>
-                <li>Third</li>
-            </TextList.Ul>
+            <div>
+                Circle
+                <TextList.Ul bulletType="circle">
+                    <li>First</li>
+                    <li>Second</li>
+                    <li>Third</li>
+                </TextList.Ul>
+            </div>
+            <div>
+                Disk
+                <TextList.Ul bulletType="disc">
+                    <li>First</li>
+                    <li>Second</li>
+                    <li>Third</li>
+                </TextList.Ul>
+            </div>
+            <div>
+                Square
+                <TextList.Ul bulletType="square">
+                    <li>First</li>
+                    <li>Second</li>
+                    <li>Third</li>
+                </TextList.Ul>
+            </div>
+            <div>
+                None
+                <TextList.Ul bulletType="none">
+                    <li>First</li>
+                    <li>Second</li>
+                    <li>Third</li>
+                </TextList.Ul>
+            </div>
         </div>
     );
 }
