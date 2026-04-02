9c42387026f97f27c046927817fa69200074455b
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-02 11:42:25 +0700

    [BOOKINGSG-9047][GZ] improve e2e story init to wait for layout to be visible

--- e2e/tests/components/text-list/text-list.e2e.spec.ts
+++ e2e/tests/components/text-list/text-list.e2e.spec.ts
@@ -1,4 +1,4 @@
-import { test as base, expect, Locator, Page } from "@playwright/test";
+import { test as base, Locator, Page } from "@playwright/test";
 import { AbstractStoryPage, compareScreenshot } from "../../utils";
 
 class StoryPage extends AbstractStoryPage {
@@ -41,37 +41,31 @@ const test = base.extend<{ story: StoryPage }>({
 test.describe("TextList", () => {
     test("Unordered default", async ({ story }) => {
         await story.init("unordered-default");
-        await expect(story.locators.unorderedDefault).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 
     test("Ordered default", async ({ story }) => {
         await story.init("ordered-default");
-        await expect(story.locators.orderedDefault).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 
     test("Ordered reversed", async ({ story }) => {
         await story.init("ordered-reversed");
-        await expect(story.locators.orderedReversed).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 
     test("Ordered custom start", async ({ story }) => {
         await story.init("ordered-start");
-        await expect(story.locators.orderedStart).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 
     test("Unordered with custom bullet icon", async ({ story }) => {
         await story.init("unordered-custom-bullet");
-        await expect(story.locators.unorderedCustomBullet).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 
     test("Nested counter", async ({ story }) => {
         await story.init("nested-counter");
-        await expect(story.locators.advancedNestedCounter).toBeVisible();
         await compareScreenshot(story, "mount");
     });
 });
