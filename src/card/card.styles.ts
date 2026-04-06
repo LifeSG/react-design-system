0648b8e2be2dcc54a09c3cdca3b6d42004e68aba
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-06 13:04:19 +0700

    [BOOKINGSG-9111][GZ] migrate token

--- src/card/card.styles.ts
+++ src/card/card.styles.ts
@@ -1,29 +1,22 @@
 import styled, { css } from "styled-components";
 
-import { Colour } from "../theme";
-import {
-    V3_Border,
-    V3_Colour,
-    V3_Radius,
-    V3_Shadow,
-    V3_Spacing,
-} from "../v3_theme";
+import { Border, Colour, Radius, Shadow, Spacing } from "../theme";
 
 // =============================================================================
 // STYLING
 // =============================================================================
 export const StyledCard = styled.div`
-    border-radius: ${V3_Radius["md"]};
+    border-radius: ${Radius["md"]};
     background: ${Colour.bg};
-    padding: ${V3_Spacing["spacing-16"]} ${V3_Spacing["spacing-32"]};
+    padding: ${Spacing["spacing-16"]} ${Spacing["spacing-32"]};
     ${(props) => {
         return props.theme?.colourMode === "dark"
             ? css`
-                  border: ${V3_Border["width-010"]} ${V3_Border["solid"]}
-                      ${V3_Colour["border"]};
+                  border: ${Border["width-010"]} ${Border["solid"]}
+                      ${Colour["border"]};
               `
             : css`
-                  box-shadow: ${V3_Shadow["md-subtle"]};
+                  box-shadow: ${Shadow["md-subtle"]};
               `;
     }}
 `;
