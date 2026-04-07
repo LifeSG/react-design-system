811af4d6d690403773dac48ef783e6fd46cba03c
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-07 11:49:57 +0700

    [BOOKINGSG-9111][GZ] remove unnecessary wrapper

--- e2e/nextjs-app/src/app/components/card/basic.e2e.tsx
+++ e2e/nextjs-app/src/app/components/card/basic.e2e.tsx
@@ -3,10 +3,8 @@ import { Card } from "@lifesg/react-design-system";
 
 export default function Story() {
     return (
-        <div>
-            <Card data-testid="card-default-text">
-                This is string content with default text styling.
-            </Card>
-        </div>
+        <Card data-testid="card-default-text">
+            This is string content with default text styling.
+        </Card>
     );
 }
