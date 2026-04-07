ab3c0054555f30dae007f4908f434502947ae459
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-07 11:47:33 +0700

    [BOOKINGSG-9111][GZ] remove custom component case

--- e2e/tests/components/card/card.e2e.spec.ts
+++ e2e/tests/components/card/card.e2e.spec.ts
@@ -1,23 +1,11 @@
-import { test as base, expect, Locator, Page } from "@playwright/test";
+import { test as base, Page } from "@playwright/test";
 import { AbstractStoryPage, compareScreenshot } from "../../utils";
 
 class StoryPage extends AbstractStoryPage {
     protected readonly component = "card";
 
-    public readonly locators: {
-        customContentCard: Locator;
-        customButton: Locator;
-        customCount: Locator;
-    };
-
     constructor(page: Page) {
         super(page);
-
-        this.locators = {
-            customContentCard: page.getByTestId("card-custom-content"),
-            customButton: page.getByTestId("card-custom-button"),
-            customCount: page.getByTestId("card-custom-count"),
-        };
     }
 }
 
@@ -42,27 +30,6 @@ test.describe("Card", () => {
             await story.init("basic", { mode: "dark" });
 
             await compareScreenshot(story, "mount");
-        });
-    });
-
-    test.describe(() => {
-        test("Custom content interaction works", async ({ story }) => {
-            await story.init("custom-content");
-
-            await test.step("Component mounts", async () => {
-                await expect(story.locators.customContentCard).toBeVisible();
-                await expect(story.locators.customCount).toHaveText("Count: 0");
-
-                await compareScreenshot(story, "mount");
-            });
-
-            await test.step("Child interaction works inside card", async () => {
-                await story.locators.customButton.click();
-
-                await expect(story.locators.customCount).toHaveText("Count: 1");
-
-                await compareScreenshot(story, "after-click");
-            });
         });
     });
 });
