3a174217c92c9ee1b19f3b80db24c877102f648a
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-04-09 12:53:09 +0700

    [BOOKINGSG-9115][ZZ] refactor: change media query implementation

--- src/layout/col-div.styles.ts
+++ src/layout/col-div.styles.ts
@@ -1,6 +1,6 @@
 import { css } from "@linaria/core";
 
-import { Breakpoint } from "../theme/tokens/breakpoint";
+import { MediaQuery } from "../theme";
 
 export const tokens = {
     xxsStart: "--fds-internal-colDiv-xxs-start",
@@ -24,27 +24,27 @@ export const colDiv = css`
 
     grid-column: var(${tokens.xxsStart}, auto) / span var(${tokens.xxsSpan}, 1);
 
-    @media (min-width: ${Breakpoint["xs-min"]}) {
+    ${MediaQuery.MinWidth.xs} {
         grid-column: var(${tokens.xsStart}, auto) / span
             var(${tokens.xsSpan}, 1);
     }
-    @media (min-width: ${Breakpoint["sm-min"]}) {
+    ${MediaQuery.MinWidth.sm} {
         grid-column: var(${tokens.smStart}, auto) / span
             var(${tokens.smSpan}, 1);
     }
-    @media (min-width: ${Breakpoint["md-min"]}) {
+    ${MediaQuery.MinWidth.md} {
         grid-column: var(${tokens.mdStart}, auto) / span
             var(${tokens.mdSpan}, 1);
     }
-    @media (min-width: ${Breakpoint["lg-min"]}) {
+    ${MediaQuery.MinWidth.lg} {
         grid-column: var(${tokens.lgStart}, auto) / span
             var(${tokens.lgSpan}, 1);
     }
-    @media (min-width: ${Breakpoint["xl-min"]}) {
+    ${MediaQuery.MinWidth.xl} {
         grid-column: var(${tokens.xlStart}, auto) / span
             var(${tokens.xlSpan}, 1);
     }
-    @media (min-width: ${Breakpoint["xxl-min"]}) {
+    ${MediaQuery.MinWidth.xxl} {
         grid-column: var(${tokens.xxlStart}, auto) / span
             var(${tokens.xxlSpan}, 1);
     }
