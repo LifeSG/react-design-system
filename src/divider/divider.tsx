558657b1f77202cc08c279ddd81bb1ea173e166a
Ryan.Nguyen <trungnh41@fpt.com>
2026-04-10 15:21:26 +0700

    [BOOKINGSG-9113][RYN] restore ColDiv handling

--- src/divider/divider.tsx
+++ src/divider/divider.tsx
@@ -1,9 +1,12 @@
 import clsx from "clsx";
 import { useRef } from "react";
+
+import { ColDiv } from "../layout/col-div";
 import { Colour, useApplyStyle, useResolvedTokenValue } from "../theme";
+import { isColourToken } from "../util/styling-helper";
+import { V2_ColDiv } from "../v2_layout/col-div";
 import * as styles from "./divider.styles";
 import type { DividerLineStyleType, DividerProps } from "./types";
-import { isColourToken } from "../util/styling-helper";
 
 const lineStyleMap: Record<DividerLineStyleType, string> = {
     solid: styles.solidLine,
@@ -13,8 +16,19 @@ const lineStyleMap: Record<DividerLineStyleType, string> = {
 export const Divider = ({
     thickness = 1,
     lineStyle = "solid",
+    layoutType = "flex",
     color,
     className,
+    xxsCols,
+    xsCols,
+    smCols,
+    mdCols,
+    lgCols,
+    xlCols,
+    xxlCols,
+    mobileCols,
+    tabletCols,
+    desktopCols,
     ...otherProps
 }: DividerProps) => {
     const ref = useRef<HTMLHRElement>(null);
@@ -35,7 +49,65 @@ export const Divider = ({
             url('data:image/svg+xml,<svg width="8" height="${thickness}" viewBox="0 0 8 1" xmlns="http://www.w3.org/2000/svg"><line x1="2" y1="1" x2="6" y2="1" stroke="${encodedColor}" stroke-width="${thickness}" stroke-dasharray="4 4" /></svg>')`,
     });
 
-    const lineClass = clsx(styles.lineBase, lineStyleMap[lineStyle], className);
+    const isV2Layout =
+        mobileCols !== undefined ||
+        tabletCols !== undefined ||
+        desktopCols !== undefined;
 
-    return <hr ref={ref} className={lineClass} {...otherProps} />;
+    const getColumnProps = () => {
+        const xxsColsProp = xxsCols || [1, -1];
+        const xsColsProp = xsCols || xxsColsProp;
+        const smColsProp = smCols || xsColsProp;
+        const mdColsProp = mdCols || smColsProp;
+        const lgColsProp = lgCols || mdColsProp;
+        const xlColsProp = xlCols || lgColsProp;
+        const xxlColsProp = xxlCols || xlColsProp;
+
+        return {
+            xxlCols: xxlColsProp,
+            xlCols: xlColsProp,
+            lgCols: lgColsProp,
+            mdCols: mdColsProp,
+            smCols: smColsProp,
+            xsCols: xsColsProp,
+            xxsCols: xxsColsProp,
+        };
+    };
+
+    const lineClass = clsx(styles.lineBase, lineStyleMap[lineStyle]);
+
+    switch (layoutType) {
+        case "flex":
+            return (
+                <hr
+                    ref={ref}
+                    className={clsx(lineClass, className)}
+                    {...otherProps}
+                />
+            );
+        case "grid":
+            if (isV2Layout) {
+                return (
+                    <V2_ColDiv
+                        className={className}
+                        mobileCols={mobileCols}
+                        tabletCols={tabletCols}
+                        desktopCols={desktopCols}
+                        {...otherProps}
+                    >
+                        <hr ref={ref} className={lineClass} />
+                    </V2_ColDiv>
+                );
+            } else {
+                return (
+                    <ColDiv
+                        className={className}
+                        {...getColumnProps()}
+                        {...otherProps}
+                    >
+                        <hr ref={ref} className={lineClass} />
+                    </ColDiv>
+                );
+            }
+    }
 };
