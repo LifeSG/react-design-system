9a21c0e71a30cf28d0d4e903a3a13f17ff8cd803
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-04-15 01:15:58 +0800

    [V4][RL] Refactor markup specs

--- e2e/tests/components/markup/markup.e2e.spec.ts
+++ e2e/tests/components/markup/markup.e2e.spec.ts
@@ -5,7 +5,6 @@ class StoryPage extends AbstractStoryPage {
     protected readonly component = "markup";
 
     public readonly locators: {
-        markup: Locator;
         link: Locator;
         strong: Locator;
     };
@@ -14,7 +13,6 @@ class StoryPage extends AbstractStoryPage {
         super(page);
 
         this.locators = {
-            markup: page.getByTestId("markup-markup"),
             link: page.getByRole("link", { name: /inline link/i }),
             strong: page.getByText("bold phrase"),
         };
@@ -28,33 +26,30 @@ const test = base.extend<{ story: StoryPage }>({
     },
 });
 
-const scenarios = [
-    {
-        story: "basic",
-        title: "Basic",
-    },
-    {
-        story: "with-text-size",
-        title: "With textSize",
-    },
-] as const;
-
 test.describe("Markup", () => {
-    scenarios.forEach((scenario) => {
-        test.describe(scenario.title, () => {
-            test.beforeEach(async ({ story }) => {
-                await story.init(scenario.story);
-            });
+    test.describe(() => {
+        test.beforeEach(async ({ story }) => {
+            await story.init("basic");
+        });
 
-            test(scenario.title, async ({ story }) => {
-                await test.step("Component mounts", async () => {
-                    await expect(story.locators.markup).toBeVisible();
-                    await expect(story.locators.link).toBeVisible();
-                    await expect(story.locators.strong).toBeVisible();
+        test("Basic", async ({ story }) => {
+            await expect(story.locators.link).toBeVisible();
+            await expect(story.locators.strong).toBeVisible();
 
-                    await compareScreenshot(story, "mount");
-                });
-            });
+            await compareScreenshot(story, "mount");
+        });
+    });
+
+    test.describe(() => {
+        test.beforeEach(async ({ story }) => {
+            await story.init("with-text-size");
+        });
+
+        test("With textSize", async ({ story }) => {
+            await expect(story.locators.link).toBeVisible();
+            await expect(story.locators.strong).toBeVisible();
+
+            await compareScreenshot(story, "mount");
         });
     });
 });
