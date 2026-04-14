d4883e7a46f68e2eece1d0fe2c726186995f81c8
Ryan.Nguyen <trungnh41@fpt.com>
2026-04-14 16:36:16 +0700

    [BOOKINGSG-9114][RYN] migrate to V4 tokens

--- src/error-display/error-display.styles.ts
+++ src/error-display/error-display.styles.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 import { Button } from "../button";
 import { Markup } from "../markup";
+import { Colour, MediaQuery, Spacing } from "../theme";
 import { Typography } from "../typography";
-import { V3_Colour, V3_MediaQuery, V3_Spacing } from "../v3_theme";
 
 export const Container = styled.div`
     display: flex;
@@ -18,15 +18,15 @@ export const Img = styled.img`
     width: 400px;
     height: auto;
 
-    ${V3_MediaQuery.MaxWidth.sm} {
+    ${MediaQuery.MaxWidth.sm} {
         width: 320px;
     }
 
-    ${V3_MediaQuery.MaxWidth.xs} {
+    ${MediaQuery.MaxWidth.xs} {
         width: 288px;
     }
 
-    ${V3_MediaQuery.MaxWidth.xxs} {
+    ${MediaQuery.MaxWidth.xxs} {
         width: 240px;
     }
 `;
@@ -41,24 +41,24 @@ export const TextContainer = styled.div`
 `;
 
 export const Title = styled(Typography.HeadingMD)`
-    margin: ${V3_Spacing["spacing-32"]} 0 ${V3_Spacing["spacing-16"]};
+    margin: ${Spacing["spacing-32"]} 0 ${Spacing["spacing-16"]};
     text-align: center;
 `;
 
 export const DescriptionContainer = styled(Markup)`
-    color: ${V3_Colour.text};
+    color: ${Colour.text};
     text-align: center;
 
     p + p {
-        margin-top: ${V3_Spacing["spacing-16"]};
+        margin-top: ${Spacing["spacing-16"]};
     }
 `;
 
 export const ActionButton = styled(Button.Default)`
-    margin: ${V3_Spacing["spacing-32"]} auto 0;
+    margin: ${Spacing["spacing-32"]} auto 0;
     width: 21rem;
 
-    ${V3_MediaQuery.MaxWidth.sm} {
+    ${MediaQuery.MaxWidth.sm} {
         width: 100%;
     }
 `;
