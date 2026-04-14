a175b5bb8b761370f85eefad05d4e3df3b81569c
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-04-15 00:47:49 +0800

    [V4][RL] Refactor divider specs

--- e2e/nextjs-app/src/app/components/divider/basic.e2e.tsx
+++ e2e/nextjs-app/src/app/components/divider/basic.e2e.tsx
@@ -5,14 +5,13 @@ export default function Story() {
     return (
         <div className="story-column-container">
             <h5>Default solid line</h5>
-            <Divider data-testid="divider-default" />
+            <Divider />
             <h5>Default dashed line</h5>
-            <Divider data-testid="divider-dashed" lineStyle="dashed" />
+            <Divider lineStyle="dashed" />
             <h5>Customized thickness & colour</h5>
-            <Divider data-testid="divider-custom-thickness" thickness={8} />
-            <Divider data-testid="divider-custom-color" color="red" />
+            <Divider thickness={8} />
+            <Divider color="red" />
             <Divider
-                data-testid="divider-dashed-custom"
                 lineStyle="dashed"
                 thickness={5}
                 color={Colour["border-primary"]}
