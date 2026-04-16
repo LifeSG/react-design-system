3d1f68d28e37b9fbf2c72a4d85f40ccd3cc1a969
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-16 15:08:58 +0700

    [BOOKINGSG-9158][GZ] delete element with dropdown style

--- src/shared/dropdown-wrapper/element-with-dropdown.styles.ts
+++
@@ -1,8 +0,0 @@
-import { css } from "@linaria/core";
-
-// hack to force child to respect max-height of parent
-// even though parent does not have explicit height
-export const dropdownContainer = css`
-    display: flex;
-    flex-direction: column;
-`;
