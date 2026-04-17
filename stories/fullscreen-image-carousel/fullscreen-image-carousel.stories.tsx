9239016e4fee72640d00f5e8abad85d5caa997c1
Miguel Parramón Teixidó <mparramon@palo-it.com>
2026-04-17 13:28:50 +0800

    [DCUBESDQLR-2356][MPT] change order of stories to make more sense semantically

--- stories/fullscreen-image-carousel/fullscreen-image-carousel.stories.tsx
+++ stories/fullscreen-image-carousel/fullscreen-image-carousel.stories.tsx
@@ -136,6 +136,54 @@ export const Configurable: StoryObj<Component> = {
     },
 };
 
+export const WithFileInfo: StoryObj<Component> = {
+    render: (_args) => {
+        const [show, setShow] = useState(false);
+        return (
+            <>
+                <Button.Default
+                    onClick={() => {
+                        setShow((old) => !old);
+                    }}
+                >
+                    Show carousel
+                </Button.Default>
+                <FullscreenImageCarousel
+                    items={[
+                        {
+                            src: "https://picsum.photos/id/157/1600/900",
+                            fileName: "landscape-photo.jpg",
+                            fileSize: "1.2 MB",
+                        },
+                        {
+                            src: "https://picsum.photos/id/10/1600/900",
+                            fileName:
+                                "this-is-a-very-long-file-name-that-should-be-truncated-when-it-exceeds-the-available-width-in-the-bar.jpg",
+                            fileSize:
+                                "234,567,890.12 MB (123,456,789.99 MB compressed)",
+                        },
+                        {
+                            src: "https://picsum.photos/id/369/1000/1000",
+                            fileName: "square-image.jpg",
+                        },
+                        {
+                            src: "https://picsum.photos/id/445/300/300",
+                            fileName: "small-photo.jpg",
+                            fileSize: "320 KB",
+                        },
+                        {
+                            src: "https://picsum.photos/id/237/800/600",
+                            fileName: "dog-photo.jpg",
+                        },
+                    ]}
+                    show={show}
+                    onClose={() => setShow(false)}
+                />
+            </>
+        );
+    },
+};
+
 export const WithCustomContent: StoryObj<Component> = {
     render: (_args) => {
         const [show, setShow] = useState(false);
@@ -189,54 +237,6 @@ export const WithCustomContent: StoryObj<Component> = {
                     ]}
                     show={show}
                     onDelete={() => undefined}
-                    onClose={() => setShow(false)}
-                />
-            </>
-        );
-    },
-};
-
-export const WithFileInfo: StoryObj<Component> = {
-    render: (_args) => {
-        const [show, setShow] = useState(false);
-        return (
-            <>
-                <Button.Default
-                    onClick={() => {
-                        setShow((old) => !old);
-                    }}
-                >
-                    Show carousel
-                </Button.Default>
-                <FullscreenImageCarousel
-                    items={[
-                        {
-                            src: "https://picsum.photos/id/157/1600/900",
-                            fileName: "landscape-photo.jpg",
-                            fileSize: "1.2 MB",
-                        },
-                        {
-                            src: "https://picsum.photos/id/10/1600/900",
-                            fileName:
-                                "this-is-a-very-long-file-name-that-should-be-truncated-when-it-exceeds-the-available-width-in-the-bar.jpg",
-                            fileSize:
-                                "234,567,890.12 MB (123,456,789.99 MB compressed)",
-                        },
-                        {
-                            src: "https://picsum.photos/id/369/1000/1000",
-                            fileName: "square-image.jpg",
-                        },
-                        {
-                            src: "https://picsum.photos/id/445/300/300",
-                            fileName: "small-photo.jpg",
-                            fileSize: "320 KB",
-                        },
-                        {
-                            src: "https://picsum.photos/id/237/800/600",
-                            fileName: "dog-photo.jpg",
-                        },
-                    ]}
-                    show={show}
                     onClose={() => setShow(false)}
                 />
             </>
