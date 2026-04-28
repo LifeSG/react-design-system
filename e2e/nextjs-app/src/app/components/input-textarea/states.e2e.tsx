5601062e5af4c157b07f4a1d4567b7987aa0b2ee
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-04-28 16:15:43 +0700

    [BOOKINGSG-9155][GZ] add placeholder variants

--- e2e/nextjs-app/src/app/components/input-textarea/states.e2e.tsx
+++ e2e/nextjs-app/src/app/components/input-textarea/states.e2e.tsx
@@ -4,34 +4,62 @@ import { Form } from "@lifesg/react-design-system/form";
 
 export default function Story() {
     return (
-        <div className="story-column-container">
-            <Form.Textarea
-                data-testid="textarea-default"
-                label="Default textarea"
-                placeholder="Default textarea"
-                defaultValue="Default value"
-            />
-            <Form.Textarea
-                data-testid="textarea-readonly"
-                label="Readonly textarea"
-                placeholder="Readonly textarea"
-                value="Readonly value"
-                readOnly
-            />
-            <Form.Textarea
-                data-testid="textarea-disabled"
-                label="Disabled textarea"
-                placeholder="Disabled textarea"
-                value="Disabled value"
-                disabled
-            />
-            <Form.Textarea
-                data-testid="textarea-error"
-                label="Error textarea"
-                placeholder="Error textarea"
-                defaultValue="Error value"
-                errorMessage="This field has an error"
-            />
+        <div className="story-row-container">
+            <div className="story-column-container">
+                <Form.Textarea
+                    data-testid="textarea-default"
+                    label="Default textarea"
+                    placeholder="Default textarea placeholder"
+                />
+                <Form.Textarea
+                    data-testid="textarea-readonly"
+                    label="Readonly textarea"
+                    placeholder="Readonly textarea placeholder"
+                    readOnly
+                />
+                <Form.Textarea
+                    data-testid="textarea-disabled"
+                    label="Disabled textarea"
+                    placeholder="Disabled textarea placeholder"
+                    disabled
+                />
+                <Form.Textarea
+                    data-testid="textarea-error"
+                    label="Error textarea"
+                    placeholder="Error textarea placeholder"
+                    errorMessage="This field has an error"
+                />
+            </div>
+
+            <div className="story-column-container">
+                <Form.Textarea
+                    data-testid="textarea-default"
+                    label="Default textarea"
+                    placeholder="Default textarea placeholder"
+                    defaultValue="Default value"
+                />
+                <Form.Textarea
+                    data-testid="textarea-readonly"
+                    label="Readonly textarea"
+                    placeholder="Readonly textarea placeholder"
+                    value="Readonly value"
+                    readOnly
+                />
+                <Form.Textarea
+                    data-testid="textarea-disabled"
+                    label="Disabled textarea"
+                    placeholder="Disabled textarea placeholder"
+                    value="Disabled value"
+                    disabled
+                />
+                <Form.Textarea
+                    data-testid="textarea-error"
+                    label="Error textarea"
+                    placeholder="Error textarea placeholder"
+                    defaultValue="Error value"
+                    errorMessage="This field has an error"
+                />
+            </div>
         </div>
     );
 }
