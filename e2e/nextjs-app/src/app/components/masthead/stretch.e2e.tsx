fb678f0ce2243e38f7b21c43212f138db9599ced
Ryan.Nguyen <trungnh41@fpt.com>
2026-04-28 16:44:25 +0700

    [BOOKINGSG-9272][RYN] add e2e specs and tests

---
+++ e2e/nextjs-app/src/app/components/masthead/stretch.e2e.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Masthead } from "@lifesg/react-design-system/masthead";
+
+export default function Story() {
+    return (
+        <div className="story-column-container story-background">
+            <div data-testid="masthead-default">
+                <Masthead stretch />
+            </div>
+        </div>
+    );
+}
