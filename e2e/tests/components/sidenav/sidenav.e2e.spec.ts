c9819df559d5df9405f16d39e931200204a7e3a1
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-05-05 14:46:57 +0700

    [BOOKINGSG-9210][ZZ] test: adding visual test for sidenav

---
+++ e2e/tests/components/sidenav/sidenav.e2e.spec.ts
@@ -0,0 +1,163 @@
+import { test as base, expect, Locator, Page } from "@playwright/test";
+import { AbstractStoryPage, compareScreenshot } from "../../utils";
+
+class StoryPage extends AbstractStoryPage {
+    protected readonly component = "sidenav";
+
+    public readonly locators: {
+        sidenav: Locator;
+        itemDashboard: Locator;
+        itemUsers: Locator;
+        itemSettings: Locator;
+        drawer: Locator;
+        drawerItemAllUsers: Locator;
+        drawerItemUserGroups: Locator;
+        drawerSubitemAdmins: Locator;
+        drawerSubitemContainer: Locator;
+        clickedId: Locator;
+    };
+
+    constructor(page: Page) {
+        super(page);
+        this.locators = {
+            sidenav: page.getByTestId("sidenav"),
+            itemDashboard: page.getByTestId("item-dashboard"),
+            itemUsers: page.getByTestId("item-users"),
+            itemSettings: page.getByTestId("item-settings"),
+            drawer: page.getByTestId("sidenav-drawer"),
+            drawerItemAllUsers: page.getByRole("button", { name: "All Users" }),
+            drawerItemUserGroups: page.getByRole("button", {
+                name: "User Groups",
+            }),
+            drawerSubitemAdmins: page.getByRole("button", { name: "Admins" }),
+            drawerSubitemContainer: page
+                .getByTestId("drawer-subitem-container")
+                .nth(1),
+            clickedId: page.getByTestId("clicked-id"),
+        };
+    }
+
+    async openDrawer() {
+        await this.locators.itemUsers.click();
+        await expect(this.locators.drawer).toBeVisible();
+    }
+}
+
+const test = base.extend<{ story: StoryPage }>({
+    story: async ({ page }, use) => {
+        const story = new StoryPage(page);
+        await use(story);
+    },
+});
+
+test.describe("Sidenav", () => {
+    test.describe(() => {
+        test.beforeEach(async ({ story }) => {
+            await story.init("default");
+        });
+
+        test("Default states", async ({ story }) => {
+            await compareScreenshot(story, "mount", { fullscreen: true });
+        });
+
+        test("Navigation structure", async ({ story }) => {
+            await expect(story.locators.sidenav).toMatchAriaSnapshot(`
+                - navigation "Sidebar":
+                    - list:
+                        - listitem:
+                            - button "Dashboard"
+                        - listitem:
+                            - button "Users" [expanded=false]
+                    - list:
+                        - listitem:
+                            - button "Settings"
+            `);
+        });
+
+        test("Drawer open on click", async ({ story }) => {
+            await story.openDrawer();
+            await compareScreenshot(story, "drawer-open", { fullscreen: true });
+        });
+
+        test("Drawer accessibility tree when open", async ({ story }) => {
+            await story.openDrawer();
+            await expect(story.locators.drawer).toMatchAriaSnapshot(`
+                - list:
+                    - listitem:
+                        - button "All Users"
+                    - listitem:
+                        - button "User Groups" [expanded=true]
+                        - list:
+                            - listitem:
+                                - button "Admins"
+                            - listitem:
+                                - button "Members"
+            `);
+        });
+
+        test("Drawer positioning and offset", async ({ story }) => {
+            await story.openDrawer();
+            const sidenavBox = await story.locators.sidenav.boundingBox();
+            const drawerBox = await story.locators.drawer.boundingBox();
+            expect(drawerBox!.x).toBeGreaterThanOrEqual(
+                sidenavBox!.x + sidenavBox!.width
+            );
+        });
+
+        test("DrawerItem expands and collapses", async ({ story }) => {
+            await story.openDrawer();
+
+            await test.step("Subitems visible when expanded by default", async () => {
+                await expect(
+                    story.locators.drawerSubitemContainer
+                ).not.toHaveCSS("height", "0px");
+            });
+
+            await test.step("Collapse drawer item hides subitems", async () => {
+                await story.locators.drawerItemUserGroups.click();
+                await compareScreenshot(story, "drawer-item-collapsed", {
+                    locator: story.locators.drawer,
+                });
+                await expect(story.locators.drawerSubitemContainer).toHaveCSS(
+                    "height",
+                    "0px"
+                );
+            });
+
+            await test.step("Expand drawer item shows subitems again", async () => {
+                await story.locators.drawerItemUserGroups.click();
+                await expect(
+                    story.locators.drawerSubitemContainer
+                ).not.toHaveCSS("height", "0px");
+            });
+        });
+
+        test("Keyboard navigation", async ({ story }) => {
+            await test.step("Tab to first item", async () => {
+                await story.page.keyboard.press("Tab");
+                await expect(story.locators.itemDashboard).toBeFocused();
+            });
+
+            await test.step("Tab to item with children, drawer opens", async () => {
+                await story.page.keyboard.press("Tab");
+                await expect(story.locators.itemUsers).toBeFocused();
+                await expect(story.locators.drawer).toBeVisible();
+            });
+
+            await test.step("Tab moves focus into drawer", async () => {
+                await story.page.keyboard.press("Tab");
+                await expect(story.locators.drawerItemAllUsers).toBeFocused();
+            });
+        });
+    });
+
+    test.describe(() => {
+        test.beforeEach(async ({ story }) => {
+            await story.init("default", { mode: "dark" });
+        });
+
+        test("Default states dark mode", async ({ story }) => {
+            await compareScreenshot(story, "mount-dark", { fullscreen: true });
+        });
+    });
+});
