eab262d9beb9189c724726c02c44f381997e2c58
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-05-08 14:06:35 +0700

    [BOOKINGSG-9324][GZ] delete leftover test

--- tests/eslint-rules/import-path-preferences.rule-test.mjs
+++ tests/eslint-rules/import-path-preferences.rule-test.mjs
@@ -4,7 +4,6 @@ import { RuleTester } from "eslint";
 import localRules from "../../eslint-local-rules/index.mjs";
 
 const rule = localRules.rules["import-path-preferences"];
-const styleNamespaceImportRule = localRules.rules["style-namespace-import"];
 
 const ruleTester = new RuleTester({
     languageOptions: {
@@ -106,63 +105,6 @@ ruleTester.run("import-path-preferences", rule, {
             code: 'import * as DesignSystem from "../../../src";',
             output: 'import * as DesignSystem from "@lifesg/react-design-system";',
             errors: [{ messageId: "nextjsPackage" }],
-        },
-    ],
-});
-
-ruleTester.run("style-namespace-import", styleNamespaceImportRule, {
-    valid: [
-        {
-            name: "namespace import from .style path",
-            code: 'import * as styles from "./button.style";',
-        },
-        {
-            name: "namespace import from .styles path",
-            code: 'import * as styles from "./button.styles";',
-        },
-        {
-            name: "side-effect style import is allowed",
-            code: 'import "./button.styles";',
-        },
-        {
-            name: "non-style import can use named import",
-            code: 'import { Button } from "./button";',
-        },
-        {
-            name: "type namespace import from .styles path",
-            code: 'import type * as styles from "./button.styles";',
-        },
-        {
-            name: "type named import from .styles path is ignored",
-            code: 'import type { MainStyleProps } from "./button.styles";',
-        },
-        {
-            name: "parent-dir style import is ignored",
-            code: 'import { Main } from "../button.styles";',
-        },
-        {
-            name: "nested style import is ignored",
-            code: 'import { Main } from "./styles/button.styles";',
-        },
-        {
-            name: "named import from .style path is ignored",
-            code: 'import { Main } from "./button.style";',
-        },
-    ],
-    invalid: [
-        {
-            name: "default import from .styles path is rejected",
-            code: 'import styles from "./button.styles";',
-            // TODO: Autofix is disabled until linaria migration is complete.
-            // output: 'import * as styles from "./button.styles";',
-            output: null,
-            errors: [{ messageId: "namespaceStyleImport" }],
-        },
-        {
-            name: "default + namespace import is reduced to namespace only",
-            code: 'import buttonStyle, * as styles from "./button.styles";',
-            output: null,
-            errors: [{ messageId: "namespaceStyleImport" }],
         },
     ],
 });
