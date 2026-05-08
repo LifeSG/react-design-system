1c25db4d85e67ee80d3ea2437d879e48bd13eaff
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-05-08 17:14:02 +0800

    [BOOKINGSG-9285][RL] DateNavigator - Migrate V3 design tokens to V4

--- src/date-navigator/date-navigator.styles.ts
+++ src/date-navigator/date-navigator.styles.ts
@@ -1,14 +1,14 @@
 import styled, { css } from "styled-components";
 
 import { Button } from "../button";
-import { V3_Colour, V3_Radius, V3_Spacing } from "../v3_theme";
+import { Colour, Radius, Spacing } from "../theme";
 
 interface StyledDateTextProps {
     $enableDateClick?: boolean;
 }
 
 export const HeaderArrowButton = styled(Button)`
-    margin: ${V3_Spacing["spacing-8"]};
+    margin: ${Spacing["spacing-8"]};
 `;
 
 export const Container = styled.div`
@@ -16,13 +16,13 @@ export const Container = styled.div`
     position: relative;
     align-items: center;
     justify-content: space-between;
-    background-color: ${V3_Colour["bg-strong"]};
-    border-radius: ${V3_Radius["sm"]};
+    background-color: ${Colour["bg-strong"]};
+    border-radius: ${Radius["sm"]};
 `;
 
 export const StyledDateTextButton = styled(Button.Default)<StyledDateTextProps>`
     color: ${(props) =>
-        props.$enableDateClick ? V3_Colour["text-primary"] : V3_Colour["text"]};
+        props.$enableDateClick ? Colour["text-primary"] : Colour["text"]};
     white-space: nowrap;
     display: inline-block;
     text-wrap: auto;
