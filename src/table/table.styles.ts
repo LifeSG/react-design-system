c5da0751d9eb13e118438fc839710676b661a00c
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-05-11 10:19:53 +0700

    [BOOKINGSG-9293][ZZ] refactor: migrate v3 to v4 tokens

--- src/table/table.styles.ts
+++ src/table/table.styles.ts
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 
-import { V3_Border, V3_Colour, V3_Radius, V3_Spacing } from "../v3_theme";
+import { Border, Colour, Radius, Spacing } from "../theme";
 
 // =============================================================================
 // STYLES CONSTANTS
 // =============================================================================
-const borderColor = V3_Colour["border"];
-const fontColor = V3_Colour["text"];
+const borderColor = Colour["border"];
+const fontColor = Colour["text"];
 
 // =============================================================================
 // STYLES
 // =============================================================================
 export const TableWrapper = styled.div`
     overflow: auto;
-    border: ${V3_Border["width-010"]} ${V3_Border["solid"]} ${borderColor};
-    border-radius: ${V3_Radius["md"]};
+    border: ${Border["width-010"]} ${Border["solid"]} ${borderColor};
+    border-radius: ${Radius["md"]};
 
     // Hide scrollbar
     &::-webkit-scrollbar {
@@ -46,40 +46,38 @@ export const TableBody = styled.tbody`
 `;
 
 export const HeaderCell = styled.th`
-    padding: ${V3_Spacing["spacing-20"]} ${V3_Spacing["spacing-16"]};
+    padding: ${Spacing["spacing-20"]} ${Spacing["spacing-16"]};
     text-align: left;
     cursor: default;
     vertical-align: middle;
     color: ${fontColor};
-    background-color: ${V3_Colour["bg-stronger"]};
+    background-color: ${Colour["bg-stronger"]};
     height: 6rem;
-    border-bottom: ${V3_Border["width-010"]} ${V3_Border["solid"]}
-        ${borderColor};
+    border-bottom: ${Border["width-010"]} ${Border["solid"]} ${borderColor};
 
     &:where(&:first-child) {
-        padding-left: ${V3_Spacing["spacing-24"]};
+        padding-left: ${Spacing["spacing-24"]};
     }
     &:where(&:last-child) {
-        padding-right: ${V3_Spacing["spacing-24"]};
+        padding-right: ${Spacing["spacing-24"]};
     }
 `;
 
 export const BodyRow = styled.tr`
-    background-color: ${V3_Colour["bg"]};
-    border-top: ${V3_Border["width-010"]} ${V3_Border["solid"]} ${borderColor};
+    background-color: ${Colour["bg"]};
+    border-top: ${Border["width-010"]} ${Border["solid"]} ${borderColor};
 `;
 
 export const BodyCell = styled.td`
-    padding: ${V3_Spacing["spacing-20"]} ${V3_Spacing["spacing-16"]};
+    padding: ${Spacing["spacing-20"]} ${Spacing["spacing-16"]};
     vertical-align: middle;
     color: ${fontColor};
-    border-bottom: ${V3_Border["width-010"]} ${V3_Border["solid"]}
-        ${borderColor};
+    border-bottom: ${Border["width-010"]} ${Border["solid"]} ${borderColor};
 
     &:where(&:first-child) {
-        padding-left: ${V3_Spacing["spacing-24"]};
+        padding-left: ${Spacing["spacing-24"]};
     }
     &:where(&:last-child) {
-        padding-right: ${V3_Spacing["spacing-24"]};
+        padding-right: ${Spacing["spacing-24"]};
     }
 `;
