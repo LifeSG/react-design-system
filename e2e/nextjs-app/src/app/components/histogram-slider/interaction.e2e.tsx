eff9782fe3164fa27805ea8c1d8793c2d6d71028
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-05-11 08:36:43 +0700

    [BOOKINGSG-9280][ZZ] refactor: remove duplicate files and remove unused variable in tests

--- e2e/nextjs-app/src/app/components/histogram-slider/interaction.e2e.tsx
+++ e2e/nextjs-app/src/app/components/histogram-slider/interaction.e2e.tsx
@@ -30,11 +30,3 @@ export default function Story() {
         </div>
     );
 }
-
-const MOCK_BINS = [
-    { count: 2, minValue: 1 },
-    { count: 5, minValue: 2 },
-    { count: 8, minValue: 3 },
-    { count: 3, minValue: 4 },
-    { count: 1, minValue: 5 },
-];
