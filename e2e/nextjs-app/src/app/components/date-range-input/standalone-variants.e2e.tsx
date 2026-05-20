39652f888179d6ba08dbb39b5b2119cc0a53022c
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-05-20 10:13:15 +0700

    [BOOKINGSG-9284][ZZ] test: adding story background for dark modes

--- e2e/nextjs-app/src/app/components/date-range-input/standalone-variants.e2e.tsx
+++ e2e/nextjs-app/src/app/components/date-range-input/standalone-variants.e2e.tsx
@@ -4,7 +4,7 @@ import { DateRangeInput } from "@lifesg/react-design-system/date-range-input";
 
 export default function Story() {
     return (
-        <div className="story-column-container">
+        <div className="story-column-container story-background">
             <DateRangeInput data-testid="date-range-input-default" />
             <DateRangeInput data-testid="date-range-input-disabled" disabled />
             <DateRangeInput data-testid="date-range-input-readonly" readOnly />
