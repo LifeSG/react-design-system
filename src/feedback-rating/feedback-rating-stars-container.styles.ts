bb24c8fde055f283fcc108aefbd182a98f442ea3
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-05-20 15:31:44 +0800

    [BOOKINGSG-9238][IT] Migrate v3 design tokens to v4 tokens

--- src/feedback-rating/feedback-rating-stars-container.styles.ts
+++ src/feedback-rating/feedback-rating-stars-container.styles.ts
@@ -2,13 +2,7 @@ import { StarIcon } from "@lifesg/react-icons/star";
 import { StarFillIcon } from "@lifesg/react-icons/star-fill";
 import styled, { css } from "styled-components";
 
-import {
-    V3_Border,
-    V3_Colour,
-    V3_MediaQuery,
-    V3_Radius,
-    V3_Spacing,
-} from "../v3_theme";
+import { Border, Colour, MediaQuery, Radius, Spacing } from "../theme";
 
 export const Input = styled.input`
     position: absolute;
@@ -20,7 +14,7 @@ export const Input = styled.input`
 const starStyle = css`
     width: 2.75rem;
     height: 2.75rem;
-    color: ${V3_Colour["icon-primary"]};
+    color: ${Colour["icon-primary"]};
 `;
 
 export const StarUnfilled = styled(StarIcon)`
@@ -32,29 +26,29 @@ export const StarFilled = styled(StarFillIcon)`
 `;
 
 export const Label = styled.div`
-    margin: 0 ${V3_Spacing["spacing-8"]};
+    margin: 0 ${Spacing["spacing-8"]};
     line-height: 0;
     display: flex;
     align-items: center;
     cursor: pointer;
 
-    ${V3_MediaQuery.MaxWidth.md} {
-        margin: 0 ${V3_Spacing["spacing-8"]};
+    ${MediaQuery.MaxWidth.md} {
+        margin: 0 ${Spacing["spacing-8"]};
     }
 `;
 
 export const Container = styled.div<{ $isFocused: boolean }>`
     display: flex;
     justify-content: center;
-    margin: ${V3_Spacing["spacing-8"]} ${V3_Spacing["spacing-16"]};
-    border-radius: ${V3_Radius["sm"]};
+    margin: ${Spacing["spacing-8"]} ${Spacing["spacing-16"]};
+    border-radius: ${Radius["sm"]};
     outline: none;
 
     ${({ $isFocused }) =>
         $isFocused &&
         css`
-            outline: ${V3_Border["width-020"]} ${V3_Border["solid"]}
-                ${V3_Colour["icon-primary"]};
+            outline: ${Border["width-020"]} ${Border["solid"]}
+                ${Colour["icon-primary"]};
             outline-offset: 2px;
         `}
 `;
--- src/feedback-rating/feedback-rating.styles.ts
+++ src/feedback-rating/feedback-rating.styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 import { Button } from "../button";
-import { V3_Colour, V3_MediaQuery } from "../v3_theme";
+import { Colour, MediaQuery } from "../theme";
 
 export const Image = styled.img`
     width: 9.5rem;
     height: 9.5rem;
     margin: 0 1rem;
-    ${V3_MediaQuery.MaxWidth.md} {
+    ${MediaQuery.MaxWidth.md} {
         width: 5.5rem;
         height: 5.5rem;
         margin: 0;
@@ -15,15 +15,15 @@ export const Image = styled.img`
 `;
 
 export const MainContainer = styled.div`
-    border-top: 1px solid ${V3_Colour.border};
-    border-bottom: 1px solid ${V3_Colour.border};
+    border-top: 1px solid ${Colour.border};
+    border-bottom: 1px solid ${Colour.border};
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
     width: 100%;
     padding: 1rem 3rem;
-    ${V3_MediaQuery.MaxWidth.md} {
+    ${MediaQuery.MaxWidth.md} {
         padding: 2rem 0 1rem;
         flex-direction: column;
     }
@@ -36,7 +36,7 @@ export const ChildContainer = styled.div`
     margin: 1rem 1rem;
     width: 100%;
     text-align: center;
-    ${V3_MediaQuery.MaxWidth.md} {
+    ${MediaQuery.MaxWidth.md} {
         margin: 1rem 1.25rem;
         max-width: calc(100% - 2.5rem);
     }
