8a76c56a34baff517b12b537ef4350e8245bb5a2
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-05-25 13:23:25 +0700

    [BOOKINGSG-9333][GZ] visual test - file thumbnail

---
+++ e2e/nextjs-app/src/app/components/file-upload/file-thumbnail.e2e.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import type { FileItemProps } from "@lifesg/react-design-system/file-upload";
+import { FileUpload } from "@lifesg/react-design-system/file-upload";
+
+const THUMBNAIL_URL =
+    "https://assets.life.gov.sg/react-design-system/img/feedback-rating/feedback-rating-banner.png";
+
+const FILE_ITEMS: FileItemProps[] = [
+    {
+        id: "pdf-icon",
+        name: "document-icon.pdf",
+        size: 2048,
+        type: "application/pdf",
+    },
+    {
+        id: "pdf-thumbnail",
+        name: "document-thumbnail.pdf",
+        size: 4096,
+        type: "application/pdf",
+        thumbnailImageDataUrl: THUMBNAIL_URL,
+    },
+    {
+        id: "image-thumbnail",
+        name: "photo-thumbnail.jpg",
+        size: 5120,
+        type: "image/jpeg",
+        thumbnailImageDataUrl: THUMBNAIL_URL,
+    },
+];
+
+export default function Story() {
+    return (
+        <FileUpload
+            data-testid="file-upload"
+            title="PDF and thumbnail"
+            description="PDF files show icon fallback while provided thumbnails are used."
+            fileItems={FILE_ITEMS}
+        />
+    );
+}
