8b44c6ff088e76fb41ab05af9d6fb02d63ef7549
Ian Tjahjono <ian.t.gds@gt.tech.gov.sg>
2026-06-02 14:18:04 +0800

    [BOOKINGSG-9335][IT] Migrate v3 design tokens to v4 tokens

--- src/time-slot-bar-week/time-slot-bar-week-days.styles.ts
+++ src/time-slot-bar-week/time-slot-bar-week-days.styles.ts
@@ -4,14 +4,8 @@ import styled, { css } from "styled-components";
 
 import { Button } from "../button";
 import { TimeSlot } from "../shared/time-slot";
+import { Colour, Font, Motion, Radius, Spacing } from "../theme";
 import { Typography } from "../typography";
-import {
-    V3_Colour,
-    V3_Font,
-    V3_Motion,
-    V3_Radius,
-    V3_Spacing,
-} from "../v3_theme";
 
 interface TimeSlotCellProps {
     $height: number;
@@ -36,7 +30,7 @@ export const HeaderCellWeek = styled.div`
     justify-content: center;
     pointer-events: none;
     user-select: none;
-    margin-bottom: ${V3_Spacing["spacing-8"]};
+    margin-bottom: ${Spacing["spacing-8"]};
     flex: 1;
 `;
 
@@ -44,7 +38,7 @@ export const CellWeekText = styled(Typography.BodyXS)<CellWeekTextStyleProps>`
     ${(props) =>
         props.$disabled &&
         css`
-            color: ${V3_Colour["text-disabled-subtlest"]};
+            color: ${Colour["text-disabled-subtlest"]};
         `}
 `;
 
@@ -61,7 +55,7 @@ export const GridWrapper = styled.div`
     width: 100%;
     display: grid;
     grid-template-columns: auto repeat(7, 1fr);
-    column-gap: ${V3_Spacing["spacing-4"]};
+    column-gap: ${Spacing["spacing-4"]};
 `;
 
 export const Expandable = animated(styled.div`
@@ -71,8 +65,8 @@ export const Expandable = animated(styled.div`
 
 export const ColumnWeekCell = styled.div`
     display: flex;
-    gap: ${V3_Spacing["spacing-4"]};
-    transition: all ${V3_Motion["duration-250"]} ${V3_Motion["ease-default"]};
+    gap: ${Spacing["spacing-4"]};
+    transition: all ${Motion["duration-250"]} ${Motion["ease-default"]};
     overflow: hidden;
 `;
 
@@ -80,9 +74,9 @@ export const TimeColumn = styled.div<TimeColumnStyleProps>`
     display: flex;
     flex-direction: column;
     grid-row: 3 / auto;
-    gap: ${V3_Spacing["spacing-4"]};
+    gap: ${Spacing["spacing-4"]};
     width: 1.375rem;
-    transition: all ${V3_Motion["duration-250"]} ${V3_Motion["ease-default"]};
+    transition: all ${Motion["duration-250"]} ${Motion["ease-default"]};
     overflow: hidden;
 
     ${(props) => props.$height && `height: ${props.$height}px;`}
@@ -96,12 +90,12 @@ export const TimeColumnWrapper = styled.div`
 `;
 
 export const TimeColumnText = styled.div`
-    ${V3_Font["body-xs-bold"]}
-    color: ${V3_Colour["text-subtler"]};
+    ${Font["body-xs-bold"]}
+    color: ${Colour["text-subtler"]};
     text-align: center;
     line-height: 12px; // align with cell on the same row
     &:first-line {
-        font-size: ${V3_Font.Spec["body-size-sm"]};
+        font-size: ${Font.Spec["body-size-sm"]};
     }
 `;
 
@@ -109,12 +103,12 @@ export const TimeSlotWrapper = styled.div`
     display: flex;
     flex-direction: column;
     flex: 1;
-    gap: ${V3_Spacing["spacing-4"]};
+    gap: ${Spacing["spacing-4"]};
 `;
 
 export const CollapseExpandAllWrapper = styled.div`
     display: flex;
-    margin-top: ${V3_Spacing["spacing-8"]};
+    margin-top: ${Spacing["spacing-8"]};
 `;
 
 export const CollapseExpandAllButton = styled(Button.Small)`
@@ -123,14 +117,13 @@ export const CollapseExpandAllButton = styled(Button.Small)`
     span {
         display: flex;
         align-items: center;
-        gap: ${V3_Spacing["spacing-8"]};
+        gap: ${Spacing["spacing-8"]};
     }
 `;
 
 export const ChevronIcon = styled(ChevronUpIcon)<ChevronIconStyleProps>`
     transform: rotate(${(props) => (props.$isExpanded ? 0 : 180)}deg);
-    transition: transform ${V3_Motion["duration-250"]}
-        ${V3_Motion["ease-default"]};
+    transition: transform ${Motion["duration-250"]} ${Motion["ease-default"]};
 `;
 
 export const TimeSlotComponent = styled(TimeSlot)<TimeSlotCellProps>`
@@ -144,10 +137,10 @@ export const TimeSlotComponent = styled(TimeSlot)<TimeSlotCellProps>`
     height: ${(props) => props.$height}px;
     min-height: ${(props) => props.$height}px;
     margin: 0;
-    border-radius: ${V3_Radius["xs"]};
+    border-radius: ${Radius["xs"]};
 
     &:focus-within {
-        outline: 2px solid ${V3_Colour["focus-ring"]};
+        outline: 2px solid ${Colour["focus-ring"]};
         outline-offset: -2px;
     }
 
@@ -164,15 +157,15 @@ export const TimeSlotComponent = styled(TimeSlot)<TimeSlotCellProps>`
                                 ? "transparent"
                                 : props.bgColor}
                             50%,
-                        ${V3_Colour["bg-strongest"]} 0%
+                        ${Colour["bg-strongest"]} 0%
                     )
                     ${props.styleType === "stripes" &&
                     `, repeating-linear-gradient(
                             135deg,
-                            ${props.bgColor2 || V3_Colour["bg-strongest"]} 0px,
-                            ${props.bgColor2 || V3_Colour["bg-strongest"]} 10px,
-                            ${props.bgColor || V3_Colour["bg-stronger"]} 10px,
-                            ${props.bgColor || V3_Colour["bg-stronger"]} 20px
+                            ${props.bgColor2 || Colour["bg-strongest"]} 0px,
+                            ${props.bgColor2 || Colour["bg-strongest"]} 10px,
+                            ${props.bgColor || Colour["bg-stronger"]} 10px,
+                            ${props.bgColor || Colour["bg-stronger"]} 20px
                         )`};
             `;
         }
