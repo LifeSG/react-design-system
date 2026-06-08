6dfd5cdc22e86cbd56ed589fc5c9a0355857a873
Ryan.Nguyen <trungnh41@fpt.com>
2026-06-08 15:05:25 +0700

    [CCUBE-2121][RYN] TimeSlotWeekView: restore tokens exposed by Calendar Manager

--- src/time-slot-week-view/time-slot-week-view.styles.ts
+++ src/time-slot-week-view/time-slot-week-view.styles.ts
@@ -1,5 +1,13 @@
 import { css } from "@linaria/core";
 
+import { MediaQuery } from "../theme";
+
 export const wrapper = css`
+    --header-bottom-spacing: 0.5rem;
+
+    ${MediaQuery.MaxWidth.sm} {
+        --header-horizontal-inset: 1.25rem;
+    }
+
     min-width: 18.5rem;
 `;
