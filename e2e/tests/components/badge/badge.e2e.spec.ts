76934e113e7c74f20cad9b1167f28badc80e068f
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-06-15 18:27:17 +0800

    [MISC][RL] Clean up badge e2e tests

--- e2e/tests/components/badge/badge.e2e.spec.ts
+++ e2e/tests/components/badge/badge.e2e.spec.ts
@@ -1,29 +1,15 @@
-import { test as base, expect, Locator, Page } from "@playwright/test";
+import { test as base, Locator, Page } from "@playwright/test";
 import { AbstractStoryPage, compareScreenshot } from "../../utils";
 
 class StoryPage extends AbstractStoryPage {
     protected readonly component = "badge";
 
-    public readonly locators: {
-        variantsRowDefault: Locator;
-        variantsRowImportant: Locator;
-        badgeCount1000: Locator;
-        badgeCount2090: Locator;
-        anchoredOffset: Locator;
-        anchoredAvatar: Locator;
-    };
+    public readonly locators: {};
 
     constructor(page: Page) {
         super(page);
 
-        this.locators = {
-            variantsRowDefault: page.getByTestId("badge-dot-default"),
-            variantsRowImportant: page.getByTestId("badge-dot-important"),
-            badgeCount1000: page.getByTestId("badge-count-1000"),
-            badgeCount2090: page.getByTestId("badge-count-2090"),
-            anchoredOffset: page.getByTestId("badge-anchored-offset"),
-            anchoredAvatar: page.getByTestId("badge-anchored-avatar"),
-        };
+        this.locators = {};
     }
 }
 
@@ -50,7 +36,7 @@ test.describe("Badge", () => {
             await story.init("variants", { mode: "dark" });
         });
 
-        test("Variants dark mode", async ({ story }) => {
+        test("Variants (dark mode)", async ({ story }) => {
             await compareScreenshot(story, "mount");
         });
     });
