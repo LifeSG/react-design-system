4e72fdb4469b2c745dd7136745c2e2eae55000da
Quek Ruo Ling <ruoling.quek@ufinity.com>
2026-07-15 22:59:41 +0800

    [CCUBE-2187][RL] Refactor to use common mocks

--- e2e/nextjs-app/src/app/components/data-table/action-bar-overflow-with-long-items.e2e.tsx
+++ e2e/nextjs-app/src/app/components/data-table/action-bar-overflow-with-long-items.e2e.tsx
@@ -2,34 +2,9 @@
 
 import { DataTable } from "@lifesg/react-design-system/data-table";
 import { useMemo, useState } from "react";
-import styles from "./data-table.module.css";
+import { buildRows, headers } from "./common";
 
-const headers = [
-    {
-        fieldKey: "title",
-        label: "Title",
-        keyColumn: true,
-    },
-    {
-        fieldKey: "status",
-        label: "Status",
-    },
-    {
-        fieldKey: "time",
-        label: "Time",
-    },
-];
-
-const rows = Array.from({ length: 50 }, (_, index) => {
-    const number = index + 1;
-
-    return {
-        id: number.toString(),
-        title: `Title ${number}`,
-        status: number % 2 === 0 ? "Completed" : "Pending",
-        time: `07/Aug/2023 ${(number % 12) + 1}.30pm`,
-    };
-});
+const rows = buildRows(50);
 
 export default function Story() {
     const [selectedIds, setSelectedIds] = useState<string[]>([]);
@@ -52,21 +27,19 @@ export default function Story() {
     };
 
     return (
-        <div className="story-column-container">
-            <DataTable
-                data-testid="data-table"
-                headers={headers}
-                rows={rows}
-                enableMultiSelect
-                enableSelectAll
-                enableActionBar
-                selectedIds={selectedIds}
-                onSelect={handleSelect}
-                onSelectAll={handleSelectAll}
-                onClearSelectionClick={() => {
-                    setSelectedIds([]);
-                }}
-            />
-        </div>
+        <DataTable
+            data-testid="data-table"
+            headers={headers}
+            rows={rows}
+            enableMultiSelect
+            enableSelectAll
+            enableActionBar
+            selectedIds={selectedIds}
+            onSelect={handleSelect}
+            onSelectAll={handleSelectAll}
+            onClearSelectionClick={() => {
+                setSelectedIds([]);
+            }}
+        />
     );
 }
