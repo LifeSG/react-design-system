11ddcc8ccc0b5a5b6d6d93a982ae04e3053e2783
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-07-17 13:28:29 +0700

    [CCUBE-2183][GZ] ensure tab content took the whole container width

--- stories/storybook-common/tabs.tsx
+++ stories/storybook-common/tabs.tsx
@@ -99,4 +99,9 @@ const content = css`
     align-items: center;
     justify-content: center;
     padding: 1rem 1rem;
+
+    // Ensure that the content takes up the full width of the container
+    & > div {
+        width: 100%;
+    }
 `;
