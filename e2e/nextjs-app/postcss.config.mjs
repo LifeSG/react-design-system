671c56c4e49dff35d5d8c3a548e594abcc0b7c00
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-08-06 13:22:17 +0700

    [CCUBE-2250][ZZ] build: omit plugins in e2e next app during ci mode

--- e2e/nextjs-app/postcss.config.mjs
+++ e2e/nextjs-app/postcss.config.mjs
@@ -4,23 +4,25 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = {
-    plugins: [
-        [
-            "postcss-import",
-            {
-                path: [path.resolve(__dirname, "../../")],
-            },
-        ],
-        [
-            "postcss-mixins",
-            {
-                mixinsFiles: path.join(
-                    path.resolve(__dirname, "../../src/theme/styles/presets"),
-                    "**/*.css"
-                ),
-            },
-        ],
-    ],
+    plugins: process.env.CI
+        ? []
+        : [
+              [
+                  "postcss-import",
+                  {
+                      path: [path.resolve(__dirname, "../../")],
+                  },
+              ],
+              [
+                  "postcss-mixins",
+                  {
+                      mixinsFiles: path.resolve(
+                          __dirname,
+                          "../../src/theme/styles/presets/**/*.css"
+                      ),
+                  },
+              ],
+          ],
 };
 
 export default config;
