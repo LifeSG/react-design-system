6ed450fb5634ed16213f6a372d35615b6697a678
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-08-13 10:14:23 +0700

    [MISC][GZ] separate tools and ui unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,45 @@
 process.env.TZ = "UTC";
+
+const moduleFileExtensions = ["tsx", "ts", "js"];
+const moduleDirectories = ["node_modules", "<rootDir>"];
+const moduleNameMapper = {
+    "react-player": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy",
+};
+
 module.exports = {
     rootDir: ".",
-    moduleFileExtensions: ["tsx", "ts", "js"],
+    moduleFileExtensions,
     coverageDirectory: "<rootDir>/tests/coverage",
     coveragePathIgnorePatterns: [
         "node_modules",
         "tools/storybook-argtypes/adapters",
     ],
-    testEnvironment: "jsdom",
-    testMatch: ["<rootDir>/tests/**/*.spec.[jt]s?(x)"],
-    moduleNameMapper: {
-        "react-player": "<rootDir>/src/__mocks__/fileMock.js",
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/src/__mocks__/fileMock.js",
-        "\\.(css|less)$": "identity-obj-proxy",
-    },
-    moduleDirectories: ["node_modules", "<rootDir>"],
-    setupFilesAfterEnv: [
-        "jest-canvas-mock",
-        "@testing-library/jest-dom",
-        "<rootDir>/tests/jest-setup.ts",
+    projects: [
+        {
+            displayName: "ui",
+            testEnvironment: "jsdom",
+            testMatch: ["<rootDir>/tests/**/*.spec.[jt]s?(x)"],
+            testPathIgnorePatterns: ["<rootDir>/tests/tools/"],
+            moduleFileExtensions,
+            moduleDirectories,
+            moduleNameMapper,
+            setupFilesAfterEnv: [
+                "jest-canvas-mock",
+                "@testing-library/jest-dom",
+                "<rootDir>/tests/jest-setup.ts",
+            ],
+        },
+        {
+            displayName: "tools",
+            testEnvironment: "node",
+            testMatch: ["<rootDir>/tests/tools/**/*.spec.[jt]s?(x)"],
+            moduleFileExtensions,
+            moduleDirectories,
+            moduleNameMapper,
+        },
     ],
     reporters: ["default", ["jest-junit", { outputName: "junit.xml" }]],
     maxWorkers: process.env.CI ? 1 : "50%",
