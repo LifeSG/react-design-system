6103f32a72f71a10b78a08e096cbba5c38109676
Ziggy Z. <ziggy.z@gt.tech.gov.sg>
2026-08-14 11:22:05 +0700

    [CCUBE-2220][ZZ] test: fix modal for close button left

--- e2e/nextjs-app/src/app/components/modal-v2/header-close-button-left.e2e.tsx
+++ e2e/nextjs-app/src/app/components/modal-v2/header-close-button-left.e2e.tsx
@@ -6,10 +6,10 @@ export default function Story() {
     return (
         <ModalV2 show>
             <ModalV2.Card>
-                <ModalV2.Header title="Signature" closeButtonPosition="right" />
+                <ModalV2.Header title="Signature" closeButtonPosition="left" />
                 <ModalV2.Content>
                     <Typography.BodyBL data-testid="header-content">
-                        Header with close button on the right and heading title
+                        Header with close button on the left and heading title
                     </Typography.BodyBL>
                 </ModalV2.Content>
             </ModalV2.Card>
