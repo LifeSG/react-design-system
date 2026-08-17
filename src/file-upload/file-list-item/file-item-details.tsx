03965dc4fba7ed730af3ab406867c50f32c79ae8
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-08-17 14:49:26 +0700

    [CCUBE-2188][GZ] merge file list item and file item edit

---
+++ src/file-upload/file-list-item/file-item-details.tsx
@@ -0,0 +1,134 @@
+import { ExclamationCircleFillIcon } from "@lifesg/react-icons/exclamation-circle-fill";
+
+import type { FormLabelProps } from "../../form/form-label/types";
+import { Typography } from "../../typography";
+import * as styles from "./file-list-item.styles";
+import type { FileItemMode } from "./types";
+
+interface Props {
+    mode: FileItemMode;
+    formattedName: string | undefined;
+    description: string | undefined;
+    fileSize: string;
+    errorMessage: string | React.ReactNode | undefined;
+    isLoading: boolean;
+    descriptionLabel?: FormLabelProps | undefined;
+    detailSectionRef: React.Ref<HTMLDivElement>;
+    children?: React.ReactNode;
+}
+
+export const FileItemDetails = ({
+    mode,
+    formattedName,
+    description,
+    fileSize,
+    errorMessage,
+    isLoading,
+    descriptionLabel,
+    detailSectionRef,
+    children,
+}: Props) => {
+    if (mode === "error") {
+        return (
+            <>
+                <div ref={detailSectionRef} className={styles.nameSection}>
+                    <Typography.BodyMD
+                        weight={description ? "semibold" : "regular"}
+                    >
+                        {formattedName}
+                    </Typography.BodyMD>
+                    {description && (
+                        <>
+                            <Typography.BodyMD
+                                className={styles.itemDescriptionLabel}
+                            >
+                                {descriptionLabel?.children ??
+                                    "Photo description"}
+                            </Typography.BodyMD>
+                            <Typography.BodyMD
+                                className={styles.itemDescriptionText}
+                            >
+                                {description}
+                            </Typography.BodyMD>
+                        </>
+                    )}
+                    {errorMessage && (
+                        <Typography.BodySM
+                            className={styles.desktopErrorMessage}
+                            weight="semibold"
+                        >
+                            <ExclamationCircleFillIcon
+                                className={styles.errorIcon}
+                                aria-hidden
+                            />
+                            {errorMessage}
+                        </Typography.BodySM>
+                    )}
+                </div>
+                <div
+                    className={styles.fileSizeSection}
+                    data-mobile-visibility={isLoading ? "hidden" : "expand"}
+                >
+                    <Typography.BodyMD className={styles.fileSizeText}>
+                        {fileSize}
+                    </Typography.BodyMD>
+                </div>
+                {errorMessage && (
+                    <Typography.BodySM
+                        className={styles.mobileErrorMessage}
+                        weight="semibold"
+                    >
+                        <ExclamationCircleFillIcon
+                            className={styles.errorIcon}
+                            aria-hidden
+                        />
+                        {errorMessage}
+                    </Typography.BodySM>
+                )}
+            </>
+        );
+    }
+
+    if (mode === "display" && description) {
+        return (
+            <div ref={detailSectionRef} className={styles.extendedNameSection}>
+                <Typography.BodyMD weight="semibold">
+                    {formattedName}
+                </Typography.BodyMD>
+                <Typography.BodyMD className={styles.itemDescriptionLabel}>
+                    {descriptionLabel?.children ?? "Photo description"}
+                </Typography.BodyMD>
+                <Typography.BodyMD className={styles.itemDescriptionText}>
+                    {description}
+                </Typography.BodyMD>
+                <Typography.BodyMD className={styles.descriptionFileSizeText}>
+                    {fileSize}
+                </Typography.BodyMD>
+                {children}
+            </div>
+        );
+    }
+
+    return (
+        <div ref={detailSectionRef} className={styles.nameSection}>
+            <Typography.BodyMD weight={description ? "semibold" : "regular"}>
+                {formattedName}
+            </Typography.BodyMD>
+            {description && (
+                <>
+                    <Typography.BodyMD className={styles.itemDescriptionLabel}>
+                        {descriptionLabel?.children ?? "Photo description"}
+                    </Typography.BodyMD>
+                    <Typography.BodyMD className={styles.itemDescriptionText}>
+                        {description}
+                    </Typography.BodyMD>
+                </>
+            )}
+            {!isLoading && (
+                <Typography.BodyMD className={styles.descriptionFileSizeText}>
+                    {fileSize}
+                </Typography.BodyMD>
+            )}
+        </div>
+    );
+};
