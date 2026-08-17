26e756a2dfd9c5ebe8e7654c9b04a10890acfab8
Ghazwan <ghazwan_muhammad@thesoftwarepractice.com>
2026-08-17 14:25:39 +0700

    [CCUBE-2188][GZ] extract shared utils, define new types

--- src/file-upload/file-list-item/types.ts
+++ src/file-upload/file-list-item/types.ts
@@ -1,15 +1,41 @@
+import type { FormLabelProps } from "../../form/form-label/types";
 import type { FileItemProps } from "../types";
 
-export interface FileListItemProps {
+export type FileItemMode = "display" | "edit" | "error";
+
+interface BaseProps {
     fileItem: FileItemProps;
     wrapperWidth: number;
+    disabled?: boolean | undefined;
+    readOnly?: boolean | undefined;
+    descriptionLabel?: FormLabelProps | undefined;
+}
 
+interface DisplayModeProps extends BaseProps {
+    mode: "display";
     /** If specified, the edit buttons will be rendered */
     editable?: boolean | undefined;
     /** If specified, drag handles will be rendered */
     sortable?: boolean | undefined;
-    disabled?: boolean | undefined;
-
     onDelete: () => void;
     onEditClick?: (() => void) | undefined;
 }
+
+interface EditModeProps extends BaseProps {
+    mode: "edit";
+    fileDescriptionMaxLength?: number | undefined;
+    descriptionRequired?: boolean | undefined;
+    onSave: (description: string) => void;
+    onCancel: () => void;
+    onBlur: (value: string) => void;
+}
+
+interface ErrorModeProps extends BaseProps {
+    mode: "error";
+    onDelete: () => void;
+}
+
+export type FileListItemProps =
+    | DisplayModeProps
+    | EditModeProps
+    | ErrorModeProps;
