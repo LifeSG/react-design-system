d9c09234f97f5d1211d68c9bbae47d55e14558d4
Ryan.Nguyen <trungnh41@fpt.com>
2026-08-21 11:08:55 +0700

    [CCUBE-2243][RYN] adjust form input keywords

--- src/input-textarea/textarea.tsx
+++ src/input-textarea/textarea.tsx
@@ -234,6 +234,6 @@ const TextareaComponent = (
 // @catalog
 /**
  * A multi-line text input with an optional character counter and prefix.
- * @keywords field, form, input, multiline, text area, text field
+ * @keywords field, form, input, multiline, text field
  */
 export const Textarea = React.forwardRef(TextareaComponent);
