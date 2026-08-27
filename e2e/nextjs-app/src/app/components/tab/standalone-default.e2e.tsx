1a0098112d7627ac781b00902f466ff7e49983e2
Ryan.Nguyen <trungnh41@fpt.com>
2026-08-27 15:07:38 +0700

    [CCUBE-2277][RYN] remove redundant e2e tests

--- e2e/nextjs-app/src/app/components/tab/standalone-default.e2e.tsx
+++ e2e/nextjs-app/src/app/components/tab/standalone-default.e2e.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { Tab } from "@lifesg/react-design-system/tab";
 
+import styles from "./tab.module.css";
+
 export default function Story() {
     return (
         <div className="story-column-container story-background">
-            <Tab.Context>
-                <Tab.TabList data-testid="standalone-tabs">
-                    <Tab.TabListItem title="Section A" />
-                    <Tab.TabListItem title="Section B" />
-                    <Tab.TabListItem title="Section C" />
-                </Tab.TabList>
-                <Tab.Panel index={0} data-testid="tab-content-a">
-                    <div className="story-padding">Contents of A</div>
-                </Tab.Panel>
-                <Tab.Panel index={1} data-testid="tab-content-b">
-                    <div className="story-padding">Contents of B</div>
-                </Tab.Panel>
-                <Tab.Panel index={2} data-testid="tab-content-c">
-                    <div className="story-padding">Contents of C</div>
-                </Tab.Panel>
-            </Tab.Context>
+            <div className={styles["tab-list-container"]}>
+                <Tab.Context>
+                    <Tab.TabList data-testid="standalone-tabs">
+                        <Tab.TabListItem title="Section A" />
+                        <Tab.TabListItem title="Section B" />
+                        <Tab.TabListItem title="Section C" />
+                    </Tab.TabList>
+                    {/* panels are intentionally separated from the tab bar by other content */}
+                    <div
+                        className={styles["interposed-content"]}
+                        data-testid="interposed-content"
+                    >
+                        Some other page content between the tab bar and panels
+                    </div>
+                    <Tab.Panel index={0} data-testid="tab-content-a">
+                        <div className="story-padding">Contents of A</div>
+                    </Tab.Panel>
+                    <Tab.Panel index={1} data-testid="tab-content-b">
+                        <div className="story-padding">Contents of B</div>
+                    </Tab.Panel>
+                    <Tab.Panel index={2} data-testid="tab-content-c">
+                        <div className="story-padding">Contents of C</div>
+                    </Tab.Panel>
+                </Tab.Context>
+            </div>
         </div>
     );
 }
